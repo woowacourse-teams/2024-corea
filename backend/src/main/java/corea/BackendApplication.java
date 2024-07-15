176463eeb01b3a722433257929d8acfaa8a1f5af
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-07-15 16:45:40 +0900

    [BE] feat: 구현 도메인 정의 구현(#7) (#8)
    * refactor: 패키지명 변경
    
    * feat: Member, Room 도메인 정의

--- backend/src/main/java/corea/BackendApplication.java
+++ backend/src/main/java/corea/BackendApplication.java
@@ -1,4 +1,4 @@
-package corea.backend;
+package corea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
--- backend/src/test/java/corea/BackendApplicationTests.java
+++ backend/src/test/java/corea/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package corea.backend;
+package corea;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
