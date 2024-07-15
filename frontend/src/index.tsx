2b282eae0103b7961dbcd13fade1ccf16261078e
Lee sang Yeop <mma7710@naver.com>
2024-07-15 15:11:41 +0900

    style: globalStyle 적용
    Co-authored-by: 강다빈 <00kang@users.noreply.github.com>
    Co-authored-by: 최진실 <chlwlstlf@users.noreply.github.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "@/router";
+import GlobalStyles from "@/styles/globalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <GlobalStyles />
     <RouterProvider router={router} />
   </React.StrictMode>,
 );
