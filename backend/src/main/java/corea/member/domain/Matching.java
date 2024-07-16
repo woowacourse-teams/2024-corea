487e6412ce7582ad1cce917868109e48c42e640a
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-07-16 11:35:39 +0900

    feat: 매칭 기능 참가자들로 작동하게 변경

--- backend/src/main/java/corea/member/domain/Matching.java
+++ backend/src/main/java/corea/member/domain/Matching.java
@@ -1,6 +1,5 @@
 package corea.member.domain;
 
-import corea.domain.Member;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,18 +12,17 @@ import static lombok.AccessLevel.PROTECTED;
 @NoArgsConstructor(access = PROTECTED)
 public class Matching {
 
-    public Map<Long, List<Long>> matchGroup(final List<Member> members, final int matchingSize) {
+    public Map<Long, List<Long>> matchGroup(final List<Long> memberIds, final int matchingSize) {
         final Map<Long, List<Long>> matchedGroup = new HashMap<>();
 
-        final List<Long> memberIds = new ArrayList<>(members.stream().map(Member::getId).toList());
         Collections.shuffle(memberIds);
 
-        final List<List<Long>> groupedMemberIds = IntStream.range(0, (members.size() + matchingSize - 1) / matchingSize)
+        final List<List<Long>> groupedMemberIds = IntStream.range(0, (memberIds.size() + matchingSize - 1) / matchingSize)
                 .mapToObj(i -> memberIds.subList(i * matchingSize, Math.min(i * matchingSize + matchingSize, memberIds.size())))
                 .toList();
 
         long groupId = 1L;
-        for (List<Long> l : groupedMemberIds) {
+        for (final List<Long> l : groupedMemberIds) {
             matchedGroup.put(groupId++, l);
         }
 
--- backend/src/main/java/corea/repository/ParticipationRepository.java
+++ backend/src/main/java/corea/repository/ParticipationRepository.java
@@ -3,5 +3,8 @@ package corea.repository;
 import corea.domain.Participation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+    List<Participation> findAllByRoomId(long roomId);
 }
