62da248de907d5cb5cf2de76580bc2dc38495949
조경찬 (Jo Gyungchan) <namejgc@naver.com>
2024-07-16 11:36:00 +0900

    [BE] feat: 방 조회 API 구현(#12) (#19)
    * feat: 방에 필요한 정보 추가
    
    * feat: 방 조회 API 구현

---
+++ backend/src/main/java/corea/cotroller/RoomController.java
@@ -0,0 +1,29 @@
+package corea.cotroller;
+
+import corea.dto.RoomResponse;
+import corea.dto.RoomResponses;
+import corea.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RoomController {
+
+    private final RoomService roomService;
+
+    @GetMapping("/rooms/{id}")
+    public ResponseEntity<RoomResponse> room(@PathVariable final long id) {
+        final RoomResponse response = roomService.findOne(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/rooms")
+    public ResponseEntity<RoomResponses> rooms() {
+        final RoomResponses response = roomService.findAll();
+        return ResponseEntity.ok(response);
+    }
+}
--- backend/src/main/java/corea/dto/RoomCreateRequest.java
+++ backend/src/main/java/corea/dto/RoomCreateRequest.java
@@ -6,11 +6,14 @@ import java.time.LocalDateTime;
 
 public record RoomCreateRequest(
         String title,
+        String content,
         long memberId,
         String repositoryLink,
         String thumbnailLink,
         int matchingSize,
         String keyword,
+        long currentParticipantsSize,
+        long limitedParticipantsSize,
         LocalDateTime submissionDeadline,
         LocalDateTime reviewDeadline
 ) {
@@ -18,11 +21,14 @@ public record RoomCreateRequest(
     public Room toEntity() {
         return new Room(
                 title,
+                content,
                 memberId,
                 repositoryLink,
                 thumbnailLink,
                 matchingSize,
                 keyword,
+                currentParticipantsSize,
+                limitedParticipantsSize,
                 submissionDeadline,
                 reviewDeadline
         );
