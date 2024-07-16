571a4f2e485e7ecedf50714ba3c0460ab804fc1e
gyungchan Jo <namejgc@naver.com>
2024-07-16 12:25:24 +0900

    config: CORS 설정

---
+++ backend/src/main/java/corea/WebConfig.java
@@ -0,0 +1,18 @@
+package corea;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081")
+                .allowedMethods("GET", "POST", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+}
