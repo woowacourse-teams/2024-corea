9a156a71f4f199e93796498c7cdf663d146e476e
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-07-18 15:44:40 +0900

    feat: 방에 참가하는 기능 요청-응답 기능 구현

---
+++ backend/src/main/java/corea/matching/controller/ParticipateController.java
@@ -0,0 +1,26 @@
+package corea.matching.controller;
+
+import corea.auth.annotation.LoginUser;
+import corea.auth.domain.AuthInfo;
+import corea.matching.dto.ParticipationRequest;
+import corea.matching.service.ParticipationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/participate")
+@RequiredArgsConstructor
+public class ParticipateController {
+    private final ParticipationService participationService;
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Void> participate(@PathVariable final long id, @LoginUser AuthInfo authInfo) {
+        participationService.participate(new ParticipationRequest(id,authInfo.getId()));
+        return ResponseEntity.ok()
+                .build();
+    }
+}
