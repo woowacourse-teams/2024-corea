84005bf6476e58ed643ecdf4c055d874c9abed8f
gyungchan Jo <namejgc@naver.com>
2024-07-18 22:49:55 +0900

    refactor: requestMapping을 이용한 중복 제거

--- backend/src/main/java/corea/room/controller/RoomController.java
+++ backend/src/main/java/corea/room/controller/RoomController.java
@@ -2,7 +2,6 @@ package corea.room.controller;
 
 import corea.auth.annotation.LoginMember;
 import corea.auth.domain.AuthInfo;
-import corea.member.domain.Member;
 import corea.room.dto.RoomResponse;
 import corea.room.dto.RoomResponses;
 import corea.room.service.RoomService;
@@ -10,27 +9,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/rooms")
 @RequiredArgsConstructor
 public class RoomController {
 
     private final RoomService roomService;
 
-    @GetMapping("/rooms/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<RoomResponse> room(@PathVariable long id) {
         RoomResponse response = roomService.findOne(id);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/rooms")
+    @GetMapping
     public ResponseEntity<RoomResponses> rooms() {
         RoomResponses response = roomService.findAll();
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/rooms/participated")
+    @GetMapping("/participated")
     public ResponseEntity<RoomResponses> participatedRooms(@LoginMember AuthInfo authInfo) {
         RoomResponses response = roomService.findParticipatedRooms(authInfo.getId());
         return ResponseEntity.ok(response);
