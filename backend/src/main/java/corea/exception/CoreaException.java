bbefc3de8b7516b9565e2b1d9b8491fdf34bec0e
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-07-18 15:39:46 +0900

    feat: 예외 타입 추가, 동적 메시지 추가

--- backend/src/main/java/corea/exception/CoreaException.java
+++ backend/src/main/java/corea/exception/CoreaException.java
@@ -1,9 +1,16 @@
 package corea.exception;
 
-public class CoreaException extends Exception {
-    private final ExceptionType exceptionType;
+import org.springframework.http.HttpStatus;
 
-    public CoreaException(ExceptionType exceptionType) {
+public class CoreaException extends RuntimeException {
+    private final ExceptionType exceptionType;
+    public CoreaException(ExceptionType exceptionType){
+        super(exceptionType.getMessage());
+        this.exceptionType = exceptionType;
+    }
+
+    public CoreaException(ExceptionType exceptionType,String message) {
+        super(message);
         this.exceptionType = exceptionType;
     }
 
@@ -12,7 +19,13 @@ public class CoreaException extends Exception {
         this.exceptionType = exceptionType;
     }
 
-    public ExceptionType getExceptionType() {
-        return exceptionType;
+    public HttpStatus getHttpStatus(){
+        return exceptionType.getHttpStatus();
     }
+    
+    @Override
+    public String getMessage(){
+        return exceptionType.getMessage();
+    }
+
 }
--- backend/src/main/java/corea/exception/ExceptionType.java
+++ backend/src/main/java/corea/exception/ExceptionType.java
@@ -3,6 +3,9 @@ package corea.exception;
 import org.springframework.http.HttpStatus;
 
 public enum ExceptionType {
+
+    NOT_FOUND_ERROR(HttpStatus.NOT_FOUND,"해당하는 값이 없습니다."),
+    ALREADY_APPLY(HttpStatus.BAD_REQUEST,"해당 방에 이미 참여했습니다"),
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"서버에 문제가 발생했습니다.");
     private final HttpStatus httpStatus;
     private final String message;
