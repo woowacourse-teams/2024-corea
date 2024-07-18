0a3b25dfde68f7f4fbc88fc92dd5af5f77d17578
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-07-18 15:44:11 +0900

    feat: 인증 기능 구현

---
+++ backend/src/main/java/corea/auth/RequestHandler.java
@@ -0,0 +1,13 @@
+package corea.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RequestHandler {
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    public String extract(HttpServletRequest request){
+        return request.getHeader(AUTHORIZATION_HEADER);
+    }
+
+}
