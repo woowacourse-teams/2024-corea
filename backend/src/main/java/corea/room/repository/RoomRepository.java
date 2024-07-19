868a02ae12c2f6431bf264d8b604dba75397a13c
gyungchan Jo <namejgc@naver.com>
2024-07-19 14:46:19 +0900

    feat: 모집중인 방 리스트를 조회하는 기능 구현

--- backend/src/main/java/corea/room/repository/RoomRepository.java
+++ backend/src/main/java/corea/room/repository/RoomRepository.java
@@ -1,7 +1,33 @@
 package corea.room.repository;
 
+import corea.room.domain.Classification;
 import corea.room.domain.Room;
+import corea.room.domain.RoomStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    @Query("""
+            SELECT r FROM Room r 
+            LEFT JOIN Participation p 
+            ON r.id = p.roomId AND p.memberId = :memberId 
+            WHERE p.id IS NULL AND r.status = :status AND r.manager.id <> :memberId
+            """)
+    Page<Room> findAllByMemberAndStatus(long memberId, RoomStatus status, PageRequest pageRequest);
+
+    @Query("""
+            SELECT r FROM Room r 
+            LEFT JOIN Participation p 
+            ON r.id = p.roomId AND p.memberId = :memberId 
+            WHERE p.id IS NULL AND r.classification = :classification AND r.status = :status AND r.manager.id <> :memberId
+            """)
+    Page<Room> findAllByMemberAndClassificationAndStatus(long memberId, Classification classification, RoomStatus status, Pageable pageable);
+
+    Page<Room> findAllByStatus(RoomStatus status, PageRequest pageRequest);
+
+    Page<Room> findAllByClassificationAndStatus(Classification classification, RoomStatus status, Pageable pageable);
 }
