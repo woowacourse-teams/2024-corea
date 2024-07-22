ef0f073788e5338d51861d8578aaa79a486ba926
최진실 <63334368+chlwlstlf@users.noreply.github.com>
2024-07-22 16:54:05 +0900

    [FE] refactor: 레이아웃 title, content 컴포넌트 리팩토링(#41) (#86)
    * refactor: 파일명 변경 후 코드 리팩토링
    
    * feat: ContentSection 스토리북 생성
    
    * refactor: 다른 컴포넌트에 ContentSection 적용

--- frontend/src/components/common/contentSection/ContentSection.style.ts
+++ frontend/src/components/common/contentSection/ContentSection.style.ts
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
-export const ContentFormContainer = styled.div`
+export const ContentSectionContainer = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 1rem;
 `;
 
-export const ContentFormTitle = styled.h2`
-  padding-bottom: 1rem;
+export const ContentSectionTitle = styled.h2`
   font: ${({ theme }) => theme.TEXT.large};
 `;
