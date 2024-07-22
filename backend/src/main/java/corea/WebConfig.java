5b194044b308a902dcb86047e71dc1a7554b01d5
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-07-22 14:32:55 +0900

    feat: 프론트-백 서버 주소 변경

--- backend/src/main/java/corea/WebConfig.java
+++ backend/src/main/java/corea/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3000);
