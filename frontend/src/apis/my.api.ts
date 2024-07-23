916f0125de10af353cd01eae319df00444d0c527
Lee sang Yeop <mma7710@naver.com>
2024-07-23 15:07:06 +0900

    refactor: API 요청을 요구사항에 맞춰 변경

--- frontend/src/apis/my.api.ts
+++ frontend/src/apis/my.api.ts
@@ -2,20 +2,20 @@ import { ReviewerInfo } from "./../@types/reviewer";
 import apiClient from "./apiClient";
 import { API_ENDPOINTS } from "./endpoints";
 
-export const getMyReviewers = async (): Promise<ReviewerInfo[]> => {
-  const res = await apiClient<ReviewerInfo[]>({
+export const getMyReviewers = async (roomId: number): Promise<ReviewerInfo[]> => {
+  const res = await apiClient<{ reviewInfo: ReviewerInfo[] }>({
     method: "get",
-    url: API_ENDPOINTS.REVIEWER,
+    url: API_ENDPOINTS.REVIEWERS(roomId),
   });
 
-  return res.data;
+  return res.data.reviewInfo;
 };
 
-export const getMyReviewees = async (): Promise<ReviewerInfo[]> => {
-  const res = await apiClient<ReviewerInfo[]>({
+export const getMyReviewees = async (roomId: number): Promise<ReviewerInfo[]> => {
+  const res = await apiClient<{ reviewInfo: ReviewerInfo[] }>({
     method: "get",
-    url: API_ENDPOINTS.REVIEWEE,
+    url: API_ENDPOINTS.REVIEWEES(roomId),
   });
 
-  return res.data;
+  return res.data.reviewInfo;
 };
