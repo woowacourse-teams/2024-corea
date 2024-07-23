117122d5a20a47f2d5b90e4a8bc30924cebb9a15
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2024-07-23 14:13:39 +0900

    [BE] 리뷰어, 리뷰이 조회 API 기능 구현 (#26, #34) (#91)
    * refactor: 도메인 수정
    
    * feat: 리뷰어, 리뷰이 조회 API 기능 구현
    
    * refactor: 중복된 기능 코드 제거
    
    * docs: 메서드 시그니쳐 통일
    
    * refactor: 리뷰 반영
    
    * refactor: 리뷰 재반영

---
+++ backend/src/main/java/corea/matching/dto/ReviewInfos.java
@@ -0,0 +1,6 @@
+package corea.matching.dto;
+
+import java.util.List;
+
+public record ReviewInfos(List<ReviewInfo> reviewInfos) {
+}
--- backend/src/test/java/corea/matching/service/MatchingServiceTest.java
+++ backend/src/test/java/corea/matching/service/MatchingServiceTest.java
@@ -13,8 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static corea.exception.ExceptionType.PARTICIPANT_SIZE_LACK;
+import static org.assertj.core.api.Assertions.*;
 
 @ServiceTest
 class MatchingServiceTest {
@@ -48,6 +48,9 @@ class MatchingServiceTest {
 
         assertThatThrownBy(() -> matchingService.matchMaking(participations, matchingSize))
                 .isInstanceOf(CoreaException.class)
-                .hasMessage("참여 인원 수가 부족합니다.");
+                .satisfies(exception -> {
+                    CoreaException coreaException = (CoreaException) exception;
+                    assertThat(coreaException.getExceptionType()).isEqualTo(PARTICIPANT_SIZE_LACK);
+                });
     }
 }
