39f21f296cdac050fdbd2c86888289c498b7cac5
gyungchan Jo <namejgc@naver.com>
2024-07-23 14:59:37 +0900

    fix: 충돌 해결

--- frontend/src/pages/roomDetail/RoomDetailPage.tsx
+++ frontend/src/pages/roomDetail/RoomDetailPage.tsx
@@ -1,4 +1,4 @@
-import ContentForm from "@/components/common/contentForm/ContentForm";
+import ContentSection from "@/components/common/contentSection/ContentSection";
 import MyReviewee from "@/components/roomDetailPage/myReviewee/MyReviewee";
 import MyReviewer from "@/components/roomDetailPage/myReviewer/MyReviewer";
 import RoomInfoCard from "@/components/roomDetailPage/roomInfoCard/RoomInfoCard";
@@ -7,16 +7,17 @@ import * as S from "@/pages/roomDetail/RoomDetailPage.style";
 const RoomDetailPage = () => {
   return (
     <S.Layout>
-      <ContentForm title="미션 정보">
+      <ContentSection title="미션 정보">
         <RoomInfoCard />
-      </ContentForm>
+      </ContentSection>
 
-      <ContentForm title="나의 리뷰어">
+      <ContentSection title="나의 리뷰어">
         <MyReviewer />
-      </ContentForm>
-      <ContentForm title="나의 리뷰이">
+      </ContentSection>
+
+      <ContentSection title="나의 리뷰이">
         <MyReviewee />
-      </ContentForm>
+      </ContentSection>
     </S.Layout>
   );
 };
