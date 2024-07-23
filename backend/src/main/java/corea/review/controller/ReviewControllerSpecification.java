18a591ee66950ff9f2f3b1cd1cee98750690bdbf
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-07-23 15:15:16 +0900

    feat: 컨트롤러 구현

---
+++ backend/src/main/java/corea/review/controller/ReviewControllerSpecification.java
@@ -0,0 +1,9 @@
+package corea.review.controller;
+
+import corea.auth.domain.AuthInfo;
+import corea.review.dto.ReviewRequest;
+import org.springframework.http.ResponseEntity;
+
+public interface ReviewControllerSpecification {
+    ResponseEntity<Void> complete(AuthInfo authInfo, ReviewRequest request);
+}
