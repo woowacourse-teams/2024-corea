6b4f0f12320a7e482136461799a01a1836938d94
jinsil <jinsil378900@gmail.com>
2024-07-24 22:07:46 +0900

    refactor: mock 데이터 사용

--- frontend/src/components/shared/roomCardModal/RoomCardModal.stories.tsx
+++ frontend/src/components/shared/roomCardModal/RoomCardModal.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import RoomCardModal from "@/components/shared/roomCardModal/RoomCardModal";
-import { RoomInfo } from "@/@types/roomInfo";
+import RoomInfo from "@/mocks/mockResponse/roomInfo.json";
 
 const meta: Meta<typeof RoomCardModal> = {
   title: "Shared/RoomCardModal",
@@ -31,29 +31,11 @@ export default meta;
 
 type Story = StoryObj<typeof RoomCardModal>;
 
-const sampleRoomInfo: RoomInfo = {
-  id: 1,
-  title: "자바 레이싱 카 - TDD",
-  content: "TDD를 배우고 싶은 자 나에게로",
-  matchingSize: 3,
-  repositoryLink: "https://github.com/example/java-racingcar",
-  thumbnailLink:
-    "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=13301655&filePath=L2Rpc2sxL25ld2RhdGEvMjAyMS8yMS9DTFMxMDAwNC8xMzMwMTY1NV9XUlRfMjFfQ0xTMTAwMDRfMjAyMTEyMTNfMQ==&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004",
-  keywords: ["TDD", "클린코드", "자바"],
-  currentParticipantSize: 15,
-  maximumParticipantSize: 20,
-  manager: "김코딩",
-  recruitmentDeadline: "2024-07-30T15:00",
-  reviewDeadline: "2024-08-10T23:59",
-  isParticipated: true,
-  isClosed: false,
-};
-
 export const Default: Story = {
   args: {
     isOpen: true,
     onClose: () => alert("Modal closed"),
-    roomInfo: sampleRoomInfo,
+    roomInfo: RoomInfo.roomInfo,
   },
   parameters: {
     docs: { disable: true },
@@ -64,7 +46,7 @@ export const SmallViewport: Story = {
   args: {
     isOpen: true,
     onClose: () => alert("Modal closed"),
-    roomInfo: sampleRoomInfo,
+    roomInfo: RoomInfo.roomInfo,
   },
   parameters: {
     viewport: {
@@ -78,7 +60,7 @@ export const MediumViewport: Story = {
   args: {
     isOpen: true,
     onClose: () => alert("Modal closed"),
-    roomInfo: sampleRoomInfo,
+    roomInfo: RoomInfo.roomInfo,
   },
   parameters: {
     viewport: {
@@ -92,7 +74,7 @@ export const LargeViewport: Story = {
   args: {
     isOpen: true,
     onClose: () => alert("Modal closed"),
-    roomInfo: sampleRoomInfo,
+    roomInfo: RoomInfo.roomInfo,
   },
   parameters: {
     viewport: {
