8cc941e5d43f754d14e70fb4352fbe730e1ff4da
jinsil <jinsil378900@gmail.com>
2024-07-24 22:34:50 +0900

    refactor: 불필요한 코드 제거

--- frontend/src/hooks/useModal.ts
+++ frontend/src/hooks/useModal.ts
@@ -3,16 +3,9 @@ import { useState } from "react";
 const useModal = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleOpenModal = () => {
-    console.log("모달 열기");
-    setIsOpen(true);
-  };
+  const handleOpenModal = () => setIsOpen(true);
 
-  const handleCloseModal = () => {
-    console.log("모달 닫기");
-
-    setIsOpen(false);
-  };
+  const handleCloseModal = () => setIsOpen(false);
 
   return { isOpen, handleOpenModal, handleCloseModal };
 };
