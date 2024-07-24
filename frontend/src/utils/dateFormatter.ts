c74a7e88a2ce645b7ebd684cb2df37cee9b374a7
jinsil <jinsil378900@gmail.com>
2024-07-24 17:34:12 +0900

    feat: 날짜 변경 함수 추가

--- frontend/src/utils/dateFormatter.ts
+++ frontend/src/utils/dateFormatter.ts
@@ -1,7 +1,17 @@
-export const formatDateString = (dateString: string): string => {
+// 마감일 포맷 함수
+export const formatDeadlineString = (dateString: string): string => {
   const [datePart, timePart] = dateString.split("T");
   const [year, month, day] = datePart.split("-");
   const [hours, minutes] = timePart.split(":");
 
   return `~ ${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분까지`;
+};
+
+// 일반 날짜 시간 포맷 함수
+export const formatDateTimeString = (dateString: string): string => {
+  const [datePart, timePart] = dateString.split("T");
+  const [year, month, day] = datePart.split("-");
+  const [hours, minutes] = timePart.split(":");
+
+  return `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`;
 };
