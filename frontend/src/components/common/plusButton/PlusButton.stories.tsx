e85c7facdeedbc682a71f8802413a003882d956f
00kang <kdb070970@gmail.com>
2024-07-24 17:32:20 +0900

    test: PlusButton 스토리북 수정

--- frontend/src/components/common/plusButton/PlusButton.stories.tsx
+++ frontend/src/components/common/plusButton/PlusButton.stories.tsx
@@ -12,6 +12,9 @@ const meta = {
       },
     },
   },
+  argTypes: {
+    onClick: { action: "clicked" },
+  },
 } satisfies Meta<typeof PlusButton>;
 
 export default meta;
@@ -19,5 +22,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  render: () => <PlusButton />,
+  args: {
+    onClick: () => {},
+  },
 };
