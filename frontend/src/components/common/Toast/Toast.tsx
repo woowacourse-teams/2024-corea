d7f083d030b9a41ac8f9227f7605fdf2336ee485
Lee sang Yeop <mma7710@naver.com>
2024-07-24 14:25:48 +0900

    feat: 토스트 모달 UI 생성

---
+++ frontend/src/components/common/Toast/Toast.tsx
@@ -0,0 +1,34 @@
+import * as S from "./Toast.style";
+import { useContext, useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+import { ToastContext } from "@/providers/ToastProvider";
+
+const Toast = () => {
+  const toastContainer = document.getElementById("toast");
+  const modalInfo = useContext(ToastContext);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (modalInfo.isOpen) {
+      setIsOpen(true);
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setIsOpen(false);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [modalInfo.isOpen]);
+
+  if (!toastContainer || !isOpen) {
+    return <></>;
+  }
+
+  return createPortal(
+    <S.Wrapper closeAnimation={!modalInfo.isOpen}>{modalInfo.message}</S.Wrapper>,
+    toastContainer,
+  );
+};
+
+export default Toast;
