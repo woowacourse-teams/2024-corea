d0b4c7ad3ea79190e668b472e81895b2967a81f4
00kang <kdb070970@gmail.com>
2024-07-25 12:59:32 +0900

    feat: MainPage에 MenuBar 추가

--- frontend/src/pages/main/MainPage.tsx
+++ frontend/src/pages/main/MainPage.tsx
@@ -1,12 +1,22 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import { useNextQuery } from "@/hooks/useNextQuery";
 import ContentSection from "@/components/common/contentSection/ContentSection";
+import MenuBar from "@/components/common/menuBar/MenuBar";
 import RoomList from "@/components/shared/roomList/RoomList";
 import * as S from "@/pages/main/MainPage.style";
 import QUERY_KEYS from "@/apis/queryKeys";
 import { getClosedRoomList, getOpenedRoomList, getParticipatedRoomList } from "@/apis/rooms.api";
 
 const MainPage = () => {
+  const [selectedCategory, setSelectedCategory] = useState(() => {
+    return localStorage.getItem("selectedCategory") || "all";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("selectedCategory", selectedCategory);
+  }, [selectedCategory]);
+
   const { data: participatedRoomList } = useQuery({
     queryKey: [QUERY_KEYS.PARTICIPATED_ROOM_LIST],
     queryFn: getParticipatedRoomList,
@@ -17,8 +27,9 @@ const MainPage = () => {
     fetchNextPage: fetchNextOpenedPage,
     hasNextPage: hasNextOpenedPage,
   } = useNextQuery({
-    queryKey: QUERY_KEYS.OPENED_ROOM_LIST,
+    queryKey: [QUERY_KEYS.OPENED_ROOM_LIST, selectedCategory],
     getRoomList: getOpenedRoomList,
+    classification: selectedCategory,
   });
 
   const {
@@ -26,12 +37,17 @@ const MainPage = () => {
     fetchNextPage: fetchNextClosedPage,
     hasNextPage: hasNextClosedPage,
   } = useNextQuery({
-    queryKey: QUERY_KEYS.CLOSED_ROOM_LIST,
+    queryKey: [QUERY_KEYS.CLOSED_ROOM_LIST, selectedCategory],
     getRoomList: getClosedRoomList,
+    classification: selectedCategory,
   });
 
   const openedRooms = openedRoomList?.pages.flatMap((page) => page.roomsInfo) || [];
   const closedRooms = closedRoomList?.pages.flatMap((page) => page.roomsInfo) || [];
+
+  const handleCategoryClick = (category: string) => {
+    setSelectedCategory(category);
+  };
 
   if (!participatedRoomList) return <></>;
 
@@ -40,6 +56,8 @@ const MainPage = () => {
       <ContentSection title="참여 중인 방 리스트">
         {participatedRoomList && <RoomList roomList={participatedRoomList.roomInfo} />}
       </ContentSection>
+
+      <MenuBar selectedCategory={selectedCategory} onCategoryClick={handleCategoryClick} />
 
       <ContentSection title="모집 중인 방 리스트">
         <RoomList
