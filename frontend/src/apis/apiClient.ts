2c01e2ba19f3eb7839a2e4602549583f0049e76a
Lee sang Yeop <mma7710@naver.com>
2024-07-25 11:41:05 +0900

    chore: 주석 제거

--- frontend/src/apis/apiClient.ts
+++ frontend/src/apis/apiClient.ts
@@ -7,7 +7,7 @@ const apiClient = axios.create({
 
 // api 요청하기 전 수행
 apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("user"); // 쿠키 이름을 지정합니다.
+  const token = localStorage.getItem("user");
 
   if (token) {
     config.headers["Authorization"] = token;
