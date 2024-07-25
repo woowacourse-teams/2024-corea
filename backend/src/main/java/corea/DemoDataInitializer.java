c63a248257016e2078efadf7f4471f8e2f037b6d
hjk0761 <hjk0761@naver.com>
2024-07-25 15:00:03 +0900

    refactor: 패키지 이동으로 인한 오류 수정

--- backend/src/main/java/corea/DemoDataInitializer.java
+++ backend/src/main/java/corea/DemoDataInitializer.java
@@ -1,11 +1,11 @@
 package corea;
 
 import corea.matching.domain.MatchResult;
-import corea.matching.domain.Participation;
 import corea.matching.repository.MatchResultRepository;
-import corea.matching.repository.ParticipationRepository;
 import corea.member.domain.Member;
 import corea.member.repository.MemberRepository;
+import corea.participation.domain.Participation;
+import corea.participation.repository.ParticipationRepository;
 import corea.room.domain.Classification;
 import corea.room.domain.Room;
 import corea.room.domain.RoomStatus;
