f753cff6cdb580729c93c274321c19ed9c007334
jinsil <jinsil378900@gmail.com>
2024-07-25 10:28:43 +0900

    refactor: 불필요한 import 제거

--- frontend/src/components/shared/roomList/RoomList.tsx
+++ frontend/src/components/shared/roomList/RoomList.tsx
@@ -1,5 +1,4 @@
-import { Link, Navigate } from "react-router-dom";
-import useModal from "@/hooks/useModal";
+import { Link } from "react-router-dom";
 import RoomCard from "@/components/shared/roomCard/RoomCard";
 import * as S from "@/components/shared/roomList/RoomList.style";
 import { RoomInfo } from "@/@types/roomInfo";
