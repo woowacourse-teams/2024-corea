b7fe83dec8345c11817533bf07a82ad9b26ab09a
hjk0761 <hjk0761@naver.com>
2024-07-25 15:23:54 +0900

    refactor: 방에 대한 설명 추가

--- backend/src/main/java/corea/DemoDataInitializer.java
+++ backend/src/main/java/corea/DemoDataInitializer.java
@@ -55,89 +55,94 @@ public class DemoDataInitializer implements ApplicationRunner {
                 new Member("pp449", "https://avatars.githubusercontent.com/u/71641127?v=4", "이상엽",
                         "darr@email.com", true, 4.8f));
 
+        // 이미 모집 완료되어 매칭까지 진행된 방
         Room room1 = roomRepository.save(
                 new Room("자바 레이싱 카 - MVC", "MVC 패턴을 아시나요?", 2,
                         "https://github.com/example/java-racingcar",
                         "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=13301655&filePath=L2Rpc2sxL25ld2RhdGEvMjAyMS8yMS9DTFMxMDAwNC8xMzMwMTY1NV9XUlRfMjFfQ0xTMTAwMDRfMjAyMTEyMTNfMQ==&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004",
                         List.of("MVC", "자바", "디자인패턴"), 5, 30, member1,
-                        LocalDateTime.now().minusDays(21),
                         LocalDateTime.now().minusDays(7),
+                        LocalDateTime.now().plusDays(7),
                         Classification.BACKEND, RoomStatus.CLOSED));
+
+        // member1 기준 아직 참여하지 않았고, 참여할 수 있는 방
         Room room2 = roomRepository.save(
                 new Room("자바 레이싱 카 - TDD", "애쉬와 함께하는 TDD", 3,
                         "https://github.com/example/java-racingcar",
                         "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=13301655&filePath=L2Rpc2sxL25ld2RhdGEvMjAyMS8yMS9DTFMxMDAwNC8xMzMwMTY1NV9XUlRfMjFfQ0xTMTAwMDRfMjAyMTEyMTNfMQ==&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004",
                         List.of("TDD", "클린코드", "자바"), 6, 30, member2,
-                        LocalDateTime.now().minusDays(3),
-                        LocalDateTime.now().plusDays(11),
+                        LocalDateTime.now().plusDays(3),
+                        LocalDateTime.now().plusDays(17),
                         Classification.BACKEND, RoomStatus.OPENED));
         Room room3 = roomRepository.save(
                 new Room("자바 로또 - 객체지향", "객체지향 한 접시", 5,
                         "https://github.com/example/java-lotto",
                         "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=13301655&filePath=L2Rpc2sxL25ld2RhdGEvMjAyMS8yMS9DTFMxMDAwNC8xMzMwMTY1NV9XUlRfMjFfQ0xTMTAwMDRfMjAyMTEyMTNfMQ==&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004",
                         List.of("객체지향", "자바"), 1, 20, member3,
-                        LocalDateTime.now().minusDays(2),
-                        LocalDateTime.now().plusDays(12),
+                        LocalDateTime.now().plusDays(2),
+                        LocalDateTime.now().plusDays(14),
                         Classification.BACKEND, RoomStatus.OPENED));
         Room room4 = roomRepository.save(
                 new Room("코틀린 숫자 야구", "코틀린 기초 같이 할 사람", 3,
                         "https://github.com/example/kotlin-baseball",
                         "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=13301655&filePath=L2Rpc2sxL25ld2RhdGEvMjAyMS8yMS9DTFMxMDAwNC8xMzMwMTY1NV9XUlRfMjFfQ0xTMTAwMDRfMjAyMTEyMTNfMQ==&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004",
                         List.of("코틀린"), 1, 20, member4,
-                        LocalDateTime.now().minusDays(4),
-                        LocalDateTime.now().plusDays(10),
+                        LocalDateTime.now().plusDays(2),
+                        LocalDateTime.now().plusDays(14),
                         Classification.ANDROID, RoomStatus.OPENED));
         Room room5 = roomRepository.save(
                 new Room("자바스크립트 크리스마스", "진짜 요구사항대로 구현하기!", 3,
                         "https://github.com/example/javascript-christmas",
                         "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=13301655&filePath=L2Rpc2sxL25ld2RhdGEvMjAyMS8yMS9DTFMxMDAwNC8xMzMwMTY1NV9XUlRfMjFfQ0xTMTAwMDRfMjAyMTEyMTNfMQ==&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004",
                         List.of("구현", "자바스크립트"), 1, 20, member5,
-                        LocalDateTime.now().minusDays(5),
-                        LocalDateTime.now().plusDays(9),
+                        LocalDateTime.now().plusDays(5),
+                        LocalDateTime.now().plusDays(19),
                         Classification.FRONTEND, RoomStatus.OPENED));
         Room room6 = roomRepository.save(
                 new Room("방 제목 6", "방 설명 6", 3,
                         null, null, List.of("TDD"),
                         1, 20, member6,
-                        LocalDateTime.now().minusDays(3),
-                        LocalDateTime.now().plusDays(11),
+                        LocalDateTime.now().plusDays(3),
+                        LocalDateTime.now().plusDays(17),
                         Classification.FRONTEND, RoomStatus.OPENED));
         Room room7 = roomRepository.save(
                 new Room("방 제목 7", "방 설명 7", 3,
                         null, null, List.of("TDD"),
                         1, 20, member7,
-                        LocalDateTime.now().minusDays(3),
-                        LocalDateTime.now().plusDays(11),
+                        LocalDateTime.now().plusDays(3),
+                        LocalDateTime.now().plusDays(17),
                         Classification.FRONTEND, RoomStatus.OPENED));
         roomRepository.save(
                 new Room("방 제목 8", "방 설명 8", 3,
                         null, null, List.of("TDD", "클린코드"),
                         1, 20, member1,
-                        LocalDateTime.now().minusDays(3),
-                        LocalDateTime.now().plusDays(11),
+                        LocalDateTime.now().plusDays(3),
+                        LocalDateTime.now().plusDays(17),
                         Classification.BACKEND, RoomStatus.OPENED));
         roomRepository.save(
                 new Room("방 제목 9", "방 설명 9", 3,
                         null, null, List.of("TDD"),
                         1, 20, member3,
-                        LocalDateTime.now().minusDays(3),
-                        LocalDateTime.now().plusDays(11),
+                        LocalDateTime.now().plusDays(3),
+                        LocalDateTime.now().plusDays(17),
                         Classification.ANDROID, RoomStatus.OPENED));
         roomRepository.save(
                 new Room("방 제목 10", "방 설명 10", 3,
                         null, null, List.of("TDD"),
                         1, 20, member5,
-                        LocalDateTime.now().minusDays(3),
-                        LocalDateTime.now().plusDays(11),
+                        LocalDateTime.now().plusDays(3),
+                        LocalDateTime.now().plusDays(17),
                         Classification.FRONTEND, RoomStatus.CLOSED));
 
+        //room1 에 참여한 참여자들
         participationRepository.save(new Participation(room1.getId(), member1.getId()));
         participationRepository.save(new Participation(room1.getId(), member2.getId()));
         participationRepository.save(new Participation(room1.getId(), member3.getId()));
         participationRepository.save(new Participation(room1.getId(), member4.getId()));
         participationRepository.save(new Participation(room1.getId(), member5.getId()));
 
-        participationRepository.save(new Participation(room2.getId(), member1.getId()));
+        //room2 에 참여한 참여자들
+        participationRepository.save(new Participation(room2.getId(), member6.getId()));
         participationRepository.save(new Participation(room2.getId(), member2.getId()));
         participationRepository.save(new Participation(room2.getId(), member3.getId()));
         participationRepository.save(new Participation(room2.getId(), member4.getId()));
@@ -153,6 +158,7 @@ public class DemoDataInitializer implements ApplicationRunner {
 
         participationRepository.save(new Participation(room7.getId(), member7.getId()));
 
+        //room1 에서 매칭된 결과
         matchResultRepository.save(new MatchResult(1L, member1, member2, "https://github.com/example/java-racingcar/pull/2"));
         matchResultRepository.save(new MatchResult(1L, member1, member3, "https://github.com/example/java-racingcar/pull/3"));
         matchResultRepository.save(new MatchResult(1L, member1, member4, "https://github.com/example/java-racingcar/pull/4"));
