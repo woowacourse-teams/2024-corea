594a3acde8c9067d0b6cf9a304c2fdf4d92f51fc
jinsil <jinsil378900@gmail.com>
2024-07-27 23:24:18 +0900

    feat: 실제 서버 연결하기

--- frontend/src/apis/rooms.api.ts
+++ frontend/src/apis/rooms.api.ts
@@ -1,45 +1,46 @@
 import apiClient from "./apiClient";
 import { API_ENDPOINTS } from "./endpoints";
 import { RoomInfo, RoomListInfo } from "@/@types/roomInfo";
+import MESSAGES from "@/constants/message";
 
 export const getParticipatedRoomList = async (): Promise<RoomListInfo> => {
-  const res = await apiClient({
-    method: "get",
-    url: API_ENDPOINTS.PARTICIPATED_ROOMS,
+  const res = await apiClient.get({
+    endpoint: API_ENDPOINTS.PARTICIPATED_ROOMS,
+    errorMessage: MESSAGES.ERROR.GET_PARTICIPATED_ROOM_LIST,
   });
 
-  return res.data;
+  return res;
 };
 
 export const getOpenedRoomList = async (
   classification: string,
   page: number,
 ): Promise<RoomListInfo> => {
-  const res = await apiClient({
-    method: "get",
-    url: `${API_ENDPOINTS.OPENED_ROOMS}?classification=${classification}&page=${page}`,
+  const res = await apiClient.get({
+    endpoint: `${API_ENDPOINTS.OPENED_ROOMS}?classification=${classification}&page=${page}`,
+    errorMessage: MESSAGES.ERROR.GET_OPENED_ROOM_LIST,
   });
 
-  return res.data;
+  return res;
 };
 
 export const getClosedRoomList = async (
   classification: string,
   page: number,
 ): Promise<RoomListInfo> => {
-  const res = await apiClient({
-    method: "get",
-    url: `${API_ENDPOINTS.CLOSED_ROOMS}?classification=${classification}&page=${page}`,
+  const res = await apiClient.get({
+    endpoint: `${API_ENDPOINTS.CLOSED_ROOMS}?classification=${classification}&page=${page}`,
+    errorMessage: MESSAGES.ERROR.GET_CLOSED_ROOM_LIST,
   });
 
-  return res.data;
+  return res;
 };
 
 export const getRoomDetailInfo = async (id: number): Promise<RoomInfo> => {
-  const res = await apiClient({
-    method: "get",
-    url: `${API_ENDPOINTS.ROOMS}/${id}`,
+  const res = await apiClient.get({
+    endpoint: `${API_ENDPOINTS.ROOMS}/${id}`,
+    errorMessage: MESSAGES.ERROR.GET_ROOM_DETAIL_INFO,
   });
 
-  return res.data;
+  return res;
 };
