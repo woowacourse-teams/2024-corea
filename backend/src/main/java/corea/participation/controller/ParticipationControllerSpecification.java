d3b1878f1df57c8010e1f52ccfe92242dc89218e
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-07-29 20:20:32 +0900

    feat: 명세 내 발생하는 에러코드 맞게 작성

--- backend/src/main/java/corea/participation/controller/ParticipationControllerSpecification.java
+++ backend/src/main/java/corea/participation/controller/ParticipationControllerSpecification.java
@@ -1,24 +1,15 @@
 package corea.participation.controller;
 
 import corea.auth.domain.AuthInfo;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import corea.exception.ExceptionType;
+import corea.exception.ExceptionTypeGroup;
+import corea.global.annotation.ApiErrorResponses;
 import org.springframework.http.ResponseEntity;
 
 public interface ParticipationControllerSpecification {
 
-    @ApiResponses(
-            value = {
-                    @ApiResponse(responseCode = "404", content = @Content(mediaType = "application/json", examples = {
-                            @ExampleObject(name = "해당하는 방이 없는 경우", value = """
-                                    {
-                                        "message": "1에 해당하는 방 없습니다."
-                                    }
-                                    """)
-                    })),
-            }
-    )
+    @ApiErrorResponses(value = {ExceptionType.MEMBER_NOT_FOUND, ExceptionType.ROOM_NOT_FOUND, ExceptionType.ALREADY_APPLY},
+            groups = ExceptionTypeGroup.INTERNAL_SERVER_ERROR)
     ResponseEntity<Void> participate(long id, AuthInfo authInfo);
 }
+
