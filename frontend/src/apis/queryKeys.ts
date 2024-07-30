214cbbb68c504e09422e2e67d873937156d54ad7
jinsil <jinsil378900@gmail.com>
2024-07-30 17:16:03 +0900

    chore: review.api를 reviews.api로 변경

--- frontend/src/apis/queryKeys.ts
+++ frontend/src/apis/queryKeys.ts
@@ -1,11 +1,11 @@
 const QUERY_KEYS = {
-  // 메인 페이지
+  // rooms
   PARTICIPATED_ROOM_LIST: "participatedRoomList",
   OPENED_ROOM_LIST: "openedRoomList",
   CLOSED_ROOM_LIST: "closedRoomList",
-
-  // 방 상세 페이지
   ROOM_DETAIL_INFO: "roomDetailInfo",
+
+  // reviews
   REVIEWERS: "reviewers",
   REVIEWEES: "reviewees",
 };
