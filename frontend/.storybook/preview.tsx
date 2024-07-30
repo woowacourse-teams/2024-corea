e8f4d463e24ffa0b86ab4e125f398b85106e2c7a
Lee sang Yeop <mma7710@naver.com>
2024-07-30 21:42:25 +0900

    [FE] 스토리북에서 MSW 적용(#72) (#165)
    * chore: msw-storybook-addon 라이브러리 추가
    
    * refactor: msw handler 들을 관리하는 handlers 추가
    
    * config: 스토리북에서도 MSW를 사용할 수 있도록 구성
    
    * chore: 불필요한 파일 제거

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -1,10 +1,14 @@
+import handlers from "../src/mocks/handlers";
 import { ToastProvider } from "../src/providers/ToastProvider";
 import GlobalStyles from "../src/styles/globalStyles";
 import { theme } from "../src/styles/theme";
 import type { Preview } from "@storybook/react";
+import { initialize, mswLoader } from "msw-storybook-addon";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+
+initialize();
 
 const preview: Preview = {
   parameters: {
@@ -14,7 +18,11 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    msw: {
+      handlers,
+    },
   },
+  loaders: [mswLoader],
 };
 
 export const decorators = [
