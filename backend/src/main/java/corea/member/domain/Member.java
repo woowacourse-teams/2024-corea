193d94f7f75562531ed58d00e6847c764b744b1a
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-07-30 14:26:37 +0900

    feat: 썸네일 링크 칼럼 길이 정의

--- backend/src/main/java/corea/member/domain/Member.java
+++ backend/src/main/java/corea/member/domain/Member.java
@@ -1,5 +1,6 @@
 package corea.member.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -22,6 +23,7 @@ public class Member {
 
     private String username;
 
+    @Column(length = 32768)
     private String thumbnailUrl;
 
     private String name;
