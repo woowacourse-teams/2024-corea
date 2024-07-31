2022fd4c012e915507f28cff7b3d2e7a6f32b5e4
jinsil <jinsil378900@gmail.com>
2024-07-31 16:54:54 +0900

    feat: textarea 스토리북 작성

---
+++ frontend/src/components/common/textarea/Textarea.stories.tsx
@@ -0,0 +1,62 @@
+import { Textarea } from "./Textarea";
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+
+const meta = {
+  title: "Common/Textarea",
+  component: Textarea,
+  argTypes: {
+    error: {
+      control: {
+        type: "boolean",
+      },
+      description: "text error 여부",
+      defaultValue: false,
+    },
+    placeholder: {
+      control: {
+        type: "text",
+      },
+      description: "placeholder",
+      defaultValue: "여기에 입력해주세요.",
+    },
+    rows: {
+      control: {
+        type: "number",
+      },
+      description: "textarea 줄 수",
+    },
+    defaultValue: {
+      control: {
+        type: "text",
+      },
+      description: "textarea의 value",
+    },
+  },
+} satisfies Meta<typeof Textarea>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    error: false,
+  },
+  render: (args) => <Textarea error={args.error} defaultValue="기본 Textarea" />,
+};
+
+export const 에러일_때: Story = {
+  args: {
+    error: true,
+  },
+  render: (args) => <Textarea error={args.error} placeholder="필수 입력" />,
+};
+
+export const Textarea_Row_변경: Story = {
+  args: {
+    error: false,
+    rows: 10,
+  },
+  render: (args) => <Textarea error={args.error} rows={args.rows} value={`${args.rows}줄짜리`} />,
+};
