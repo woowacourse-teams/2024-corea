9a55cda66043b03b2c152dde2e47e6f3d827c50b
jinsil <jinsil378900@gmail.com>
2024-08-01 15:42:06 +0900

    feat: 헤더에 네브바 추가

--- frontend/src/components/common/header/Header.tsx
+++ frontend/src/components/common/header/Header.tsx
@@ -1,32 +1,55 @@
-import { useNavigate } from "react-router-dom";
+import ProfileDropdown from "./ProfileDropdown";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import * as S from "@/components/common/header/Header.style";
 
-const Header = () => {
-  const navigate = useNavigate();
+const headerItems = [
+  {
+    name: "가이드",
+    path: "/guide",
+  },
+  {
+    name: "랭킹",
+    path: "/ranking",
+  },
+];
 
-  const headerItems = [
-    {
-      name: "가이드",
-      link: "/",
-    },
-    {
-      name: "랭킹",
-      link: "/",
-    },
-    {
-      name: "마이",
-      link: "/",
-    },
-  ];
+const Header = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const [isSelect, setIsSelect] = useState("");
+
+  const handlePage = (path: string, name: string) => {
+    setIsSelect(name);
+    navigate(path);
+  };
+
+  useEffect(() => {
+    const currentItem = headerItems.find((item) => item.path === pathname);
+    if (currentItem) {
+      setIsSelect(currentItem.name);
+    } else {
+      setIsSelect("");
+    }
+  }, [pathname, headerItems]);
 
   return (
     <S.HeaderContainer>
-      <S.HeaderLogo onClick={() => navigate("/")}>CoReA</S.HeaderLogo>
-      <S.HeaderList>
-        {headerItems.map((item) => (
-          <S.HeaderItem key={item.name}>{item.name}</S.HeaderItem>
-        ))}
-      </S.HeaderList>
+      <S.HeaderLogo onClick={() => handlePage("/", "")}>CoReA</S.HeaderLogo>
+      <S.HeaderNavBarContainer>
+        <S.HeaderList>
+          {headerItems.map((item) => (
+            <S.HeaderItem
+              key={item.name}
+              onClick={() => handlePage(item.path, item.name)}
+              className={isSelect === item.name ? "selected" : ""}
+            >
+              {item.name}
+            </S.HeaderItem>
+          ))}
+        </S.HeaderList>
+        <ProfileDropdown></ProfileDropdown>
+      </S.HeaderNavBarContainer>
     </S.HeaderContainer>
   );
 };
