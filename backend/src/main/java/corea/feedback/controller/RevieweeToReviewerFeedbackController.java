7413021d5af03371356559c8b3b9a146d8546925
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-08-01 17:56:56 +0900

    feat: 리뷰이 -> 리뷰어 피드백 명세에 맞게 구현

---
+++ backend/src/main/java/corea/feedback/controller/RevieweeToReviewerFeedbackController.java
@@ -0,0 +1,42 @@
+package corea.feedback.controller;
+
+import corea.auth.annotation.LoginMember;
+import corea.auth.domain.AuthInfo;
+import corea.feedback.dto.RevieweeToReviewerFeedbackRequest;
+import corea.feedback.dto.RevieweeToReviewerResponse;
+import corea.feedback.service.RevieweeToReviewerFeedbackService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rooms/{roomId}/reviewer/feedbacks")
+@RequiredArgsConstructor
+public class RevieweeToReviewerFeedbackController implements RevieweeToReviewerFeedbackControllerSpecification {
+
+    private final RevieweeToReviewerFeedbackService revieweeToReviewerFeedbackService;
+
+    @Override
+    @PostMapping
+    public ResponseEntity<Void> create(@PathVariable long roomId, @LoginMember AuthInfo authInfo, @RequestBody RevieweeToReviewerFeedbackRequest request) {
+        revieweeToReviewerFeedbackService.create(roomId, authInfo.getId(), request);
+        return ResponseEntity.ok()
+                .build();
+    }
+
+    @Override
+    @GetMapping
+    public ResponseEntity<RevieweeToReviewerResponse> revieweeToReviewer(@PathVariable long roomId, @RequestParam String username, @LoginMember AuthInfo authInfo) {
+        RevieweeToReviewerResponse response = revieweeToReviewerFeedbackService.findRevieweeToReviewer(roomId, authInfo.getId(), username);
+        return ResponseEntity.ok()
+                .body(response);
+    }
+
+    @Override
+    @PutMapping("/{feedbackId}")
+    public ResponseEntity<Void> update(@PathVariable long roomId, @PathVariable long feedbackId, @LoginMember AuthInfo authInfo, @RequestBody RevieweeToReviewerFeedbackRequest request) {
+        revieweeToReviewerFeedbackService.update(feedbackId, roomId, authInfo.getId(), request);
+        return ResponseEntity.ok()
+                .build();
+    }
+}
