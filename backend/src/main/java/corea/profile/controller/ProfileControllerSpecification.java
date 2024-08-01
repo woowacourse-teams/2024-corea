36a29f859a37949817502f968c2ed00fdd23c491
gyungchan Jo <namejgc@naver.com>
2024-08-01 14:47:22 +0900

    chore: 서버 예외 명세서 제거

--- backend/src/main/java/corea/profile/controller/ProfileControllerSpecification.java
+++ backend/src/main/java/corea/profile/controller/ProfileControllerSpecification.java
@@ -2,13 +2,12 @@ package corea.profile.controller;
 
 import corea.auth.domain.AuthInfo;
 import corea.exception.ExceptionType;
-import corea.exception.ExceptionTypeGroup;
 import corea.global.annotation.ApiErrorResponses;
 import corea.profile.dto.ProfileResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface ProfileControllerSpecification {
 
-    @ApiErrorResponses(value = {ExceptionType.MEMBER_NOT_FOUND}, groups = ExceptionTypeGroup.INTERNAL_SERVER_ERROR)
+    @ApiErrorResponses(value = ExceptionType.MEMBER_NOT_FOUND)
     ResponseEntity<ProfileResponse> profile(AuthInfo authInfo);
 }
