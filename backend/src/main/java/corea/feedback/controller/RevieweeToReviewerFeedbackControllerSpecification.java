b5dafd749a2e67bf3305c63c717303701bcca733
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-08-01 17:54:13 +0900

    feat: API 명세에 따른 명세서 작성/변경

---
+++ backend/src/main/java/corea/feedback/controller/RevieweeToReviewerFeedbackControllerSpecification.java
@@ -0,0 +1,19 @@
+package corea.feedback.controller;
+
+import corea.auth.domain.AuthInfo;
+import corea.exception.ExceptionType;
+import corea.feedback.dto.RevieweeToReviewerFeedbackRequest;
+import corea.feedback.dto.RevieweeToReviewerResponse;
+import corea.global.annotation.ApiErrorResponses;
+import org.springframework.http.ResponseEntity;
+
+public interface RevieweeToReviewerFeedbackControllerSpecification {
+    @ApiErrorResponses(value = {ExceptionType.ALREADY_COMPLETED_FEEDBACK, ExceptionType.NOT_MATCHED_MEMBER})
+    ResponseEntity<Void> create(long roomId, AuthInfo authInfo, RevieweeToReviewerFeedbackRequest request);
+
+    @ApiErrorResponses(value = {ExceptionType.FEEDBACK_NOT_FOUND})
+    ResponseEntity<RevieweeToReviewerResponse> revieweeToReviewer(long roomId, String username, AuthInfo authInfo);
+
+    @ApiErrorResponses(value = {ExceptionType.FEEDBACK_NOT_FOUND, ExceptionType.NOT_MATCHED_MEMBER})
+    ResponseEntity<Void> update(long roomId, long feedbackId, AuthInfo authInfo, RevieweeToReviewerFeedbackRequest request);
+}
--- backend/src/main/java/corea/feedback/controller/ReviewerToRevieweeControllerSpecification.java
+++ backend/src/main/java/corea/feedback/controller/ReviewerToRevieweeControllerSpecification.java
@@ -9,12 +9,12 @@ import org.springframework.http.ResponseEntity;
 
 public interface ReviewerToRevieweeControllerSpecification {
 
-    @ApiErrorResponses(value = {ExceptionType.NOT_MATCHED_MEMBER})
+    @ApiErrorResponses(value = {ExceptionType.ALREADY_COMPLETED_FEEDBACK, ExceptionType.NOT_MATCHED_MEMBER})
     ResponseEntity<Void> create(long roomId, AuthInfo authInfo, ReviewerToRevieweeRequest request);
 
     @ApiErrorResponses(value = {ExceptionType.FEEDBACK_NOT_FOUND})
     ResponseEntity<ReviewerToRevieweeResponse> reviewerToReviewee(long roomId, String username, AuthInfo authInfo);
 
-    @ApiErrorResponses(value = {ExceptionType.ALREADY_COMPLETED_FEEDBACK, ExceptionType.NOT_MATCHED_MEMBER})
+    @ApiErrorResponses(value = {ExceptionType.FEEDBACK_NOT_FOUND, ExceptionType.NOT_MATCHED_MEMBER})
     ResponseEntity<Void> update(long roomId, long feedbackId, AuthInfo authInfo, ReviewerToRevieweeRequest request);
 }
