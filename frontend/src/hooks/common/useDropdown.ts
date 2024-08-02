46bd691c81e972ecec4dcec17a84477b0be819e2
jinsil <jinsil378900@gmail.com>
2024-08-02 09:58:40 +0900

    refactor: 불필요한 코드 제거

--- frontend/src/hooks/common/useDropdown.ts
+++ frontend/src/hooks/common/useDropdown.ts
@@ -1,16 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 
-const isDescendant = (parent: Node, child: Node | null): boolean => {
-  let node = child;
-  while (node !== null) {
-    if (node === parent) {
-      return true;
-    }
-    node = node.parentNode;
-  }
-  return false;
-};
-
 const useDropdown = () => {
   const [isOpen, setIsOpen] = useState(false);
 
