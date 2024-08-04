cef2b0486c7a2a464307b87a2e11fabf943ba2f0
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-08-04 17:53:18 +0900

    feat: 여러개 맵에서 중복되지 않은 키 스트림 추출 기능 구현

---
+++ backend/src/main/java/corea/global/util/MapHandler.java
@@ -0,0 +1,18 @@
+package corea.global.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Stream;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MapHandler {
+    public static <T> Stream<T> extractDistinctKeyStreams(Map<? extends T, ?>... maps) {
+        return (Stream<T>) Arrays.stream(maps)
+                .flatMap(map -> map.keySet()
+                        .stream())
+                .distinct();
+    }
+}
