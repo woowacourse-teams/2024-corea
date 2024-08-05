4e42b68514cbe9eb35ae4f10a1c45fe2a201feda
Lee sang Yeop <mma7710@naver.com>
2024-08-05 17:46:42 +0900

    feat: ProfilePage 페이지 추가

--- frontend/src/pages/profile/ProfilePage.tsx
+++ frontend/src/pages/profile/ProfilePage.tsx
@@ -1,5 +1,30 @@
+import * as S from "./ProfilePage.style";
+import useFetchProfile from "@/hooks/queries/profile/useFetchProfile";
+import useFetchParticipatedRoomList from "@/hooks/queries/rooms/useFetchParticipatedRoomList";
+import ContentSection from "@/components/common/contentSection/ContentSection";
+import ProfileCard from "@/components/profile/profileCard/ProfileCard";
+import RoomList from "@/components/shared/roomList/RoomList";
+
 const ProfilePage = () => {
-  return <div>여기는 마이페이지</div>;
+  const { data: profileData } = useFetchProfile();
+  const { data: roomList } = useFetchParticipatedRoomList();
+
+  if (!profileData || !roomList) return;
+
+  const participatingRoomList = roomList.rooms.filter((room) => !room.isClosed);
+  const participatedRoomList = roomList.rooms.filter((room) => room.isClosed);
+
+  return (
+    <S.ProfilePageContainer>
+      <ProfileCard {...profileData} />
+      <ContentSection title="참여 중인 방 리스트">
+        <RoomList roomList={participatingRoomList} roomType="participated" />
+      </ContentSection>
+      <ContentSection title="참여 했던 방 리스트">
+        <RoomList roomList={participatedRoomList} roomType="participated" />
+      </ContentSection>
+    </S.ProfilePageContainer>
+  );
 };
 
 export default ProfilePage;
