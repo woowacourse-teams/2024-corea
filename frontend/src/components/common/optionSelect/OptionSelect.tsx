5a0ee999ae92d8d9a17a3f98c5e102f86eb8ba81
Lee sang Yeop <mma7710@naver.com>
2024-08-07 17:50:12 +0900

    feat: 옵션 선택하는 컴포넌트 추가

---
+++ frontend/src/components/common/optionSelect/OptionSelect.tsx
@@ -0,0 +1,26 @@
+import * as S from "./OptionSelect.style";
+import { NonEmptyArray } from "@/@types/NonEmptyArray";
+
+interface OptionSelect<T extends NonEmptyArray<string>> {
+  selected: T[number];
+  options: T;
+  handleSelectedOption: (option: T[number]) => void;
+}
+
+const OptionSelect = <T extends NonEmptyArray<string>>({
+  selected,
+  options,
+  handleSelectedOption,
+}: OptionSelect<T>) => {
+  return (
+    <S.OptionSelectContainer>
+      {options.map((option) => (
+        <S.Option $isSelected={option === selected} onClick={() => handleSelectedOption(option)}>
+          {option}
+        </S.Option>
+      ))}
+    </S.OptionSelectContainer>
+  );
+};
+
+export default OptionSelect;
