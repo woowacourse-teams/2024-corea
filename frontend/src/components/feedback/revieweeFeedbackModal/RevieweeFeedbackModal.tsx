910f57cece3dcedef015f0abde9f9e9000de6c12
00kang <kdb070970@gmail.com>
2024-08-08 13:54:05 +0900

    refactor: RevieweeFedbackModal 리팩터링

--- frontend/src/components/feedback/revieweeFeedbackModal/RevieweeFeedbackModal.tsx
+++ frontend/src/components/feedback/revieweeFeedbackModal/RevieweeFeedbackModal.tsx
@@ -1,20 +1,13 @@
-import React, { useEffect, useState } from "react";
-import useMutateFeedback from "@/hooks/mutations/useMutateFeedback";
-import { useFetchRevieweeFeedback } from "@/hooks/queries/useFetchFeedback";
+import React from "react";
+import { useRevieweeFeedbackForm } from "@/hooks/feedback/useRevieweeFeedbackForm";
 import Button from "@/components/common/button/Button";
 import Label from "@/components/common/label/Label";
 import Modal from "@/components/common/modal/Modal";
-import { Textarea } from "@/components/common/textarea/Textarea";
-import EvaluationPointBar from "@/components/feedback/evaluationPointBar/EvaluationPointBar";
-import OptionButton from "@/components/feedback/keywordOptionButton/KeywordOptionButton";
-import RecommendationPointBar from "@/components/feedback/recommendationPointBar/RecommendationPointBar";
+import RevieweeFeedbackForm from "@/components/feedback/feedbackForm/RevieweeFeedbackForm";
 import * as S from "@/components/feedback/revieweeFeedbackModal/RevieweeFeedbackModal.style";
-import { RevieweeFeedbackData } from "@/@types/feedback";
 import { ReviewerInfo } from "@/@types/reviewer";
 import { RoomInfo } from "@/@types/roomInfo";
 import { FeedbackModalType } from "@/utils/feedbackUtils";
-
-type RevieweeFeedbackForm = Omit<RevieweeFeedbackData, "feedbackId" | "revieweeId">;
 
 interface RevieweeFeedbackModalProps {
   isOpen: boolean;
@@ -25,93 +18,16 @@ interface RevieweeFeedbackModalProps {
   buttonText: string;
 }
 
-const initialFormState: RevieweeFeedbackData = {
-  feedbackId: 0,
-  revieweeId: 0,
-  evaluationPoint: 0,
-  feedbackKeywords: [],
-  feedbackText: "",
-  recommendationPoint: 0,
-};
-
-const RevieweeFeedbackModal = ({
+const RevieweeFeedbackModal: React.FC<RevieweeFeedbackModalProps> = ({
   isOpen,
   onClose,
   roomInfo,
   reviewee,
   modalType,
   buttonText,
-}: RevieweeFeedbackModalProps) => {
-  const {
-    data: feedbackData,
-    isLoading,
-    error,
-  } = useFetchRevieweeFeedback(roomInfo.id, reviewee.username);
-  const { postRevieweeFeedbackMutation, putRevieweeFeedbackMutation } = useMutateFeedback();
-
-  const [formState, setFormState] = useState<RevieweeFeedbackData>(initialFormState);
-
-  useEffect(() => {
-    if (isOpen) {
-      if (modalType === "create") {
-        setFormState({
-          ...initialFormState,
-          revieweeId: reviewee.userId,
-        });
-      } else if (feedbackData) {
-        setFormState(feedbackData);
-      }
-    }
-  }, [isOpen, modalType, feedbackData, reviewee.userId]);
-
-  const handleClose = () => {
-    setFormState(initialFormState);
-    onClose();
-  };
-
-  const isFormValid =
-    formState.evaluationPoint !== 0 &&
-    formState.feedbackKeywords.length > 0 &&
-    formState.recommendationPoint !== 0;
-
-  const handleChange = (
-    key: keyof RevieweeFeedbackForm,
-    value: RevieweeFeedbackForm[keyof RevieweeFeedbackForm],
-  ) => {
-    if (modalType === "view") return;
-    setFormState((prevState) => ({
-      ...prevState,
-      [key]: value,
-    }));
-  };
-
-  const handleSubmit = () => {
-    if (!isFormValid || modalType === "view") return;
-
-    const feedbackData = {
-      ...formState,
-    };
-
-    if (modalType === "create") {
-      postRevieweeFeedbackMutation.mutate(
-        { roomId: roomInfo.id, feedbackData },
-        {
-          onSuccess: () => {
-            handleClose();
-          },
-        },
-      );
-    } else if (modalType === "edit") {
-      putRevieweeFeedbackMutation.mutate(
-        { roomId: roomInfo.id, feedbackId: feedbackData.feedbackId, feedbackData },
-        {
-          onSuccess: () => {
-            handleClose();
-          },
-        },
-      );
-    }
-  };
+}) => {
+  const { formState, handleChange, isFormValid, handleSubmit, handleClose } =
+    useRevieweeFeedbackForm(roomInfo.id, reviewee.username, reviewee.userId, modalType, onClose);
 
   return (
     <Modal isOpen={isOpen} onClose={handleClose}>
@@ -128,47 +44,7 @@ const RevieweeFeedbackModal = ({
           ))}
         </S.Keywords>
 
-        <S.ItemContainer>
-          <S.ModalQuestion required>
-            리뷰이의 개발 역량 향상을 위해 코드를 평가 해주세요.
-          </S.ModalQuestion>
-          <EvaluationPointBar
-            initialOptionId={formState.evaluationPoint}
-            onChange={(value) => handleChange("evaluationPoint", value)}
-            readonly={modalType === "view"}
-          />
-        </S.ItemContainer>
-
-        <S.ItemContainer>
-          <S.ModalQuestion required>어떤 점이 만족스러웠나요?</S.ModalQuestion>
-          <OptionButton
-            initialOptions={formState.feedbackKeywords}
-            onChange={(value) => handleChange("feedbackKeywords", value)}
-            selectedEvaluationId={formState.evaluationPoint}
-            readonly={modalType === "view"}
-          />
-        </S.ItemContainer>
-
-        <S.ItemContainer>
-          <S.ModalQuestion>추가적으로 하고 싶은 피드백이 있다면 남겨 주세요.</S.ModalQuestion>
-          <Textarea
-            rows={5}
-            maxLength={512}
-            placeholder="상대 리뷰이의 개발 역량 향상을 위해 피드백을 남겨주세요."
-            value={formState.feedbackText}
-            onChange={(e) => handleChange("feedbackText", e.target.value)}
-            readOnly={modalType === "view"}
-          />
-        </S.ItemContainer>
-
-        <S.ItemContainer>
-          <S.ModalQuestion required>리뷰이의 코드를 추천하시나요?</S.ModalQuestion>
-          <RecommendationPointBar
-            initialOptionId={formState.recommendationPoint}
-            onChange={(value) => handleChange("recommendationPoint", value)}
-            readonly={modalType === "view"}
-          />
-        </S.ItemContainer>
+        <RevieweeFeedbackForm formState={formState} onChange={handleChange} modalType={modalType} />
 
         {modalType !== "view" && (
           <S.ButtonWrapper>
