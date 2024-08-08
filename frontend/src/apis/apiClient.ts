94fc352b5f341c4f9b788a3598914451bac569b0
jinsil <jinsil378900@gmail.com>
2024-08-08 16:09:33 +0900

    feat: apiClient에 토큰 갱신 코드 추가

--- frontend/src/apis/apiClient.ts
+++ frontend/src/apis/apiClient.ts
@@ -1,3 +1,4 @@
+import { API_ENDPOINTS } from "./endpoints";
 import { serverUrl } from "@/config/serverUrl";
 import MESSAGES from "@/constants/message";
 
@@ -14,18 +15,72 @@ interface RequestProps extends ApiProps {
   method: Method;
 }
 
+const refreshToken = localStorage.getItem("refreshToken");
+
+let isRefreshing = false;
+let failedQueue: any[] = [];
+
+const processQueue = (error: any, token: string | null = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+
+  failedQueue = [];
+};
+
+const refreshAccessToken = async (): Promise<string | void> => {
+  try {
+    const response = await fetch(`${serverUrl}${API_ENDPOINTS.REFRESH}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ refreshToken }),
+    });
+
+    if (!response.ok) {
+      const error = new Error("Failed to refresh token");
+      processQueue(error, null);
+      throw error;
+    }
+
+    const authHeader = response.headers.get("Authorization");
+    const newAccessToken = authHeader?.split(" ")[1];
+
+    if (!newAccessToken) {
+      const error = new Error("Authorization header is missing");
+      processQueue(error, null);
+      throw error;
+    }
+
+    localStorage.setItem("accessToken", newAccessToken);
+    processQueue(null, newAccessToken);
+    isRefreshing = false;
+
+    return newAccessToken;
+  } catch (e) {
+    isRefreshing = false;
+    failedQueue = [];
+    localStorage.clear();
+  }
+};
+
 const createRequestInit = (
   method: Method,
   headers: Record<string, string>,
   body: object | null,
 ): RequestInit => {
-  const token = localStorage.getItem("user");
+  const token = localStorage.getItem("accessToken");
 
   return {
     method,
     headers: {
       ...headers,
-      Authorization: token || "",
+      Authorization: token ? `Bearer ${token}` : "",
       "Content-Type": "application/json",
     },
     body: body ? JSON.stringify(body) : null,
@@ -36,36 +91,72 @@ const fetchWithErrorHandling = async (
   endpoint: string,
   requestInit: RequestInit,
   errorMessage: string = "",
-) => {
-  // 오프라인 확인
+): Promise<any> => {
   if (!navigator.onLine) {
     throw new Error(MESSAGES.ERROR.OFFLINE);
   }
 
   const response = await fetch(`${serverUrl}${endpoint}`, requestInit);
 
+  if (response.status === 401) {
+    if (isRefreshing) {
+      return new Promise((resolve, reject) => {
+        failedQueue.push({ resolve, reject });
+        console.log("Queue length", failedQueue.length);
+      })
+        .then((token) => {
+          requestInit.headers = {
+            ...requestInit.headers,
+            Authorization: `Bearer ${token}`,
+          };
+          return fetch(`${serverUrl}${endpoint}`, requestInit);
+        })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(errorMessage || `Error: ${response.status} ${response.statusText}`);
+          }
+          return response.json();
+        });
+    }
+
+    isRefreshing = true;
+    return refreshAccessToken()
+      .then((newAccessToken) => {
+        requestInit.headers = {
+          ...requestInit.headers,
+          Authorization: `Bearer ${newAccessToken}`,
+        };
+        return fetch(`${serverUrl}${endpoint}`, requestInit);
+      })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(errorMessage || `Error: ${response.status} ${response.statusText}`);
+        }
+        return response.json();
+      });
+  }
+
   if (!response.ok) {
     throw new Error(errorMessage || `Error: ${response.status} ${response.statusText}`);
   }
 
   const text = await response.text();
-
   return text ? JSON.parse(text) : response;
 };
 
 const apiClient = {
-  get: ({ endpoint, headers = {}, errorMessage = "" }: ApiProps) => {
-    return apiClient.request({ method: "GET", endpoint, headers, errorMessage });
-  },
-  post: ({ endpoint, headers = {}, body = {}, errorMessage = "" }: ApiProps) => {
-    return apiClient.request({ method: "POST", endpoint, headers, body, errorMessage });
-  },
-  patch: ({ endpoint, headers = {}, body = {}, errorMessage = "" }: ApiProps) => {
-    return apiClient.request({ method: "PATCH", endpoint, headers, body, errorMessage });
-  },
-  delete: ({ endpoint, headers = {}, errorMessage = "" }: ApiProps) => {
-    return apiClient.request({ method: "DELETE", endpoint, headers, errorMessage });
-  },
+  get: ({ endpoint, headers = {}, errorMessage = "" }: ApiProps) =>
+    apiClient.request({ method: "GET", endpoint, headers, errorMessage }),
+
+  post: ({ endpoint, headers = {}, body = {}, errorMessage = "" }: ApiProps) =>
+    apiClient.request({ method: "POST", endpoint, headers, body, errorMessage }),
+
+  patch: ({ endpoint, headers = {}, body = {}, errorMessage = "" }: ApiProps) =>
+    apiClient.request({ method: "PATCH", endpoint, headers, body, errorMessage }),
+
+  delete: ({ endpoint, headers = {}, errorMessage = "" }: ApiProps) =>
+    apiClient.request({ method: "DELETE", endpoint, headers, errorMessage }),
+
   request: async ({
     method,
     endpoint,
