34706929eec6c2e18eef975c718929cd8e3e520d
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-08-08 14:57:43 +0900

    feat: 로직 내 debug 로그 명시

--- backend/src/main/java/corea/feedback/service/SocialFeedbackService.java
+++ backend/src/main/java/corea/feedback/service/SocialFeedbackService.java
@@ -9,6 +9,8 @@ import corea.feedback.repository.SocialFeedbackRepository;
 import corea.matching.domain.MatchResult;
 import corea.matching.repository.MatchResultRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,12 +19,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class SocialFeedbackService {
 
+    private static final Logger log = LogManager.getLogger(SocialFeedbackService.class);
     private final SocialFeedbackRepository socialFeedbackRepository;
     private final MatchResultRepository matchResultRepository;
 
     @Transactional
     public SocialFeedbackResponse create(long roomId, long deliverId, SocialFeedbackRequest request) {
         validateAlreadyExist(roomId, deliverId, request.receiverId());
+        log.debug("소설 피드백 작성[작성자({}), 요청값({})",deliverId,request);
         SocialFeedback socialFeedback = socialFeedbackRepository.save(createEntity(roomId, deliverId, request));
         return SocialFeedbackResponse.of(socialFeedback);
     }
@@ -30,6 +34,8 @@ public class SocialFeedbackService {
     @Transactional
     public SocialFeedbackResponse update(long feedbackId, long roomId, long deliverId, SocialFeedbackRequest request) {
         validateNotExist(feedbackId);
+
+        log.debug("소설 피드백 업데이트[작성자({}), 피드백 ID({}), 요청값({})",deliverId,feedbackId,request);
         SocialFeedback socialFeedback = socialFeedbackRepository.save(createEntity(feedbackId, roomId, deliverId, request));
         return SocialFeedbackResponse.of(socialFeedback);
     }
