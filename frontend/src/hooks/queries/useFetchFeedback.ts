f0b103f0d620cbe2941c4d06177cd99c7e18d033
00kang <kdb070970@gmail.com>
2024-08-08 16:07:59 +0900

    feat: 리뷰어피드백 관련 커스텀 훅 정의 (useFetchReviewerFeedback, postReviewerFeedbackMutation, putReviewerFeedbackMutation)

--- frontend/src/hooks/queries/useFetchFeedback.ts
+++ frontend/src/hooks/queries/useFetchFeedback.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getRevieweeFeedback } from "@/apis/feedback.api";
+import { getRevieweeFeedback, getReviewerFeedback } from "@/apis/feedback.api";
 import QUERY_KEYS from "@/apis/queryKeys";
 
 // 리뷰어 -> 리뷰이
@@ -7,6 +7,15 @@ export const useFetchRevieweeFeedback = (roomId: number, username: string) => {
   return useQuery({
     queryKey: [QUERY_KEYS.REVIEWEE_FEEDBACK, roomId.toString(), username],
     queryFn: () => getRevieweeFeedback(roomId, username),
+    enabled: !!roomId && !!username,
+  });
+};
+
+// 리뷰이 -> 리뷰어
+export const useFetchReviewerFeedback = (roomId: number, username: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.REVIEWER_FEEDBACK, roomId.toString(), username],
+    queryFn: () => getReviewerFeedback(roomId, username),
     enabled: !!roomId && !!username,
   });
 };
