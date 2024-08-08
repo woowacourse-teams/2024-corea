5fc50f266f6bf162e1b045adb6b917886cff8d55
00kang <kdb070970@gmail.com>
2024-08-08 16:06:38 +0900

    feat: 리뷰이 -> 리뷰어 api 정의 (get, post, put)

--- frontend/src/apis/feedback.api.ts
+++ frontend/src/apis/feedback.api.ts
@@ -1,4 +1,4 @@
-import { RevieweeFeedbackData } from "@/@types/feedback";
+import { RevieweeFeedbackData, ReviewerFeedbackData } from "@/@types/feedback";
 import apiClient from "@/apis/apiClient";
 import { API_ENDPOINTS } from "@/apis/endpoints";
 import MESSAGES from "@/constants/message";
@@ -38,6 +38,46 @@ export const putRevieweeFeedback = async (
     endpoint: API_ENDPOINTS.PUT_REVIEWEE_FEEDBACK(roomId, feedbackId),
     body: feedbackData,
     errorMessage: MESSAGES.ERROR.PUT_REVIEWEE_FEEDBACK,
+  });
+
+  return res;
+};
+
+// 리뷰이 -> 리뷰어
+export const getReviewerFeedback = async (
+  roomId: number,
+  username: string,
+): Promise<RevieweeFeedbackData> => {
+  const res = await apiClient.get({
+    endpoint: `${API_ENDPOINTS.REVIEWER_FEEDBACK(roomId)}?username=${username}`,
+    errorMessage: MESSAGES.ERROR.GET_REVIEWER_FEEDBACK,
+  });
+
+  return res;
+};
+
+export const postReviewerFeedback = async (
+  roomId: number,
+  feedbackData: Omit<ReviewerFeedbackData, "feedbackId">,
+) => {
+  const res = await apiClient.post({
+    endpoint: API_ENDPOINTS.REVIEWER_FEEDBACK(roomId),
+    body: feedbackData,
+    errorMessage: MESSAGES.ERROR.POST_REVIEWER_FEEDBACK,
+  });
+
+  return res;
+};
+
+export const putReviewerFeedback = async (
+  roomId: number,
+  feedbackId: number,
+  feedbackData: Omit<ReviewerFeedbackData, "feedbackId">,
+) => {
+  const res = await apiClient.put({
+    endpoint: API_ENDPOINTS.PUT_REVIEWER_FEEDBACK(roomId, feedbackId),
+    body: feedbackData,
+    errorMessage: MESSAGES.ERROR.PUT_REVIEWER_FEEDBACK,
   });
 
   return res;
