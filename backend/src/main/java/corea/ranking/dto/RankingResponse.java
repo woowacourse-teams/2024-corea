1d43d181079e6d14e8cc477d465320d953fcff35
ashsty <qkralsdk991@naver.com>
2024-08-12 17:25:23 +0900

    feat: 랭킹 API 수정(프론트 요청)

--- backend/src/main/java/corea/ranking/dto/RankingResponse.java
+++ backend/src/main/java/corea/ranking/dto/RankingResponse.java
@@ -5,8 +5,14 @@ import corea.member.domain.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "랭킹 정보 응답")
-public record RankingResponse(@Schema(description = "아이디", example = "ashsty")
+public record RankingResponse(@Schema(description = "랭킹 순위", example = "1")
+                              int rank,
+
+                              @Schema(description = "아이디", example = "ashsty")
                               String nickname,
+
+                              @Schema(description = "프로필 이미지", example = "www.profileImage.com")
+                              String profileImage,
 
                               @Schema(description = "깃허브 PR 링크", example = "www.github.com/sample/pull/24")
                               String githubLink,
@@ -21,9 +27,11 @@ public record RankingResponse(@Schema(description = "아이디", example = "ashs
                               String classification
 ) {
 
-    public static RankingResponse of(Member member, long deliverCount, float averageEvaluatePoint, EvaluateClassification classification) {
+    public static RankingResponse of(int rank, Member member, long deliverCount, float averageEvaluatePoint, EvaluateClassification classification) {
         return new RankingResponse(
+                rank,
                 member.getUsername(),
+                member.getThumbnailUrl(),
                 member.getProfileLink(),
                 deliverCount,
                 averageEvaluatePoint,
