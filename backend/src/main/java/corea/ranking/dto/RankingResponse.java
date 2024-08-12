0fa280c485bb87c02b135032eccb3827d95b7127
ashsty <qkralsdk991@naver.com>
2024-08-12 16:23:59 +0900

    docs: API 명세 작성

--- backend/src/main/java/corea/ranking/dto/RankingResponse.java
+++ backend/src/main/java/corea/ranking/dto/RankingResponse.java
@@ -2,13 +2,23 @@ package corea.ranking.dto;
 
 import corea.evaluation.domain.EvaluateClassification;
 import corea.member.domain.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record RankingResponse(
-        String nickname,
-        String githubLink,
-        long givenReviewCount,
-        float averageRating,
-        String classification
+@Schema(description = "랭킹 정보 응답")
+public record RankingResponse(@Schema(description = "아이디", example = "ashsty")
+                              String nickname,
+
+                              @Schema(description = "깃허브 PR 링크", example = "www.github.com/sample/pull/24")
+                              String githubLink,
+
+                              @Schema(description = "리뷰 개수", example = "17")
+                              long givenReviewCount,
+
+                              @Schema(description = "평균 별점", example = "4.9")
+                              float averageRating,
+
+                              @Schema(description = "개발 분야", example = "be")
+                              String classification
 ) {
 
     public static RankingResponse of(Member member, long deliverCount, float averageEvaluatePoint, EvaluateClassification classification) {
