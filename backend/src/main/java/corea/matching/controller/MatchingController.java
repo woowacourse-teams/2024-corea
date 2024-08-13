510a2f290dab676d0f0bbda3494adb200ba701a4
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-08-13 22:28:23 +0900

    feat: 매칭 기능 컨트롤러 분리

---
+++ backend/src/main/java/corea/matching/controller/MatchingController.java
@@ -0,0 +1,32 @@
+package corea.matching.controller;
+
+import corea.auth.annotation.LoginMember;
+import corea.auth.domain.AuthInfo;
+import corea.matching.domain.PullRequestInfo;
+import corea.matching.service.MatchingService;
+import corea.matching.service.PullRequestProvider;
+import corea.room.dto.RoomResponse;
+import corea.room.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MatchingController {
+
+    MatchingService matchingService;
+    RoomService roomService;
+    PullRequestProvider pullRequestProvider;
+
+    @PostMapping("/rooms/{id}/matching")
+    public ResponseEntity<Void> matching(@PathVariable long id, @LoginMember AuthInfo authInfo) {
+        RoomResponse response = roomService.getRoomById(id);
+        PullRequestInfo pullRequestInfo = pullRequestProvider.getUntilDeadline(response.repositoryLink(), response.recruitmentDeadline());
+        matchingService.match(id, pullRequestInfo);
+        return ResponseEntity.ok()
+                .build();
+    }
+}
