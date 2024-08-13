a9a95e29e3d891b35afcd68338a3f66c2e778def
jinsil <jinsil378900@gmail.com>
2024-08-13 19:10:15 +0900

    feat: RankingPage에서 RankingCard 호출

--- frontend/src/pages/ranking/RankingPage.tsx
+++ frontend/src/pages/ranking/RankingPage.tsx
@@ -1,5 +1,34 @@
+import { useFetchRanking } from "@/hooks/queries/useFetchRanking";
+import RankingCard from "@/components/ranking/RankingCard/RankingCard";
+import * as S from "@/pages/ranking/RankingPage.style";
+import { RankingClassification } from "@/@types/ranking";
+
+const rankingTypes = [
+  { classification: "re", title: "평점 좋은 리뷰어 랭킹" },
+  { classification: "an", title: "[Android] 코드 랭킹" },
+  { classification: "be", title: "[Backend] 코드 랭킹" },
+  { classification: "fe", title: "[Frontend] 코드 랭킹" },
+];
+
 const RankingPage = () => {
-  return <div>여기는 랭킹페이지</div>;
+  const { data: rankingAllData } = useFetchRanking();
+
+  if (!rankingAllData) return null;
+
+  return (
+    <S.RankingPageLayout>
+      {rankingTypes.map(
+        ({ classification, title }) =>
+          rankingAllData[classification as RankingClassification] && (
+            <RankingCard
+              key={classification}
+              title={title}
+              rankingData={rankingAllData[classification as RankingClassification]}
+            />
+          ),
+      )}
+    </S.RankingPageLayout>
+  );
 };
 
 export default RankingPage;
