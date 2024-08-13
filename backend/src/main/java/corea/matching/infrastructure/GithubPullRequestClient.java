f40df1b232cbbf62a60ef5440794656844b085e9
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-08-13 22:11:39 +0900

    feat: 깃허브에 PullRequest 요청 보내는 Client 구현

---
+++ backend/src/main/java/corea/matching/infrastructure/GithubPullRequestClient.java
@@ -0,0 +1,37 @@
+package corea.matching.infrastructure;
+
+import corea.auth.infrastructure.GithubProperties;
+import corea.matching.infrastructure.dto.PullRequestData;
+import corea.matching.infrastructure.dto.PullRequestResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@EnableConfigurationProperties(GithubProperties.class)
+@Component
+@RequiredArgsConstructor
+public class GithubPullRequestClient {
+
+    private static final String PULLS_LINK = "/pulls";
+    private static final String PER_PAGE_QUERY = "per_page=";
+    private static final String PAGE = "page=";
+    private final RestClient restClient;
+
+
+    public PullRequestData getPullRequestListWithPageNumber(String repositoryLink, int perPageSize, int pageNumber) {
+        PullRequestResponse[] response = restClient.get()
+                .uri(repositoryLink + PULLS_LINK + "?" + PER_PAGE_QUERY + perPageSize + "&" + PAGE + pageNumber)
+                .accept(APPLICATION_JSON)
+                .retrieve()
+                .body(PullRequestResponse[].class);
+
+        return new PullRequestData(isLastPage(response), response);
+    }
+
+    private boolean isLastPage(PullRequestResponse[] responses) {
+        return responses.length == 0;
+    }
+}
---
+++ backend/src/main/java/corea/matching/infrastructure/dto/PullRequestResponse.java
@@ -0,0 +1,21 @@
+package corea.matching.infrastructure.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public record PullRequestResponse(
+        String pullRequestLink,
+        GithubUserResponse user,
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+        LocalDateTime created_at
+) {
+
+    public String getUserId() {
+        return this.user.id();
+    }
+
+    public boolean isAfter(LocalDateTime localDateTime) {
+        return this.created_at.isAfter(localDateTime);
+    }
+}
