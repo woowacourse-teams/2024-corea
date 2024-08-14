14cc8e32aab1cd23c203a5c95864ce80fb4782ea
Lee sang Yeop <mma7710@naver.com>
2024-08-14 17:54:04 +0900

    feat: 로딩이 필요한 query 를 useSuspenseQuery 로 변경

--- frontend/src/hooks/queries/useFetchRooms.ts
+++ frontend/src/hooks/queries/useFetchRooms.ts
@@ -1,11 +1,11 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useSuspenseInfiniteQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 import { RoomListInfo } from "@/@types/roomInfo";
 import QUERY_KEYS from "@/apis/queryKeys";
 import { getParticipatedRoomList } from "@/apis/rooms.api";
 
 export const useFetchParticipatedRoomList = () => {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: [QUERY_KEYS.PARTICIPATED_ROOM_LIST],
     queryFn: getParticipatedRoomList,
     networkMode: "always",
@@ -26,7 +26,7 @@ export const useInfiniteFetchRoomList = ({
 }: RoomListQueryProps) => {
   const [searchParams] = useSearchParams();
 
-  return useInfiniteQuery({
+  return useSuspenseInfiniteQuery({
     queryKey,
     queryFn: ({ pageParam = 0 }) => {
       return getRoomList(classification, pageParam);
