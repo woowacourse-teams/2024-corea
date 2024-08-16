346fc24bfc9a9e8d02dbca6b9df9b38c6121601d
jinsil <jinsil378900@gmail.com>
2024-08-16 15:04:03 +0900

    fix: 컨텐트가 스크롤바 넘어가지 않게 하기

--- frontend/src/components/layout/Layout.style.ts
+++ frontend/src/components/layout/Layout.style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ContentContainer = styled.section`
-  width: 100vw;
+  width: 100%;
   min-width: 375px;
   max-width: 1200px;
   margin: 2rem auto;
