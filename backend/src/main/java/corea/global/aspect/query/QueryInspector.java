faab187750c585b2aafae0ef5eef2f86872e799c
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-08-18 17:32:04 +0900

    feat: 쿼리 정보를 가져와서 저장하는 기능 구현

---
+++ backend/src/main/java/corea/global/aspect/query/QueryInspector.java
@@ -0,0 +1,35 @@
+package corea.global.aspect.query;
+
+import org.hibernate.resource.jdbc.spi.StatementInspector;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@Component
+public class QueryInspector implements StatementInspector {
+
+    private final transient QueryInfo queryInfo;
+    private static final String FLAG_PROFILE = "inspect";
+    private final boolean flag;
+
+    public QueryInspector(final QueryInfo apiQueryCounter, final Environment environment) {
+        this.queryInfo = apiQueryCounter;
+        this.flag = Arrays.stream(environment.getActiveProfiles())
+                .anyMatch(profile -> profile.equals(FLAG_PROFILE));
+    }
+
+    @Override
+    public String inspect(final String sql) {
+        if (isInRequestScope() || flag) {
+            queryInfo.increaseQueryCount(sql);
+        }
+        return sql;
+    }
+
+    private boolean isInRequestScope() {
+        return Objects.nonNull(RequestContextHolder.getRequestAttributes());
+    }
+}
