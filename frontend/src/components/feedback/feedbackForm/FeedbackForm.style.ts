a2e8faa1c41184bcb71c7ccb77b17dfe8beb67a4
00kang <kdb070970@gmail.com>
2024-08-20 00:58:28 +0900

    feat: * 에서 *필수입력 으로 명시적 표현 수정

--- frontend/src/components/feedback/feedbackForm/FeedbackForm.style.ts
+++ frontend/src/components/feedback/feedbackForm/FeedbackForm.style.ts
@@ -13,6 +13,7 @@ export const ItemContainer = styled.div`
 export const ModalQuestion = styled.p<ModalQuestionProps>`
   display: flex;
   flex-direction: row;
+  align-items: center;
 
   font: ${({ theme }) => theme.TEXT.small};
   font-weight: 600;
@@ -22,7 +23,9 @@ export const ModalQuestion = styled.p<ModalQuestionProps>`
     required &&
     `
     &::after {
-      content: "*";
+      content: "*필수입력";
+      font: ${theme.TEXT.semiSmall};
+      font-weight: 400;
       color: ${theme.COLOR.error};
       margin-left: 4px;
     }
