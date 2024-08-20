b3c4335de5d919e61205f146d7f22c4d88d2f863
강다빈 <70834044+00kang@users.noreply.github.com>
2024-08-20 13:51:39 +0900

    [FE] 방 상세 페이지 디자인 수정(#313) (#321)
    * feat: 공통 Button 컴포넌트에 xSmall 사이즈 속성 추가
    
    * feat: 공통 Label 컴포넌트에 배경색 props로 받도록 수정
    
    * feat: 디데이 포맷팅 유틸함수 추가
    
    * design: theme.COLOR의 error 속성에 # 추가
    
    * design: 피드백 부분 레이아웃 수정
    
    * feat: 방 상세 정보카드에 디데이 추가, 반응형 디자인 대응하도록 수정
    
    * design: MyReviewer 컴포넌트 반응형 디자인 대응, 버튼 컬러 수정
    
    * design: MyReviewee 컴포넌트 반응형 디자인 대응, 버튼 컬러 수정
    
    * feat: * 에서 *필수입력 으로 명시적 표현 수정
    
    * feat: KeywordOptionButton 컴포넌트 props로 색 받도록 수정
    
    * design: 피드백 모달 내의 키워드 라벨 배경색 추가
    
    * refactor: 모달타입이 view일 때 Textarea 컴포넌트를 사용하지 않도록 수정
    
    * feat: 버튼에 lightBlue 속성 추가
    
    * design: IconRadioButton 컴포넌트의 도형과 텍스트 사이 갭 추가
    
    * design: 스크롤바의 색 회색으로 수정
    
    * feat: formateDateTimeString 유틸함수 리턴형식 수정
    
    * design: primary2 로 수정

--- frontend/src/components/common/button/Button.style.ts
+++ frontend/src/components/common/button/Button.style.ts
@@ -1,8 +1,8 @@
 import { css, styled } from "styled-components";
 
 export const ButtonContainer = styled.button<{
-  $variant: "primary" | "secondary" | "disable";
-  $size: "small" | "medium" | "large";
+  $variant: "primary" | "secondary" | "disable" | "lightBlue";
+  $size: "xSmall" | "small" | "medium" | "large";
 }>`
   display: flex;
   align-items: center;
@@ -32,7 +32,10 @@ const variantStyles = {
     background-color: ${({ theme }) => theme.COLOR.primary2};
   `,
   secondary: css`
-    background-color: ${({ theme }) => theme.COLOR.error};
+    background-color: ${({ theme }) => theme.COLOR.secondary};
+  `,
+  lightBlue: css`
+    background-color: ${({ theme }) => theme.COLOR.lightGrass};
   `,
   disable: css`
     background-color: ${({ theme }) => theme.COLOR.grey1};
@@ -40,22 +43,28 @@ const variantStyles = {
 };
 
 const sizeStyles = {
+  xSmall: css`
+    width: 90px;
+    padding: 0.1rem 0;
+    font: ${({ theme }) => theme.TEXT.small};
+    border-radius: 4px;
+  `,
   small: css`
     width: 120px;
     padding: 0.1rem 0;
     font: ${({ theme }) => theme.TEXT.small};
-    border-radius: 5px;
+    border-radius: 4px;
   `,
   medium: css`
     width: 200px;
     height: 40px;
     font: ${({ theme }) => theme.TEXT.medium};
-    border-radius: 5px;
+    border-radius: 4px;
   `,
   large: css`
     width: 100%;
     height: 40px;
     font: ${({ theme }) => theme.TEXT.medium};
-    border-radius: 5px;
+    border-radius: 4px;
   `,
 };
--- frontend/src/components/common/iconRadioButton/IconRadioButton.style.ts
+++ frontend/src/components/common/iconRadioButton/IconRadioButton.style.ts
@@ -10,6 +10,7 @@ export const IconRadioButtonContainer = styled.label`
 
   display: flex;
   flex-direction: column;
+  gap: 0.6rem;
   align-items: center;
   justify-content: center;
 `;
@@ -31,7 +32,7 @@ export const IconRadioButtonBox = styled.div<IconRadioButtonBoxProps>`
   background-color: "transparent";
   border: ${({ $color, $isSelected, theme }) =>
     $isSelected
-      ? `4px solid ${$color ? $color : theme.COLOR.primary3}`
+      ? `4px solid ${$color ? $color : theme.COLOR.primary2}`
       : `1px solid ${theme.COLOR.grey1}`};
   border-radius: 50%;
   box-shadow: 0 4px 4px rgb(0 0 0 / 10%);
