3440bec3426d46c9173d362806c6cb62899106fe
jinsil <jinsil378900@gmail.com>
2024-08-20 20:51:33 +0900

    feat: fallback 페이지 분리, 캐릭터 넣기

--- frontend/src/components/common/errorBoundary/SentryTotalBoundary.tsx
+++ frontend/src/components/common/errorBoundary/SentryTotalBoundary.tsx
@@ -1,23 +1,17 @@
+import Fallback from "./Fallback";
 import { ReactNode, Suspense } from "react";
-import Button from "@/components/common/button/Button";
 import DelaySuspense from "@/components/common/delaySuspense/DelaySuspense";
 import SentryApiErrorBoundary from "@/components/common/errorBoundary/SentryApiErrorBoundary";
 import Loading from "@/components/common/loading/Loading";
 import { Sentry } from "@/Sentry";
-
-const FallbackComponent = ({ resetError }: { resetError: () => void }) => {
-  return (
-    <div>
-      <h2>일시적인 에러가 발생했어요</h2>
-      <Button onClick={resetError}>다시 시도하기</Button>
-    </div>
-  );
-};
+import MESSAGES from "@/constants/message";
 
 const SentryTotalBoundary = ({ children }: { children: ReactNode }) => {
   return (
     <Sentry.ErrorBoundary
-      fallback={({ resetError }) => <FallbackComponent resetError={resetError} />}
+      fallback={({ resetError }) => (
+        <Fallback message={MESSAGES.ERROR.BOUNDARY_TOTAL} resetError={resetError} />
+      )}
       onError={(error) => {
         Sentry.withScope((scope) => {
           scope.setTag("type", "runtimeError");
