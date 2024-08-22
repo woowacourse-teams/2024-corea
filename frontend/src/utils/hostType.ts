8e1f17c599c89f9157af521c5353de5b98340f57
jinsil <jinsil378900@gmail.com>
2024-08-22 13:48:06 +0900

    fix: dev host 이름 변경

--- frontend/src/utils/hostType.ts
+++ frontend/src/utils/hostType.ts
@@ -1,10 +1,10 @@
 const getHostType = () => {
   const hostname = window.location.hostname;
 
-  if (hostname === "dev.code-review-area.com") {
+  if (hostname === "code-review-area.com") {
     return "production";
   }
-  if (hostname === "code-review-area.com") {
+  if (hostname === "dev.code-review-area.com") {
     return "release";
   }
   return "develop";
