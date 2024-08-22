2b7b4d773f8c898b919ef257d962bb55b69349d1
00kang <kdb070970@gmail.com>
2024-08-23 02:00:02 +0900

    feat: RoomDetailPage에서 토글 형식으로 프로세스 정보를 제공할 수 있도록 추가

--- frontend/src/pages/roomDetail/RoomDetailPage.style.ts
+++ frontend/src/pages/roomDetail/RoomDetailPage.style.ts
@@ -39,3 +39,54 @@ export const StyledDescription = styled.p`
   font: ${({ theme }) => theme.TEXT.small};
   color: ${({ theme }) => theme.COLOR.grey3};
 `;
+
+export const ExpandableSection = styled.div<{ $isExpanded: boolean }>`
+  cursor: pointer;
+  overflow: hidden;
+  transition: all 0.3s ease-in-out;
+`;
+
+export const StyledSquare = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 1rem;
+
+  font: ${({ theme }) => theme.TEXT.small};
+  color: ${({ theme }) => theme.COLOR.grey3};
+
+  background-color: ${({ theme }) => theme.COLOR.primary1};
+  border-left: 4px solid ${({ theme }) => theme.COLOR.primary3};
+`;
+
+export const ExpandableContent = styled.div`
+  padding: 1rem;
+
+  font: ${({ theme }) => theme.TEXT.small};
+  color: ${({ theme }) => theme.COLOR.grey3};
+
+  background-color: ${({ theme }) => theme.COLOR.grey0};
+  border-left: 4px solid ${({ theme }) => theme.COLOR.primary3};
+
+  transition: all 0.3s ease-in-out;
+
+  p {
+    margin-bottom: 1rem;
+  }
+
+  ol {
+    padding-left: 1rem;
+  }
+
+  li {
+    margin: 0 0 0.4rem 1rem;
+    list-style-type: auto;
+  }
+`;
+
+export const ToggleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
