7101c480c3f704678743f39520aff880a00b4577
00kang <kdb070970@gmail.com>
2024-08-22 13:47:37 +0900

    feat: 필요한 이미지 소스 assets에 추가

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -6,3 +6,12 @@ export { default as spinner } from "./spinner.svg";
 export { default as rank1 } from "./rank-1.svg";
 export { default as rank2 } from "./rank-2.svg";
 export { default as rank3 } from "./rank-3.svg";
+
+export { default as github_pr } from "./GitHub PR 화면.png";
+export { default as github_suggestion } from "./GitHub Suggestion 기능.png";
+export { default as github_comment } from "./GitHub 코멘트 기능.png";
+export { default as github_comment_pending } from "./Pending 상태의 코멘트.png";
+export { default as gihub_pr_ex } from "./PR 메시지 예시.png";
+export { default as github_suggestion_ex } from "./Suggestion 예시.png";
+export { default as code_reviewe_bad } from "./나쁜 코드 리뷰 예시.png";
+export { default as code_reviewe_good } from "./좋은 코드 리뷰 예시.png";
