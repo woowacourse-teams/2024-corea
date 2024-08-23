5d44959e7febfe88a6cc8f450526933c23a15134
jinsil <jinsil378900@gmail.com>
2024-08-23 10:45:58 +0900

    design: 프로필 사진 변경

--- frontend/src/components/common/profile/Profile.style.ts
+++ frontend/src/components/common/profile/Profile.style.ts
@@ -4,9 +4,13 @@ export const ProfileContainer = styled.button<{ $size: number | undefined }>`
   cursor: pointer;
 
   overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   width: ${({ $size }) => ($size ? $size : 32)}px;
   height: ${({ $size }) => ($size ? $size : 32)}px;
+  padding: 0;
 
   object-fit: contain;
   border-radius: 50%;
