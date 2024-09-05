71a2ccac751a8e5ae8c0ca0ca269465b61548a98
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-05 23:07:32 +0900

    feat: 깃허브 클라이언트,프로바이더 분리

---
+++ backend/src/main/java/corea/auth/service/GithubOAuthProvider.java
@@ -0,0 +1,17 @@
+package corea.auth.service;
+
+import corea.auth.dto.GithubUserInfo;
+import corea.auth.infrastructure.GithubOAuthClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class GithubOAuthProvider {
+    private final GithubOAuthClient githubOAuthClient;
+
+    public GithubUserInfo getUserInfo(String code) {
+        String accessToken = githubOAuthClient.getAccessToken(code);
+        return githubOAuthClient.getUserInfo(accessToken);
+    }
+}
