9940decd0443016816bc5b60fe86e57e541add80
Lee sang Yeop <mma7710@naver.com>
2024-09-06 16:56:42 +0900

    refactor: 참여중, 모집중, 종료된 방을 각각 컴포넌트로 분리 및 필요할 때 네트워크 요청하도록 변경

--- frontend/src/pages/main/MainPage.tsx
+++ frontend/src/pages/main/MainPage.tsx
@@ -1,26 +1,13 @@
 import { useState } from "react";
 import useSelectedCategory from "@/hooks/common/useSelectedCategory";
-import {
-  useFetchParticipatedRoomList,
-  useInfiniteFetchRoomList,
-} from "@/hooks/queries/useFetchRooms";
-import ContentSection from "@/components/common/contentSection/ContentSection";
-import Dropdown, { DropdownItem } from "@/components/common/dropdown/Dropdown";
 import OptionSelect from "@/components/common/optionSelect/OptionSelect";
 import Banner from "@/components/main/banner/Banner";
-import RoomList from "@/components/shared/roomList/RoomList";
+import ClosedRoomList from "@/components/main/room/ClosedRoomList";
+import OpenedRoomList from "@/components/main/room/OpenedRoomList";
+import ParticipatedRoomList from "@/components/main/room/ParticipatedRoomList";
 import * as S from "@/pages/main/MainPage.style";
 import { Option } from "@/@types/rooms";
-import QUERY_KEYS from "@/apis/queryKeys";
-import { getClosedRoomList, getOpenedRoomList } from "@/apis/rooms.api";
 import { optionsLoggedIn, optionsLoggedOut } from "@/constants/room";
-
-export const dropdownItems: DropdownItem[] = [
-  { text: "ALL", value: "all" },
-  { text: "ANDROID", value: "an" },
-  { text: "BACKEND", value: "be" },
-  { text: "FRONTEND", value: "fe" },
-];
 
 const MainPage = () => {
   const isLoggedIn = !!localStorage.getItem("accessToken");
@@ -28,78 +15,24 @@ const MainPage = () => {
   const [selectedTab, setSelectedTab] = useState<Option>(options[0]);
 
   const { selectedCategory, handleSelectedCategory } = useSelectedCategory();
-  const { data: participatedRoomList } = useFetchParticipatedRoomList();
-
-  const {
-    data: openedRoomList,
-    fetchNextPage: fetchNextOpenedPage,
-    hasNextPage: hasNextOpenedPage,
-  } = useInfiniteFetchRoomList({
-    queryKey: [QUERY_KEYS.OPENED_ROOM_LIST, selectedCategory],
-    getRoomList: getOpenedRoomList,
-    classification: selectedCategory,
-  });
-
-  const {
-    data: closedRoomList,
-    fetchNextPage: fetchNextClosedPage,
-    hasNextPage: hasNextClosedPage,
-  } = useInfiniteFetchRoomList({
-    queryKey: [QUERY_KEYS.CLOSED_ROOM_LIST, selectedCategory],
-    getRoomList: getClosedRoomList,
-    classification: selectedCategory,
-  });
-
-  const openedRooms = openedRoomList?.pages.flatMap((page) => page.rooms) || [];
-  const closedRooms = closedRoomList?.pages.flatMap((page) => page.rooms) || [];
 
   const renderContent = () => {
     switch (selectedTab) {
       case "참여 중":
-        return (
-          <ContentSection title="">
-            {participatedRoomList ? (
-              <RoomList roomList={participatedRoomList.rooms} roomType="participated" />
-            ) : (
-              <div>❗ 참여 중인 방이 없습니다.</div>
-            )}
-          </ContentSection>
-        );
+        return <ParticipatedRoomList />;
       case "모집 중":
         return (
-          <ContentSection title="">
-            <S.DropdownWrapper>
-              <Dropdown
-                dropdownItems={dropdownItems}
-                selectedCategory={selectedCategory}
-                onSelectCategory={handleSelectedCategory}
-              />
-            </S.DropdownWrapper>
-            <RoomList
-              roomList={openedRooms}
-              hasNextPage={hasNextOpenedPage}
-              onLoadMore={() => fetchNextOpenedPage()}
-              roomType="opened"
-            />
-          </ContentSection>
+          <OpenedRoomList
+            selectedCategory={selectedCategory}
+            handleSelectedCategory={handleSelectedCategory}
+          />
         );
       case "모집 마감":
         return (
-          <ContentSection title="">
-            <S.DropdownWrapper>
-              <Dropdown
-                dropdownItems={dropdownItems}
-                selectedCategory={selectedCategory}
-                onSelectCategory={handleSelectedCategory}
-              />
-            </S.DropdownWrapper>
-            <RoomList
-              roomList={closedRooms}
-              hasNextPage={hasNextClosedPage}
-              onLoadMore={() => fetchNextClosedPage()}
-              roomType="closed"
-            />
-          </ContentSection>
+          <ClosedRoomList
+            selectedCategory={selectedCategory}
+            handleSelectedCategory={handleSelectedCategory}
+          />
         );
       default:
         return null;
