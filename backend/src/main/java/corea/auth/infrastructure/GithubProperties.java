eee147fe64105f2194927a494319a0b5ff6d2cc1
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-06 13:34:08 +0900

    feat: PAT 받는 기능 구현

--- backend/src/main/java/corea/auth/infrastructure/GithubProperties.java
+++ backend/src/main/java/corea/auth/infrastructure/GithubProperties.java
@@ -2,12 +2,17 @@ package corea.auth.infrastructure;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+
 @ConfigurationProperties(prefix = "security.github")
-public record GithubProperties(BaseUrl baseUrl, OAuth oauth) {
+public record GithubProperties(BaseUrl baseUrl, OAuth oauth,PullRequest pullRequest) {
 
     public record BaseUrl(String oauth, String user) {
     }
 
     public record OAuth(String clientId, String clientSecret) {
+    }
+
+    public record PullRequest(List<String> tokens) {
     }
 }
