eedaed9733619b5fdbd542d431cd22731d2360d8
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-10 18:54:20 +0900

    test: 만료 시간 연장

--- backend/src/test/java/corea/auth/service/LoginServiceTest.java
+++ backend/src/test/java/corea/auth/service/LoginServiceTest.java
@@ -60,11 +60,12 @@ class LoginServiceTest {
 
     @Test
     @DisplayName("RefreshToken의 유효기간이 만료되었을 경우 DB에서 해당 유저의 로그인 정보를 삭제한다.")
-    void authorizeException_TokenExpired() {
-        String expiredRefreshToken = tokenProvider.createToken(member, 10L);
+    void authorizeException_TokenExpired() throws InterruptedException {
+        String expiredRefreshToken = tokenProvider.createToken(member, 30L);
 
         loginInfoRepository.save(new LoginInfo(member, expiredRefreshToken));
 
+        Thread.sleep(30);
         assertThatThrownBy(() -> loginService.authorize(expiredRefreshToken))
                 .isInstanceOf(CoreaException.class)
                 .satisfies(exception -> {
