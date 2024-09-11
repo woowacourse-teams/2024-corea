93d17d01aa4cb67c777d8574eae61b9d7d09ba3f
hjk0761 <hjk0761@naver.com>
2024-09-11 16:56:50 +0900

    feat: FeedBacksResponse 에서 isClosed 제거
    피드백 중 닫힌 방에서 작성된 것만 응답하도록 수정

--- backend/src/test/java/corea/fixture/RoomFixture.java
+++ backend/src/test/java/corea/fixture/RoomFixture.java
@@ -47,4 +47,23 @@ public class RoomFixture {
                 RoomStatus.OPENED
         );
     }
+
+    public static Room ROOM_DOMAIN_WITH_CLOSED(Member member) {
+        return new Room(
+                "자바 레이싱 카 - MVC",
+                "MVC 패턴을 아시나요?",
+                4,
+                "https://github.com/example/java-racingcar",
+                "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=13301655&filePath=L2Rpc2sxL25ld2RhdGEvMjAyMS8yMS9DTFMxMDAwNC8xMzMwMTY1NV9XUlRfMjFfQ0xTMTAwMDRfMjAyMTEyMTNfMQ==&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004",
+                List.of("TDD, 클린코드,자바"),
+                17,
+                30,
+                member,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+                        .plusDays(14),
+                RoomClassification.BACKEND,
+                RoomStatus.CLOSED
+        );
+    }
 }
