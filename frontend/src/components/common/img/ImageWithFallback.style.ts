089c33d188b682aa3ac8af71676744c9a61b5e10
00kang <kdb070970@gmail.com>
2024-09-12 13:04:11 +0900

    feat: 이미지 경로가 잘못되거나 없을 때 errorCharacter로 보여주는 공통 컴포넌트(ImageWithFallback) 추가

---
+++ frontend/src/components/common/img/ImageWithFallback.style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: scale-down;
+`;
---
+++ frontend/src/components/common/img/ImageWithFallback.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import * as S from "@/components/common/img/ImageWithFallback.style";
+import { errorCharacter } from "@/assets";
+
+interface ImageWithFallbackProps {
+  src: string;
+  alt: string;
+  fallbackSrc?: string;
+}
+
+const ImageWithFallback = ({
+  src,
+  alt,
+  fallbackSrc = errorCharacter,
+  ...props
+}: ImageWithFallbackProps) => {
+  const handleError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    const img = e.target as HTMLImageElement;
+    img.onerror = null;
+    img.src = fallbackSrc;
+  };
+
+  return <S.StyledImg src={src} alt={alt} onError={handleError} {...props} />;
+};
+
+export default ImageWithFallback;
