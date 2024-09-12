ead22f75b7a6923f97b054b58dbebf6504cd13e8
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-12 11:06:22 +0900

    feat: 불필요한 분리 제거

--- backend/src/main/java/corea/global/aspect/query/QueryInfo.java
+++ backend/src/main/java/corea/global/aspect/query/QueryInfo.java
@@ -1,14 +1,56 @@
 package corea.global.aspect.query;
 
-public interface QueryInfo {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
-    void increaseQueryCount(String query);
+import java.util.HashMap;
+import java.util.Map;
 
-    String toFormatString();
+@Component
+@RequiredArgsConstructor
+@Profile({"local", "dev"})
+@RequestScope
+@Getter
+public class QueryInfo {
 
-    boolean isExceedQuery();
+    private static final int MIN_WARN_SIZE = 10;
+    private static final String NEWLINE = System.lineSeparator();
+    private Map<String, Integer> data = new HashMap<>();
 
-    int getCount();
+    public void increaseQueryCount(String query) {
+        data.merge(query, 1, Integer::sum);
+    }
 
-    void clear();
+    public String toFormatString() {
+        StringBuilder sb = new StringBuilder();
+        data.forEach((key, value) ->
+                sb.append(key)
+                        .append(" : ")
+                        .append(value)
+                        .append(NEWLINE)
+        );
+        return sb.toString();
+    }
+
+    public boolean isExceedQuery() {
+        return this.sum() >= MIN_WARN_SIZE;
+    }
+
+    public void clear() {
+        data.clear();
+    }
+
+    public int getCount() {
+        return sum();
+    }
+
+    private int sum() {
+        return data.values()
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
 }
