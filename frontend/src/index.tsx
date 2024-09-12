efe515aaf7ffe752e544412a2814ca298ee46432
Lee sang Yeop <mma7710@naver.com>
2024-09-12 10:39:47 +0900

    chore: react-dev-tools 라이브러리 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
@@ -30,6 +31,7 @@ enableMocking().then(() => {
             <RouterProvider router={router} />
           </ToastProvider>
         </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </React.StrictMode>,
   );
