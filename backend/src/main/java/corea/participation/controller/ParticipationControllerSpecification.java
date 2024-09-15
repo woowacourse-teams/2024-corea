2d2474200c1f6f5ae52ab0c24c1ce0cd515619fd
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-16 00:16:49 +0900

    feat: 참여 취소하는 기능 명세 추가

--- backend/src/main/java/corea/participation/controller/ParticipationControllerSpecification.java
+++ backend/src/main/java/corea/participation/controller/ParticipationControllerSpecification.java
@@ -25,5 +25,21 @@ public interface ParticipationControllerSpecification {
             @Parameter(description = "방 아이디", example = "1")
             long id,
             AuthInfo authInfo);
+
+    @Operation(summary = "참여한 방을 취소합니다.",
+            description = "원하는 방 상세 페이지에 위치한 취소 버튼을 누르면 취소할 수 있습니다. <br>" +
+                    "요청 시 `Authorization Header`에 `Bearer JWT token`을 포함시켜야 합니다. " +
+                    "이 토큰을 기반으로 `AuthInfo` 객체가 생성되며 사용자의 정보가 자동으로 주입됩니다. <br>" +
+                    "JWT 토큰에서 추출된 사용자 정보는 피드백 작성에 필요한 인증된 사용자 정보를 제공합니다. " +
+                    "<br><br>**참고:** 이 API를 사용하기 위해서는 유효한 JWT 토큰이 필요하며, " +
+                    "토큰이 없거나 유효하지 않은 경우 인증 오류가 발생합니다.")
+    @ApiErrorResponses(value = {ExceptionType.MEMBER_NOT_FOUND, ExceptionType.ROOM_NOT_FOUND, ExceptionType.NOT_ALREADY_APPLY},
+            groups = ExceptionTypeGroup.INTERNAL_SERVER_ERROR)
+    ResponseEntity<Void> cancelParticipate(
+            @Parameter(description = "방 아이디", example = "1")
+            long id,
+            AuthInfo authInfo);
+
+
 }
 
