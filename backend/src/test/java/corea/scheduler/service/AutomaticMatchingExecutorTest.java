605fb8433846e8a6c61e96307b209a3bd6205812
gyungchan Jo <namejgc@naver.com>
2024-09-19 17:06:46 +0900

    feat: 매칭하는 기능 구현

---
+++ backend/src/test/java/corea/scheduler/service/AutomaticMatchingExecutorTest.java
@@ -0,0 +1,113 @@
+package corea.scheduler.service;
+
+import config.ServiceTest;
+import corea.fixture.MemberFixture;
+import corea.fixture.RoomFixture;
+import corea.matching.domain.MatchResult;
+import corea.matching.domain.PullRequestInfo;
+import corea.matching.infrastructure.dto.GithubUserResponse;
+import corea.matching.infrastructure.dto.PullRequestResponse;
+import corea.matching.repository.MatchResultRepository;
+import corea.matching.service.PullRequestProvider;
+import corea.member.domain.Member;
+import corea.member.repository.MemberRepository;
+import corea.participation.domain.Participation;
+import corea.participation.repository.ParticipationRepository;
+import corea.room.domain.Room;
+import corea.room.repository.RoomRepository;
+import corea.scheduler.domain.AutomaticMatching;
+import corea.scheduler.repository.AutomaticMatchingRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+
+@ServiceTest
+class AutomaticMatchingExecutorTest {
+
+    @Autowired
+    private AutomaticMatchingExecutor automaticMatchingExecutor;
+
+    @Autowired
+    private AutomaticMatchingRepository automaticMatchingRepository;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private MatchResultRepository matchResultRepository;
+
+    @Autowired
+    private ParticipationRepository participationRepository;
+
+    @MockBean
+    private PullRequestProvider pullRequestProvider;
+
+    private Room room;
+
+    @BeforeEach
+    void setUp() {
+        Member pororo = memberRepository.save(MemberFixture.MEMBER_PORORO());
+        Member ash = memberRepository.save(MemberFixture.MEMBER_ASH());
+        Member joysun = memberRepository.save(MemberFixture.MEMBER_YOUNGSU());
+        Member movin = memberRepository.save(MemberFixture.MEMBER_MUBIN());
+        Member ten = memberRepository.save(MemberFixture.MEMBER_TENTEN());
+        Member cho = memberRepository.save(MemberFixture.MEMBER_CHOCO());
+
+        room = roomRepository.save(RoomFixture.ROOM_DOMAIN_WITH_DEADLINE(pororo, LocalDateTime.now().plusSeconds(3)));
+        long roomId = room.getId();
+
+        participationRepository.save(new Participation(roomId, pororo.getId(), pororo.getGithubUserId()));
+        participationRepository.save(new Participation(roomId, ash.getId(), ash.getGithubUserId()));
+        participationRepository.save(new Participation(roomId, joysun.getId(), joysun.getGithubUserId()));
+        participationRepository.save(new Participation(roomId, movin.getId(), movin.getGithubUserId()));
+        participationRepository.save(new Participation(roomId, ten.getId(), ten.getGithubUserId()));
+        participationRepository.save(new Participation(roomId, cho.getId(), cho.getGithubUserId()));
+
+        Mockito.when(pullRequestProvider.getUntilDeadline(any(), any()))
+                .thenReturn(new PullRequestInfo(Map.of(
+                        pororo.getGithubUserId(),
+                        new PullRequestResponse("link", new GithubUserResponse(pororo.getGithubUserId()),
+                                LocalDateTime.of(2024, 10, 12, 18, 00)),
+                        ash.getGithubUserId(),
+                        new PullRequestResponse("link", new GithubUserResponse(ash.getGithubUserId()),
+                                LocalDateTime.of(2024, 10, 12, 18, 20)),
+                        joysun.getGithubUserId(),
+                        new PullRequestResponse("link", new GithubUserResponse(joysun.getGithubUserId()),
+                                LocalDateTime.of(2024, 10, 12, 18, 30)),
+                        movin.getGithubUserId(),
+                        new PullRequestResponse("link", new GithubUserResponse(movin.getGithubUserId()),
+                                LocalDateTime.of(2024, 10, 12, 18, 10)),
+                        ten.getGithubUserId(),
+                        new PullRequestResponse("link", new GithubUserResponse(ten.getGithubUserId()),
+                                LocalDateTime.of(2024, 10, 12, 18, 01)),
+                        cho.getGithubUserId(),
+                        new PullRequestResponse("link", new GithubUserResponse(cho.getGithubUserId()),
+                                LocalDateTime.of(2024, 10, 12, 18, 01)
+                        )
+                )));
+    }
+
+    @Test
+    @DisplayName("매칭을 진행한다.")
+    void execute() {
+        AutomaticMatching automaticMatching = automaticMatchingRepository.save(new AutomaticMatching(room.getId(), room.getRecruitmentDeadline()));
+
+        automaticMatchingExecutor.execute(automaticMatching);
+
+        List<MatchResult> matchResults = matchResultRepository.findAll();
+        assertThat(matchResults).isNotEmpty();
+    }
+}
