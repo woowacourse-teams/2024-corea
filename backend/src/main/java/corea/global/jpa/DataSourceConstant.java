dfed80434329ea58cf7e51b12298cf287c2cea7b
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-19 21:38:25 +0900

    feat: Read & Write DB DataSource 통해 분리

---
+++ backend/src/main/java/corea/global/jpa/DataSourceConstant.java
@@ -0,0 +1,10 @@
+package corea.global.jpa;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DataSourceConstant {
+    public static final String READ = "read";
+    public static final String WRITE = "write";
+}
