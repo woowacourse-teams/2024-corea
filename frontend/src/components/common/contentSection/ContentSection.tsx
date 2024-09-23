69d40e82922a8c4cd46ef3bca0b75eb5242de981
00kang <kdb070970@gmail.com>
2024-09-23 18:54:06 +0900

    feat: ContentSection 컴포넌트를 경우에 따라 버튼(취소/삭제)이 렌더링되도록 수정

--- frontend/src/components/common/contentSection/ContentSection.tsx
+++ frontend/src/components/common/contentSection/ContentSection.tsx
@@ -1,15 +1,28 @@
 import { ReactNode } from "react";
+import Button, { ButtonProps } from "@/components/common/button/Button";
 import * as S from "@/components/common/contentSection/ContentSection.style";
+
+interface ContentSectionButton extends ButtonProps {
+  label: string;
+}
 
 interface ContentSectionProps {
   title: string;
   children?: ReactNode;
+  button?: ContentSectionButton | false;
 }
 
-const ContentSection = ({ title, children }: ContentSectionProps) => {
+const ContentSection = ({ title, children, button }: ContentSectionProps) => {
   return (
     <S.ContentSectionContainer>
-      <S.ContentSectionTitle>{title}</S.ContentSectionTitle>
+      <S.ContentSectionHeader>
+        <S.ContentSectionTitle>{title}</S.ContentSectionTitle>
+        {button && (
+          <Button onClick={button.onClick} size="small">
+            {button.label}
+          </Button>
+        )}
+      </S.ContentSectionHeader>
       {children}
     </S.ContentSectionContainer>
   );
