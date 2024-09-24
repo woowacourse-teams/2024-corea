8d69bb304dfcfc3ecac25017563b2c07fa28c914
00kang <kdb070970@gmail.com>
2024-09-24 17:42:56 +0900

    feat: ContentSectionButton에 label(텍스트)뿐만 아니라 icon도 넣을 수 있도록 수정

--- frontend/src/components/common/contentSection/ContentSection.tsx
+++ frontend/src/components/common/contentSection/ContentSection.tsx
@@ -1,9 +1,12 @@
 import { ReactNode } from "react";
 import Button, { ButtonProps } from "@/components/common/button/Button";
 import * as S from "@/components/common/contentSection/ContentSection.style";
+import Icon from "@/components/common/icon/Icon";
+import IconKind from "@/@types/icon";
 
 interface ContentSectionButton extends ButtonProps {
-  label: string;
+  label?: string;
+  icon?: IconKind;
 }
 
 interface ContentSectionProps {
@@ -13,12 +16,15 @@ interface ContentSectionProps {
 }
 
 const ContentSection = ({ title, children, button }: ContentSectionProps) => {
+  const buttonSize = button?.icon ? "xSmall" : "small";
+
   return (
     <S.ContentSectionContainer>
       <S.ContentSectionHeader>
         <S.ContentSectionTitle>{title}</S.ContentSectionTitle>
         {button && (
-          <Button onClick={button.onClick} size="small">
+          <Button onClick={button.onClick} size={buttonSize}>
+            {button.icon && <Icon kind={button.icon} size={20} />}
             {button.label}
           </Button>
         )}
