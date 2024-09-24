4f2942f4400e22728cb2d4783c53369324a1cf8a
00kang <kdb070970@gmail.com>
2024-09-24 16:10:29 +0900

    feat: 방 참여 관련 api에서 바로 return하도록 수정

--- frontend/src/apis/rooms.api.ts
+++ frontend/src/apis/rooms.api.ts
@@ -46,14 +46,14 @@ export const getRoomDetailInfo = async (id: number): Promise<RoomInfo> => {
 };
 
 export const postParticipateIn = async (roomId: number): Promise<void> => {
-  const res = await apiClient.post({
+  return apiClient.post({
     endpoint: API_ENDPOINTS.PARTICIPATE_IN(roomId),
     errorMessage: MESSAGES.ERROR.POST_PARTICIPATE_IN,
   });
 };
 
 export const deleteParticipateIn = async (roomId: number): Promise<void> => {
-  const res = await apiClient.delete({
+  return apiClient.delete({
     endpoint: API_ENDPOINTS.PARTICIPATE_IN(roomId),
     errorMessage: MESSAGES.ERROR.DELETE_PARTIPATE_IN,
   });
