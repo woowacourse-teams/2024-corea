69ea665ab0fcbe5f43952388596b6038b2eab2fd
jinsil <jinsil378900@gmail.com>
2024-09-25 13:55:35 +0900

    feat: 방 생성 성공했을 때 toast 띄우기

--- frontend/src/hooks/mutations/useMutateRoom.ts
+++ frontend/src/hooks/mutations/useMutateRoom.ts
@@ -1,17 +1,21 @@
+import useToast from "../common/useToast";
 import useMutateHandlers from "./useMutateHandlers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { CreateRoomInfo } from "@/@types/roomInfo";
 import QUERY_KEYS from "@/apis/queryKeys";
 import { deleteParticipateIn, postCreateRoom, postParticipateIn } from "@/apis/rooms.api";
+import MESSAGES from "@/constants/message";
 
 const useMutateRoom = () => {
   const queryClient = useQueryClient();
+  const { openToast } = useToast("success");
   const { handleMutateError } = useMutateHandlers();
 
   const postCreateRoomMutation = useMutation({
     mutationFn: (roomData: CreateRoomInfo) => postCreateRoom(roomData),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.PARTICIPATED_ROOM_LIST] });
+      openToast(MESSAGES.SUCCESS.POST_CREATE_ROOM);
     },
     onError: (error) => handleMutateError(error),
   });
