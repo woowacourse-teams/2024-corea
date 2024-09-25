8633e87976aa9ca1470ff15b72e0fdb1c5f33e57
hjk0761 <hjk0761@naver.com>
2024-09-25 20:50:21 +0900

    test: 리뷰 주소 가져오는 기능에 대한 테스트 작성

---
+++ backend/src/test/java/corea/review/controller/ReviewControllerTest.java
@@ -0,0 +1,96 @@
+package corea.review.controller;
+
+import corea.auth.service.TokenService;
+import corea.fixture.MemberFixture;
+import corea.fixture.RoomFixture;
+import corea.matching.domain.MatchResult;
+import corea.matching.domain.PullRequestInfo;
+import corea.matching.repository.MatchResultRepository;
+import corea.matching.service.MatchingService;
+import corea.matching.service.PullRequestProvider;
+import corea.member.domain.Member;
+import corea.member.repository.MemberRepository;
+import corea.participation.dto.ParticipationRequest;
+import corea.participation.service.ParticipationService;
+import corea.review.dto.ReviewRequest;
+import corea.room.domain.Room;
+import corea.room.repository.RoomRepository;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ReviewControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private MatchingService matchingService;
+
+    @Autowired
+    private PullRequestProvider pullRequestProvider;
+
+    @Autowired
+    private MatchResultRepository matchResultRepository;
+
+    @Autowired
+    private ParticipationService participationService;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    @DisplayName("리뷰어가 리뷰를 작성했을 때, 해당 리뷰의 링크로 업데이트 한다.")
+    void test() {
+        Member manager = memberRepository.save(MemberFixture.MEMBER_MOVIN());
+        Member reviewer = memberRepository.save(MemberFixture.MEMBER_PORORO());
+        Member reviewee = memberRepository.save(MemberFixture.MEMBER_YOUNGSU());
+        Member participant = memberRepository.save(MemberFixture.MEMBER_DARR());
+
+        Room room = roomRepository.save(RoomFixture.ROOM_PULL_REQUEST(manager));
+
+        participationService.participate(new ParticipationRequest(room.getId(), manager.getId()));
+        participationService.participate(new ParticipationRequest(room.getId(), reviewer.getId()));
+        participationService.participate(new ParticipationRequest(room.getId(), reviewee.getId()));
+        participationService.participate(new ParticipationRequest(room.getId(), participant.getId()));
+
+        String accessToken = tokenService.createAccessToken(reviewer);
+        PullRequestInfo prInfo = pullRequestProvider.getUntilDeadline(room.getRepositoryLink(), room.getRecruitmentDeadline());
+        matchingService.match(room.getId(), prInfo);
+
+        MatchResult matchResultBeforeReview = matchResultRepository.findByRoomIdAndReviewerIdAndRevieweeId(room.getId(), reviewer.getId(), reviewee.getId()).get();
+        assertThat(matchResultBeforeReview.getReviewLink()).isEmpty();
+
+        ReviewRequest request1 = new ReviewRequest(room.getId(), reviewee.getId());
+        RestAssured.given().log().all()
+                .auth().oauth2(accessToken)
+                .contentType(ContentType.JSON)
+                .body(request1)
+                .when().post("/review/complete")
+                .then().log().all()
+                .statusCode(200)
+                .extract();
+
+        MatchResult matchResultAfterReview = matchResultRepository.findByRoomIdAndReviewerIdAndRevieweeId(room.getId(), reviewer.getId(), reviewee.getId()).get();
+        assertThat(matchResultAfterReview.getReviewLink()).isEqualTo("https://github.com/youngsu5582/github-api-test/pull/5#pullrequestreview-2327172283");
+    }
+}
