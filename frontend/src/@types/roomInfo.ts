3a253d55f9922597ecea7265233fd830ef0caf7a
jinsil <jinsil378900@gmail.com>
2024-10-05 16:02:29 +0900

    feat: 모킹 데이터, 스토리북에 memberRole 변수 추가

--- frontend/src/@types/roomInfo.ts
+++ frontend/src/@types/roomInfo.ts
@@ -4,7 +4,7 @@ export type RoomStatus = "OPEN" | "CLOSE" | "PROGRESS";
 
 export type ParticipationStatus = "NOT_PARTICIPATED" | "PARTICIPATED" | "MANAGER";
 
-export type Role = "BOTH" | "REVIEWER" | "REVIEWEE";
+export type Role = "BOTH" | "REVIEWER" | "REVIEWEE" | "NONE";
 
 interface BaseRoomInfo {
   title: string;
@@ -27,6 +27,7 @@ export interface RoomInfo extends BaseRoomInfo {
   currentParticipants: number;
   roomStatus: RoomStatus;
   participationStatus: ParticipationStatus;
+  memberRole: Role;
 }
 
 export interface RoomListInfo {
