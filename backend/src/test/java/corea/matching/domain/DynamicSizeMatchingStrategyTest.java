4969a231e9fc0d74ab828473d5cef51bfda84e1b
hjk0761 <hjk0761@naver.com>
2024-10-05 13:14:48 +0900

    refactor: 매칭 로직 수정

--- backend/src/test/java/corea/matching/domain/DynamicSizeMatchingStrategyTest.java
+++ backend/src/test/java/corea/matching/domain/DynamicSizeMatchingStrategyTest.java
@@ -16,9 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 @SpringBootTest
 class DynamicSizeMatchingStrategyTest {
@@ -39,6 +39,9 @@ class DynamicSizeMatchingStrategyTest {
     private Member movin;
     private Member pororo;
     private Member ash;
+    private Member tenten;
+    private Member darr;
+    private Member choco;
     private Room room;
 
 
@@ -48,6 +51,9 @@ class DynamicSizeMatchingStrategyTest {
         movin = memberRepository.save(MemberFixture.MEMBER_MOVIN());
         pororo = memberRepository.save(MemberFixture.MEMBER_PORORO());
         ash = memberRepository.save(MemberFixture.MEMBER_ASH());
+        tenten = memberRepository.save(MemberFixture.MEMBER_TENTEN());
+        darr = memberRepository.save(MemberFixture.MEMBER_DARR());
+        choco = memberRepository.save(MemberFixture.MEMBER_CHOCO());
         room = roomRepository.save(RoomFixture.ROOM_DOMAIN(joyson));
     }
 
@@ -55,38 +61,59 @@ class DynamicSizeMatchingStrategyTest {
     @DisplayName("다양한 matchingSize 에 맞게 매칭 결과를 생성한다.")
     void matchPairs() {
         List<Participation> participations = participationRepository.saveAll(List.of(
-                new Participation(room, joyson.getId(), joyson.getGithubUserId(), 4),
+                new Participation(room, joyson.getId(), joyson.getGithubUserId(), 5),
                 new Participation(room, movin.getId(), movin.getGithubUserId(), 2),
-                new Participation(room, pororo.getId(), pororo.getGithubUserId(), 3)
-                , new Participation(room, ash.getId(), ash.getGithubUserId(), 3)
+                new Participation(room, pororo.getId(), pororo.getGithubUserId(), 6),
+                new Participation(room, ash.getId(), ash.getGithubUserId(), 4),
+                new Participation(room, tenten.getId(), tenten.getGithubUserId(), 6),
+                new Participation(room, darr.getId(), darr.getGithubUserId(), 2),
+                new Participation(room, choco.getId(), choco.getGithubUserId(), 3)
         ));
 
-        List<Pair> pairs = matchingStrategy.matchPairs(participations);
+        List<Pair> pairs = matchingStrategy.matchPairs(participations, room.getMatchingSize());
 
-        List<Pair> pairWithReviewerMovin = pairs.stream().filter(pair -> pair.getReceiver().getUsername().equals(movin.getUsername())).toList();
-        List<Pair> pairWithRevieweeJoyson = pairs.stream().filter(pair -> pair.getDeliver().getUsername().equals(joyson.getUsername())).toList();
-
-        assertThat(pairs).hasSize(11);
-        assertSoftly(softly -> {
-            softly.assertThat(pairWithReviewerMovin).hasSize(2);
-            softly.assertThat(pairWithRevieweeJoyson).hasSize(3);
-        });
+        assertThat(pairs).hasSize(28);
     }
 
     @Test
-    @DisplayName("리뷰어가 포함된 경우 알맞는 매칭 결과를 생성한다.")
+    @DisplayName("리뷰어가 포함된 다양한 matchingSize 에 맞게 매칭 결과를 생성한다.")
     void matchPairsWithReviewer() {
         List<Participation> participations = participationRepository.saveAll(List.of(
-                new Participation(room, joyson.getId(), joyson.getGithubUserId(), 4),
+                new Participation(room, joyson.getId(), joyson.getGithubUserId(), 5),
                 new Participation(room, movin.getId(), movin.getGithubUserId(), 2),
-                new Participation(room, pororo.getId(), pororo.getGithubUserId(), 3)
-                , new Participation(room, ash.getId(), ash.getGithubUserId(), MemberRole.REVIEWER, 3)
+                new Participation(room, pororo.getId(), pororo.getGithubUserId(), 5),
+                new Participation(room, ash.getId(), ash.getGithubUserId(), 4),
+                new Participation(room, tenten.getId(), tenten.getGithubUserId(), 5),
+                new Participation(room, darr.getId(), darr.getGithubUserId(), 2),
+                new Participation(room, choco.getId(), choco.getGithubUserId(), MemberRole.REVIEWER, 3)
         ));
 
-        List<Pair> pairs = matchingStrategy.matchPairs(participations);
+        List<Pair> pairs = matchingStrategy.matchPairs(participations, room.getMatchingSize());
 
-        pairs.forEach(pair -> System.out.println(pair.getDeliver().getUsername() + ":" + pair.getReceiver().getUsername()));
+        assertThat(pairs).hasSize(26);
+    }
 
-        assertThat(pairs).hasSize(8);
+    @Test
+    void some() {
+        List<Member> members = memberRepository.saveAll(MemberFixture.CREATE_MEMBERS(100));
+
+        List<Participation> participations = participationRepository.saveAll(
+                members.stream()
+                        .map(this::createParticipationWithRandom).toList());
+        List<Pair> pairs = matchingStrategy.matchPairs(participations, room.getMatchingSize());
+
+        participations.forEach(participation -> validateWithMatchingSize(participation, pairs));
+    }
+
+    private Participation createParticipationWithRandom(Member member) {
+        return new Participation(room, member.getId(), member.getGithubUserId(), (int)(Math.random() * (10 - 2 + 1)+2));
+    }
+
+    private void validateWithMatchingSize(Participation participation, List<Pair> pairs) {
+        long deliver_count = pairs.stream()
+                .filter(pair -> pair.getDeliver().isMatchingId(participation.getMemberId()))
+                .count();
+
+        assertThat(participation.getMatchingSize()).isEqualTo(deliver_count);
     }
 }
