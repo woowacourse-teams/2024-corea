ace4cd9e06c2f2eb17581c841e9b1f05bbe2bad7
00kang <kdb070970@gmail.com>
2024-10-07 14:16:50 +0900

    feat: IntroPage에 scroll-snap 적용

---
+++ frontend/src/pages/intro/IntroPage.style.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Layout = styled.div`
+  scroll-behavior: smooth;
+  scroll-snap-type: y mandatory;
+
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  height: 100vh;
+
+  background-color: ${({ theme }) => theme.COLOR.grey0};
+`;
+
+export const ContentLayout = styled.div`
+  scroll-snap-align: start;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 1200px;
+  min-height: 100vh;
+
+  background-color: ${({ theme }) => theme.COLOR.primary2};
+  border: 1px solid ${({ theme }) => theme.COLOR.black};
+`;
---
+++ frontend/src/pages/intro/IntroPage.tsx
@@ -0,0 +1,12 @@
+import * as S from "@/pages/intro/IntroPage.style";
+
+const IntroPage = () => {
+  return (
+    <S.Layout>
+      <S.ContentLayout>섹션1</S.ContentLayout>
+      <S.ContentLayout>섹션2</S.ContentLayout>
+    </S.Layout>
+  );
+};
+
+export default IntroPage;
