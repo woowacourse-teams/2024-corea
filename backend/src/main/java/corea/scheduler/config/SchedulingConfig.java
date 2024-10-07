c396c653e53ab32a05a3b2f4be52c222f821ba82
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-10-07 21:38:04 +0900

    feat: Async 활성화 추가

--- backend/src/main/java/corea/scheduler/config/SchedulingConfig.java
+++ backend/src/main/java/corea/scheduler/config/SchedulingConfig.java
@@ -1,10 +1,12 @@
 package corea.scheduler.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@EnableAsync
 public class SchedulingConfig {
 
 }
