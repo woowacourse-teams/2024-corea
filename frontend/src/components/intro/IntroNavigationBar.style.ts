8c1179d24be803ca912070e03c5ace8a16731e46
00kang <kdb070970@gmail.com>
2024-10-08 21:29:29 +0900

    style: 네비게이션바 스타일 수정

--- frontend/src/components/intro/IntroNavigationBar.style.ts
+++ frontend/src/components/intro/IntroNavigationBar.style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const NavContainer = styled.div`
   position: fixed;
   top: 50%;
-  right: 380px;
+  right: 12%;
   transform: translateY(-50%);
 
   display: flex;
@@ -22,7 +22,7 @@ export const NavDot = styled.div<NavDotProps>`
   width: 10px;
   height: 10px;
 
-  background-color: ${({ theme, active }) => (active ? theme.COLOR.white : theme.COLOR.grey2)};
+  background-color: ${({ theme, active }) => (active ? theme.COLOR.grey3 : theme.COLOR.grey1)};
   border-radius: 50%;
 
   transition: all 0.3s ease;
