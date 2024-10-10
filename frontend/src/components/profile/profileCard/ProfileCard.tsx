6fded9bf07afeba7bd8801ccb4446bf6f18d2afb
Lee sang Yeop <mma7710@naver.com>
2024-10-11 01:48:00 +0900

    feat: 프로필카드에 받은 피드백이 없다면 기본 메시지가 보이도록 추가

--- frontend/src/components/profile/profileCard/ProfileCard.tsx
+++ frontend/src/components/profile/profileCard/ProfileCard.tsx
@@ -47,12 +47,19 @@ const ProfileCard = (profileData: ProfileData) => {
         </S.ProfileInfoWrapper>
 
         <S.KeywordContainer>
-          {profileData.feedbackKeywords.map((keyword) => (
-            <S.KeywordWrapper key={keyword}>
+          {profileData.feedbackKeywords.length !== 0 ? (
+            profileData.feedbackKeywords.map((keyword) => (
+              <S.KeywordWrapper key={keyword}>
+                <Icon kind="people" />
+                <S.Keyword>{keyword}</S.Keyword>
+              </S.KeywordWrapper>
+            ))
+          ) : (
+            <S.KeywordWrapper>
               <Icon kind="people" />
-              <S.Keyword>{keyword}</S.Keyword>
+              <S.Keyword>받은 피드백을 기반으로 키워드가 선정됩니다</S.Keyword>
             </S.KeywordWrapper>
-          ))}
+          )}
         </S.KeywordContainer>
       </S.ProfileCardWrapper>
 
