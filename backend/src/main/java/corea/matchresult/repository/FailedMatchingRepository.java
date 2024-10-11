20d4f03c781c8b16a3183730273ad5571a96611e
gyungchan Jo <namejgc@naver.com>
2024-10-12 02:49:36 +0900

    feat: 매칭을 실패했을 경우 실패한 매칭 정보를 저장하는 기능 구현

--- backend/src/main/java/corea/matchresult/repository/FailedMatchingRepository.java
+++ backend/src/main/java/corea/matchresult/repository/FailedMatchingRepository.java
@@ -3,5 +3,9 @@ package corea.matchresult.repository;
 import corea.matchresult.domain.FailedMatching;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FailedMatchingRepository extends JpaRepository<FailedMatching, Long> {
+
+    Optional<FailedMatching> findByRoomId(long roomId);
 }
