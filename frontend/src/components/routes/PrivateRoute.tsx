cc2ea5664d96a7b1b85768e290d3a26e9f234a4a
jinsil <jinsil378900@gmail.com>
2024-10-11 10:20:59 +0900

    feat: 홈 화면 바로가기 버튼 추가

--- frontend/src/components/routes/PrivateRoute.tsx
+++ frontend/src/components/routes/PrivateRoute.tsx
@@ -1,8 +1,10 @@
-import { Outlet } from "react-router-dom";
+import Button from "../common/button/Button";
+import { Outlet, useNavigate } from "react-router-dom";
 import * as S from "@/components/routes/PrivateRoute.style";
 import { errorCharacter } from "@/assets";
 
 const PrivateRoute = () => {
+  const navigate = useNavigate();
   const isLoggedIn = !!localStorage.getItem("accessToken");
 
   if (isLoggedIn) {
@@ -13,6 +15,7 @@ const PrivateRoute = () => {
     <S.PrivateRouteContainer>
       <img src={errorCharacter} alt="로그인 후 이용해주세요" />
       <p>로그인 후 이용 가능한 페이지입니다.</p>
+      <Button onClick={() => navigate("/")}>홈 화면으로 가기</Button>
     </S.PrivateRouteContainer>
   );
 };
