22ab3aab320a5d8c8bdb1aa50c2c4c54595ba3f3
gyungchan Jo <namejgc@naver.com>
2024-10-14 21:44:33 +0900

    test: 자동 예약 테스트 중복 코드 추상 클래스로 이동

--- backend/src/test/java/corea/scheduler/service/AutomaticUpdateExecutorTest.java
+++ backend/src/test/java/corea/scheduler/service/AutomaticUpdateExecutorTest.java
@@ -1,25 +1,15 @@
 package corea.scheduler.service;
 
-import config.ServiceTest;
-import config.TestAsyncConfig;
 import corea.fixture.MemberFixture;
-import corea.fixture.RoomFixture;
 import corea.matchresult.domain.MatchResult;
 import corea.matchresult.domain.ReviewStatus;
 import corea.matchresult.repository.MatchResultRepository;
-import corea.member.domain.Member;
-import corea.member.repository.MemberRepository;
-import corea.room.domain.Room;
-import corea.room.dto.RoomCreateRequest;
-import corea.room.repository.RoomRepository;
 import corea.scheduler.domain.AutomaticUpdate;
 import corea.scheduler.repository.AutomaticUpdateRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
@@ -33,34 +23,16 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
-@ServiceTest
-@Import(TestAsyncConfig.class)
-class AutomaticUpdateExecutorTest {
+class AutomaticUpdateExecutorTest extends UpdateTest {
 
     @Autowired
     private AutomaticUpdateExecutor automaticUpdateExecutor;
-
-    @Autowired
-    private RoomRepository roomRepository;
-
-    @Autowired
-    private MemberRepository memberRepository;
 
     @Autowired
     private AutomaticUpdateRepository automaticUpdateRepository;
 
     @MockBean
     private MatchResultRepository matchResultRepository;
-
-    private Room room;
-
-    @BeforeEach
-    void setUp() {
-        Member member = memberRepository.save(MemberFixture.MEMBER_PORORO());
-        RoomCreateRequest request = RoomFixture.ROOM_CREATE_REQUEST();
-
-        room = roomRepository.save(request.toEntity(member));
-    }
 
     @Test
     @DisplayName("동시에 10개의 자동 업데이트를 실행해도 PESSIMISTIC_WRITE 락을 통해 동시성을 제어할 수 있다.")
--- backend/src/test/java/corea/scheduler/service/UpdateExecutorTest.java
+++ backend/src/test/java/corea/scheduler/service/UpdateExecutorTest.java
@@ -1,51 +1,22 @@
 package corea.scheduler.service;
 
-import config.ServiceTest;
-import config.TestAsyncConfig;
-import corea.fixture.MemberFixture;
-import corea.fixture.RoomFixture;
-import corea.member.domain.Member;
-import corea.member.repository.MemberRepository;
-import corea.room.domain.Room;
 import corea.room.domain.RoomStatus;
-import corea.room.dto.RoomCreateRequest;
-import corea.room.repository.RoomRepository;
 import corea.scheduler.domain.AutomaticUpdate;
 import corea.scheduler.repository.AutomaticUpdateRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ServiceTest
-@Import(TestAsyncConfig.class)
-class UpdateExecutorTest {
+class UpdateExecutorTest extends UpdateTest {
 
     @Autowired
     private UpdateExecutor updateExecutor;
 
     @Autowired
-    private RoomRepository roomRepository;
-
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Autowired
     private AutomaticUpdateRepository automaticUpdateRepository;
-
-    private Room room;
-
-    @BeforeEach
-    void setUp() {
-        Member member = memberRepository.save(MemberFixture.MEMBER_PORORO());
-        RoomCreateRequest request = RoomFixture.ROOM_CREATE_REQUEST();
-
-        room = roomRepository.save(request.toEntity(member));
-    }
 
     @Transactional
     @Test
