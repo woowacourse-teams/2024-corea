9a45c8e5a3aae0d15915f97fa4e757dbe7819c74
jinsil <jinsil378900@gmail.com>
2024-10-14 16:20:22 +0900

    design: 세부 피드백 높이 지정

--- frontend/src/components/feedback/feedbackCard/FeedbackCard.style.ts
+++ frontend/src/components/feedback/feedbackCard/FeedbackCard.style.ts
@@ -103,6 +103,8 @@ export const FeedbackDetailContainer = styled.div`
 export const FeedbackDetail = styled.p`
   overflow: hidden auto;
 
+  height: 172px;
+
   font: ${({ theme }) => theme.TEXT.small};
   line-height: 2rem;
   text-overflow: ellipsis;
