39d15c099fbd060bb648d18eb06fa7ea801b51d0
00kang <kdb070970@gmail.com>
2024-10-17 16:27:37 +0900

    feat: 참여중 라벨 추가

--- frontend/src/components/common/label/Label.tsx
+++ frontend/src/components/common/label/Label.tsx
@@ -1,7 +1,7 @@
 import * as S from "@/components/common/label/Label.style";
 import { ThemeType } from "@/styles/theme";
 
-export type LabelType = "KEYWORD" | "OPEN" | "CLOSE" | "PROGRESS" | "FAIL";
+export type LabelType = "KEYWORD" | "PARTICIPATED" | "OPEN" | "PROGRESS" | "CLOSE" | "FAIL";
 export type LabelSize = keyof ThemeType["TEXT"];
 
 interface LabelProps {
@@ -15,9 +15,10 @@ const Label = ({ text, type, size = "semiSmall", backgroundColor }: LabelProps) 
   return (
     <S.LabelWrapper type={type} $size={size} $backgroundColor={backgroundColor}>
       {type === "KEYWORD" && `#${text}`}
+      {type === "PARTICIPATED" && "참여 중"}
       {type === "OPEN" && "모집 중"}
-      {type === "CLOSE" && "종료"}
       {type === "PROGRESS" && "진행 중"}
+      {type === "CLOSE" && "종료"}
       {type === "FAIL" && "매칭 실패"}
     </S.LabelWrapper>
   );
