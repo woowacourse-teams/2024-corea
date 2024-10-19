27f293320d90af0be68d2c1f1d68bfdee6be28ac
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-10-19 20:45:47 +0900

    refactor: 리뷰 클라이언트 패키지 이동 및 리팩토링

--- backend/src/main/java/corea/review/dto/GithubPullRequestReview.java
+++ backend/src/main/java/corea/review/dto/GithubPullRequestReview.java
@@ -1,6 +1,7 @@
-package corea.auth.dto;
+package corea.review.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import corea.auth.dto.GithubUserInfo;
 
 public record GithubPullRequestReview(
 
@@ -13,4 +14,7 @@ public record GithubPullRequestReview(
         @JsonProperty("html_url")
         String html_url
 ) {
+    public String getUserId() {
+        return user.id();
+    }
 }
