e6da6f69f490a7d317edb0f39f7c05b58c2ee08a
gyungchan Jo <namejgc@naver.com>
2024-10-19 19:41:13 +0900

    test: 소셜 피드백 테스트 추가

--- backend/src/test/java/corea/feedback/service/SocialFeedbackServiceTest.java
+++ backend/src/test/java/corea/feedback/service/SocialFeedbackServiceTest.java
@@ -2,6 +2,7 @@ package corea.feedback.service;
 
 import config.ServiceTest;
 import corea.exception.CoreaException;
+import corea.exception.ExceptionType;
 import corea.feedback.dto.SocialFeedbackCreateRequest;
 import corea.feedback.dto.SocialFeedbackResponse;
 import corea.feedback.dto.SocialFeedbackUpdateRequest;
@@ -14,6 +15,7 @@ import corea.member.domain.Member;
 import corea.member.repository.MemberRepository;
 import corea.room.domain.Room;
 import corea.room.repository.RoomRepository;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -67,8 +69,31 @@ class SocialFeedbackServiceTest {
         Member reviewer = memberRepository.save(MemberFixture.MEMBER_PORORO());
         Member reviewee = memberRepository.save(MemberFixture.MEMBER_YOUNGSU());
 
+        assertThatThrownBy(() -> socialFeedbackService.create(room.getId(), reviewee.getId(), createRequest(reviewer.getId())))
+                .asInstanceOf(InstanceOfAssertFactories.type(CoreaException.class))
+                .extracting(CoreaException::getExceptionType)
+                .isEqualTo(ExceptionType.NOT_MATCHED_MEMBER);
+    }
+
+    @Test
+    @DisplayName("소셜(리뷰이 -> 리뷰어) 에 대한 피드백이 이미 있다면 피드백을 생성할 때 예외를 발생한다.")
+    void throw_exception_when_already_feedback_exist() {
+        Member manager = memberRepository.save(MemberFixture.MEMBER_ROOM_MANAGER_JOYSON());
+        Room room = roomRepository.save(RoomFixture.ROOM_DOMAIN(manager));
+        Member reviewer = memberRepository.save(MemberFixture.MEMBER_PORORO());
+        Member reviewee = memberRepository.save(MemberFixture.MEMBER_YOUNGSU());
+        matchResultRepository.save(MatchResultFixture.MATCH_RESULT_DOMAIN(
+                room.getId(),
+                reviewer,
+                reviewee
+        ));
+
+        socialFeedbackService.create(room.getId(), reviewee.getId(), createRequest(reviewer.getId()));
+
         assertThatCode(() -> socialFeedbackService.create(room.getId(), reviewee.getId(), createRequest(reviewer.getId())))
-                .isInstanceOf(CoreaException.class);
+                .asInstanceOf(InstanceOfAssertFactories.type(CoreaException.class))
+                .extracting(CoreaException::getExceptionType)
+                .isEqualTo(ExceptionType.ALREADY_COMPLETED_FEEDBACK);
     }
 
     @Test
@@ -124,9 +149,30 @@ class SocialFeedbackServiceTest {
                 .isInstanceOf(CoreaException.class);
     }
 
-    private SocialFeedbackCreateRequest createRequest(long revieweeId) {
+    @Test
+    @DisplayName("소셜(리뷰어 -> 리뷰이) 피드백 작성자가 아닌 사람이 업데이트시 예외를 발생한다.")
+    void throw_exception_when_anonymous_updates_feedback() {
+        Member manager = memberRepository.save(MemberFixture.MEMBER_ROOM_MANAGER_JOYSON());
+        Room room = roomRepository.save(RoomFixture.ROOM_DOMAIN(manager));
+        Member reviewer = memberRepository.save(MemberFixture.MEMBER_PORORO());
+        Member reviewee = memberRepository.save(MemberFixture.MEMBER_YOUNGSU());
+        matchResultRepository.save(MatchResultFixture.MATCH_RESULT_DOMAIN(
+                room.getId(),
+                reviewer,
+                reviewee
+        ));
+
+        SocialFeedbackResponse createResponse = socialFeedbackService.create(room.getId(), reviewee.getId(), createRequest(reviewer.getId()));
+
+        assertThatThrownBy(() -> socialFeedbackService.update(createResponse.feedbackId(), reviewer.getId(), updateRequest()))
+                .asInstanceOf(InstanceOfAssertFactories.type(CoreaException.class))
+                .extracting(CoreaException::getExceptionType)
+                .isEqualTo(ExceptionType.FEEDBACK_UPDATE_AUTHORIZATION_ERROR);
+    }
+
+    private SocialFeedbackCreateRequest createRequest(long receiverId) {
         return new SocialFeedbackCreateRequest(
-                revieweeId,
+                receiverId,
                 4,
                 List.of("방의 목적에 맞게 코드를 작성했어요", "코드를 이해하기 쉬웠어요"),
                 "처음 자바를 접해봤다고 했는데 \n 생각보다 매우 구성되어 있는 코드 였던거 같습니다. ..."
