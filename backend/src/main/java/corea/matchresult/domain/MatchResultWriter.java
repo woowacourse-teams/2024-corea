4a40fe29d5488112d1761978a92c89c133fa0ba1
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-10-19 20:55:21 +0900

    fix: 충돌 해결

--- backend/src/main/java/corea/matchresult/domain/MatchResultWriter.java
+++ backend/src/main/java/corea/matchresult/domain/MatchResultWriter.java
@@ -1,5 +1,8 @@
 package corea.matchresult.domain;
 
+import corea.exception.CoreaException;
+import corea.exception.ExceptionType;
+import corea.matchresult.repository.MatchResultRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,8 +12,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class MatchResultWriter {
 
+    private final MatchResultRepository matchResultRepository;
+
     public void reviewComplete(MatchResult matchResult, String prLink) {
         matchResult.reviewComplete();
         matchResult.updateReviewLink(prLink);
+    }
+
+    public MatchResult completeDevelopFeedback(long roomId, long deliverId, long receiverId) {
+        MatchResult matchResult = matchResultRepository.findByRoomIdAndReviewerIdAndRevieweeId(roomId, deliverId, receiverId)
+                .orElseThrow(() -> new CoreaException(ExceptionType.NOT_MATCHED_MEMBER));
+
+        matchResult.reviewerCompleteFeedback();
+        return matchResult;
     }
 }
