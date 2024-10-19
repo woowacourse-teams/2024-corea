ac93d76a668390803e75a80898e806328c69013d
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-10-19 20:49:53 +0900

    refactor: 리뷰 서비스 리팩토링

--- backend/src/main/java/corea/review/service/ReviewService.java
+++ backend/src/main/java/corea/review/service/ReviewService.java
@@ -1,22 +1,19 @@
 package corea.review.service;
 
-import corea.auth.dto.GithubPullRequestReview;
-import corea.auth.service.GithubOAuthProvider;
 import corea.exception.CoreaException;
 import corea.exception.ExceptionType;
 import corea.matchresult.domain.MatchResult;
-import corea.matchresult.repository.MatchResultRepository;
-import corea.member.domain.Member;
-import corea.member.repository.MemberRepository;
-import corea.room.domain.Room;
-import corea.room.repository.RoomRepository;
+import corea.matchresult.domain.MatchResultReader;
+import corea.matchresult.domain.MatchResultWriter;
+import corea.review.dto.GithubPullRequestReview;
+import corea.review.infrastructure.GithubReviewProvider;
+import corea.room.domain.RoomReader;
+import corea.room.domain.RoomStatus;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -25,53 +22,28 @@ public class ReviewService {
 
     private static final Logger log = LogManager.getLogger(ReviewService.class);
 
-    private final GithubOAuthProvider githubOAuthProvider;
-    private final RoomRepository roomRepository;
-    private final MemberRepository memberRepository;
-    private final MatchResultRepository matchResultRepository;
+    private final GithubReviewProvider githubReviewProvider;
+    private final RoomReader roomReader;
+    private final MatchResultReader matchResultReader;
+    private final MatchResultWriter matchResultWriter;
 
     @Transactional
     public void completeReview(long roomId, long reviewerId, long revieweeId) {
-        Room room = getRoom(roomId);
-        validateRoomStatus(room);
-
-        MatchResult matchResult = getMatchResult(roomId, reviewerId, revieweeId);
-        matchResult.reviewComplete();
-        updateReviewLink(matchResult, reviewerId);
+        boolean isNotProgress = roomReader.isNotStatus(roomId, RoomStatus.PROGRESS);
+        if (isNotProgress) {
+            throw new CoreaException(ExceptionType.ROOM_STATUS_INVALID);
+        }
+        MatchResult matchResult = matchResultReader.findOne(roomId, reviewerId, revieweeId);
+        String prLink = getPrReviewLink(matchResult.getPrLink(), matchResult.getReviewerGithubId());
+        matchResultWriter.reviewComplete(matchResult, prLink);
 
         log.info("리뷰 완료[{매칭 ID({}), 리뷰어 ID({}, 리뷰이 ID({})", matchResult.getId(), reviewerId, revieweeId);
     }
 
-    private Room getRoom(long roomId) {
-        return roomRepository.findById(roomId)
-                .orElseThrow(() -> new CoreaException(ExceptionType.ROOM_NOT_FOUND, String.format("해당 Id의 방이 없습니다. 입력된 Id=%d", roomId)));
-    }
-
-    private void validateRoomStatus(Room room) {
-        if (room.isNotProgress()) {
-            throw new CoreaException(ExceptionType.ROOM_STATUS_INVALID);
-        }
-    }
-
-    private void updateReviewLink(MatchResult matchResult, long reviewerId) {
-        Member reviewer = memberRepository.findById(reviewerId)
-                .orElseThrow(() -> new CoreaException(ExceptionType.MEMBER_NOT_FOUND));
-        String userName = reviewer.getUsername();
-        String newReviewLink = findReviewLink(userName, matchResult.getPrLink());
-        matchResult.updateReviewLink(newReviewLink);
-    }
-
-    private String findReviewLink(String userName, String prLink) {
-        GithubPullRequestReview[] githubPullRequestReviews = githubOAuthProvider.getPullRequestReview(prLink);
-        return Stream.of(githubPullRequestReviews)
-                .filter(review -> review.user().login().equals(userName))
-                .findFirst()
+    private String getPrReviewLink(String prLink, String reviewerGithubId) {
+        return githubReviewProvider.getReviewWithPrLink(prLink)
+                .findWithGithubId(reviewerGithubId)
                 .map(GithubPullRequestReview::html_url)
                 .orElseThrow(() -> new CoreaException(ExceptionType.NOT_COMPLETE_GITHUB_REVIEW));
-    }
-
-    private MatchResult getMatchResult(long roomId, long reviewerId, long revieweeId) {
-        return matchResultRepository.findByRoomIdAndReviewerIdAndRevieweeId(roomId, reviewerId, revieweeId)
-                .orElseThrow(() -> new CoreaException(ExceptionType.NOT_MATCHED_MEMBER));
     }
 }
