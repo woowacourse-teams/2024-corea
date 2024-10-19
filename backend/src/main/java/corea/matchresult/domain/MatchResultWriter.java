e340649cd3ead1ea7cff38b24c88433f7b489d89
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-10-19 20:49:53 +0900

    refactor: 리뷰 서비스 리팩토링

--- backend/src/main/java/corea/matchresult/domain/MatchResultWriter.java
+++ backend/src/main/java/corea/matchresult/domain/MatchResultWriter.java
@@ -1,8 +1,5 @@
 package corea.matchresult.domain;
 
-import corea.exception.CoreaException;
-import corea.exception.ExceptionType;
-import corea.matchresult.repository.MatchResultRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,13 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class MatchResultWriter {
 
-    private final MatchResultRepository matchResultRepository;
-
-    public MatchResult completeDevelopFeedback(long roomId, long deliverId, long receiverId) {
-        MatchResult matchResult = matchResultRepository.findByRoomIdAndReviewerIdAndRevieweeId(roomId, deliverId, receiverId)
-                .orElseThrow(() -> new CoreaException(ExceptionType.NOT_MATCHED_MEMBER));
-
-        matchResult.reviewerCompleteFeedback();
-        return matchResult;
+    public void reviewComplete(MatchResult matchResult, String prLink) {
+        matchResult.reviewComplete();
+        matchResult.updateReviewLink(prLink);
     }
 }
