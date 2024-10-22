e190415204844aa0d6f8b69c801a9a2a815a8ac7
00kang <kdb070970@gmail.com>
2024-10-22 11:19:20 +0900

    feat: 모바일 버전에서 나타낼 SideNavBar 컴포넌트 구현

---
+++ frontend/src/components/common/header/SideNavBar.style.ts
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+
+export const SideNavBarContainer = styled.div<{ isOpen: boolean }>`
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  right: ${({ isOpen }) => (isOpen ? "0" : "-60%")};
+
+  width: 60%;
+  height: 100vh;
+
+  background-color: ${({ theme }) => theme.COLOR.white};
+
+  transition: right 0.3s ease-in-out;
+`;
+
+export const TopSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  padding: 1rem;
+
+  background-color: ${({ theme }) => theme.COLOR.primary2};
+`;
+
+export const ProfileWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  width: 100%;
+`;
+
+export const ProfileInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+  color: ${({ theme }) => theme.COLOR.white};
+
+  strong {
+    font: ${({ theme }) => theme.TEXT.medium_bold};
+  }
+
+  span {
+    font: ${({ theme }) => theme.TEXT.semiSmall};
+  }
+`;
+
+export const NavItems = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 0.6rem;
+
+  width: 100%;
+  padding: 1rem;
+
+  list-style-type: none;
+`;
+
+export const NavItem = styled.li`
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 42px;
+
+  font: ${({ theme }) => theme.TEXT.medium};
+  color: ${({ theme }) => theme.COLOR.black};
+  text-align: center;
+
+  &:hover,
+  &:focus {
+    background-color: ${({ theme }) => theme.COLOR.grey0};
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    height: 100%;
+
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+export const LogoutButton = styled(NavItem)`
+  color: ${({ theme }) => theme.COLOR.black};
+`;
---
+++ frontend/src/components/common/header/SideNavBar.tsx
@@ -0,0 +1,110 @@
+import { Link } from "react-router-dom";
+import useMutateAuth from "@/hooks/mutations/useMutateAuth";
+import Button from "@/components/common/button/Button";
+import * as S from "@/components/common/header/SideNavBar.style";
+import Icon from "@/components/common/icon/Icon";
+import Profile from "@/components/common/profile/Profile";
+import { githubAuthUrl } from "@/config/githubAuthUrl";
+
+const loggedInMenuItems = [
+  {
+    name: "마이페이지",
+    path: "/profile",
+  },
+  {
+    name: "피드백 모아보기",
+    path: "/feedback",
+  },
+  {
+    name: "코드리뷰가이드",
+    path: "/guide",
+  },
+];
+
+const commonMenuItems = [
+  {
+    name: "코드리뷰가이드",
+    path: "/guide",
+  },
+];
+
+interface SideNavBarProps {
+  isOpen: boolean;
+  onClose: () => void;
+  isLoggedIn: boolean;
+}
+
+const SideNavBar = ({ isOpen, onClose, isLoggedIn }: SideNavBarProps) => {
+  const userInfo = JSON.parse(localStorage.getItem("userInfo") || "{}");
+  const { postLogoutMutation } = useMutateAuth();
+
+  const handleLogoutClick = () => {
+    postLogoutMutation.mutate();
+    onClose();
+  };
+
+  return (
+    <S.SideNavBarContainer isOpen={isOpen}>
+      {isLoggedIn ? (
+        <>
+          <S.TopSection>
+            <Button onClick={onClose} size="xSmall">
+              <Icon kind="close" size="2.4rem" />
+            </Button>
+            <S.ProfileWrapper>
+              <Profile imgSrc={userInfo.avatar_url} size={44} />
+              <S.ProfileInfo>
+                <strong>{userInfo.name}</strong>
+                <span>{userInfo.email !== "" ? userInfo.email : "email 비공개"}</span>
+              </S.ProfileInfo>
+            </S.ProfileWrapper>
+            <S.LogoutButton
+              onClick={handleLogoutClick}
+              tabIndex={0}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") handleLogoutClick();
+              }}
+            >
+              로그아웃
+            </S.LogoutButton>
+          </S.TopSection>
+
+          <S.NavItems>
+            {loggedInMenuItems.map((item) => (
+              <S.NavItem key={item.name}>
+                <Link to={item.path} onClick={onClose}>
+                  {item.name}
+                </Link>
+              </S.NavItem>
+            ))}
+          </S.NavItems>
+        </>
+      ) : (
+        <>
+          <S.TopSection>
+            <Button onClick={onClose} size="xSmall">
+              <Icon kind="close" size="2.4rem" />
+            </Button>
+            <S.NavItem>
+              <a href={githubAuthUrl} onClick={onClose}>
+                로그인
+              </a>
+            </S.NavItem>
+          </S.TopSection>
+
+          <S.NavItems>
+            {commonMenuItems.map((item) => (
+              <S.NavItem key={item.name}>
+                <Link to={item.path} onClick={onClose}>
+                  {item.name}
+                </Link>
+              </S.NavItem>
+            ))}
+          </S.NavItems>
+        </>
+      )}
+    </S.SideNavBarContainer>
+  );
+};
+
+export default SideNavBar;
