b052437c95e4cd1627294d3ee4854825f908bffe
gyungchan Jo <namejgc@naver.com>
2024-10-23 01:50:07 +0900

    refactor: pr에 작성된 모든 리뷰들을 가져오는 기능 수정

--- backend/src/main/java/corea/review/infrastructure/GithubReviewClient.java
+++ backend/src/main/java/corea/review/infrastructure/GithubReviewClient.java
@@ -1,12 +1,15 @@
 package corea.review.infrastructure;
 
 import corea.auth.infrastructure.GithubProperties;
-import corea.auth.infrastructure.GithubPullRequestUrlExchanger;
 import corea.review.dto.GithubPullRequestReview;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
@@ -17,12 +20,31 @@ public class GithubReviewClient {
 
     private final RestClient restClient;
 
-    public GithubPullRequestReview[] getReviewLink(String prLink) {
-        String url = GithubPullRequestUrlExchanger.pullRequestUrlToReview(prLink);
+    public List<GithubPullRequestReview> getReviewLink(String prLink) {
+        String reviewUrl = GithubPullRequestUrlExchanger.pullRequestUrlToReview(prLink);
+
+        return Stream.iterate(1, page -> page + 1)
+                .map(page -> getGithubPullRequestReviews(page, reviewUrl))
+                .takeWhile(this::isLastPage)
+                .flatMap(Arrays::stream)
+                .toList();
+    }
+
+    private GithubPullRequestReview[] getGithubPullRequestReviews(Integer page, String reviewUrl) {
+        String url = convertToPageUrl(page, reviewUrl);
+
         return restClient.get()
                 .uri(url)
                 .accept(APPLICATION_JSON)
                 .retrieve()
                 .body(GithubPullRequestReview[].class);
+    }
+
+    private String convertToPageUrl(Integer page, String reviewUrl) {
+        return reviewUrl + "?page=" + page + "&per_page=100";
+    }
+
+    private boolean isLastPage(GithubPullRequestReview[] githubPullRequestReviews) {
+        return githubPullRequestReviews.length > 0;
     }
 }
--- backend/src/test/java/corea/review/infrastructure/GithubReviewClientTest.java
+++ backend/src/test/java/corea/review/infrastructure/GithubReviewClientTest.java
@@ -1,16 +1,17 @@
-package corea.auth.infrastructure;
+package corea.review.infrastructure;
 
 import corea.review.dto.GithubPullRequestReview;
-import corea.review.infrastructure.GithubReviewClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class GithubPullRequestReviewClientTest {
+class GithubReviewClientTest {
 
     @Autowired
     private GithubReviewClient client;
@@ -23,10 +24,10 @@ class GithubPullRequestReviewClientTest {
         String prLink3 = "https://github.com/woowacourse-teams/2024-corea/pull/114";
         String prLink4 = "https://github.com/woowacourse-teams/2024-corea/pull/8";
 
-        GithubPullRequestReview[] reviews1 = client.getReviewLink(prLink1);
-        GithubPullRequestReview[] reviews2 = client.getReviewLink(prLink2);
-        GithubPullRequestReview[] reviews3 = client.getReviewLink(prLink3);
-        GithubPullRequestReview[] reviews4 = client.getReviewLink(prLink4);
+        List<GithubPullRequestReview> reviews1 = client.getReviewLink(prLink1);
+        List<GithubPullRequestReview> reviews2 = client.getReviewLink(prLink2);
+        List<GithubPullRequestReview> reviews3 = client.getReviewLink(prLink3);
+        List<GithubPullRequestReview> reviews4 = client.getReviewLink(prLink4);
 
         assertThat(reviews1).hasSize(1);
         assertThat(reviews2).hasSize(8);
