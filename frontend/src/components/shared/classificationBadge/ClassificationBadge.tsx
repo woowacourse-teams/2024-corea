7860a3ba034eed6180e60a6f38ba3c24fc2a8aff
Lee sang Yeop <mma7710@naver.com>
2024-10-22 15:42:59 +0900

    feat: 룸카드 뱃지의 접근성 추가

--- frontend/src/components/shared/classificationBadge/ClassificationBadge.tsx
+++ frontend/src/components/shared/classificationBadge/ClassificationBadge.tsx
@@ -2,8 +2,28 @@ import * as S from "@/components/shared/classificationBadge/ClassificationBadge.
 import { Classification } from "@/@types/roomInfo";
 import { classificationText } from "@/utils/roomInfoUtils";
 
+const convertToKorean = (text: Classification) => {
+  switch (text) {
+    case "ALL":
+      return "전체";
+    case "ANDROID":
+      return "안드로이드";
+    case "BACKEND":
+      return "백엔드";
+    case "FRONTEND":
+      return "프론트엔드";
+  }
+};
+
 const ClassificationBadge = ({ text }: { text: Classification }) => {
-  return <S.StyledBadge $text={text}>{classificationText(text)}</S.StyledBadge>;
+  return (
+    <>
+      <S.StyledBadge $text={text} aria-hidden>
+        {classificationText(text)}
+      </S.StyledBadge>
+      <S.ScreenReader>{`모집분야 ${convertToKorean(text)}`}</S.ScreenReader>
+    </>
+  );
 };
 
 export default ClassificationBadge;
