1860f017bd04d11091188608bde2f1f142ece801
Lee sang Yeop <mma7710@naver.com>
2024-10-22 20:17:38 +0900

    feat: 모바일 사이드바에서 문의 탭 추가

--- frontend/src/components/common/header/SideNavBar.tsx
+++ frontend/src/components/common/header/SideNavBar.tsx
@@ -101,6 +101,15 @@ const SideNavBar = ({ isOpen, onClose, isLoggedIn }: SideNavBarProps) => {
                 </Link>
               </S.NavItem>
             ))}
+            <S.NavItem>
+              <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSeuuEJ6oCnRi5AYjlKhWt-H5EEibZPIRLA4lY-5oqZpC2b0SA/viewform"
+                target="_blank"
+                rel="noreferrer"
+              >
+                문의
+              </a>
+            </S.NavItem>
           </S.NavItems>
         </>
       ) : (
@@ -124,6 +133,15 @@ const SideNavBar = ({ isOpen, onClose, isLoggedIn }: SideNavBarProps) => {
                 </Link>
               </S.NavItem>
             ))}
+            <S.NavItem>
+              <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSeuuEJ6oCnRi5AYjlKhWt-H5EEibZPIRLA4lY-5oqZpC2b0SA/viewform"
+                target="_blank"
+                rel="noreferrer"
+              >
+                문의
+              </a>
+            </S.NavItem>
           </S.NavItems>
         </>
       )}
