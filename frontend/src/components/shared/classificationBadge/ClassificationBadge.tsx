8019789aed855357fa486bfa05c0ad7e1dc05857
Lee sang Yeop <mma7710@naver.com>
2024-10-23 14:05:18 +0900

    refactor: 뱃지의 종류를 한국어로 변환해주는 로직을 유틸 함수로 분리

--- frontend/src/components/shared/classificationBadge/ClassificationBadge.tsx
+++ frontend/src/components/shared/classificationBadge/ClassificationBadge.tsx
@@ -1,19 +1,7 @@
 import * as S from "@/components/shared/classificationBadge/ClassificationBadge.style";
 import { Classification } from "@/@types/roomInfo";
+import convertClassificationToKorean from "@/utils/convertClassificationToKorean";
 import { classificationText } from "@/utils/roomInfoUtils";
-
-const convertToKorean = (text: Classification) => {
-  switch (text) {
-    case "ALL":
-      return "전체";
-    case "ANDROID":
-      return "안드로이드";
-    case "BACKEND":
-      return "백엔드";
-    case "FRONTEND":
-      return "프론트엔드";
-  }
-};
 
 const ClassificationBadge = ({ text }: { text: Classification }) => {
   return (
@@ -21,7 +9,7 @@ const ClassificationBadge = ({ text }: { text: Classification }) => {
       <S.StyledBadge $text={text} aria-hidden>
         {classificationText(text)}
       </S.StyledBadge>
-      <S.ScreenReader>{`모집분야 ${convertToKorean(text)}`}</S.ScreenReader>
+      <S.ScreenReader>{`모집분야 ${convertClassificationToKorean(text)}`}</S.ScreenReader>
     </>
   );
 };
