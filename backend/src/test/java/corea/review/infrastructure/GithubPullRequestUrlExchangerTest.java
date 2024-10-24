356f625a5d30f865758252b79bfdfb1fb2d9418d
gyungchan Jo <namejgc@naver.com>
2024-10-24 17:42:41 +0900

    test: 테스트 메서드 위치 변경

--- backend/src/test/java/corea/review/infrastructure/GithubPullRequestUrlExchangerTest.java
+++ backend/src/test/java/corea/review/infrastructure/GithubPullRequestUrlExchangerTest.java
@@ -1,16 +1,11 @@
 package corea.review.infrastructure;
 
-import corea.exception.CoreaException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Disabled
 class GithubPullRequestUrlExchangerTest {
@@ -23,8 +18,8 @@ class GithubPullRequestUrlExchangerTest {
     }
 
     @Test
-    @DisplayName("올바른 Pull Request URL 을 상응하는 Github API 요청 주소로 변환한다.")
-    void test() {
+    @DisplayName("올바른 Pull Request URL 을 상응하는 Github API review 요청 주소로 변환한다.")
+    void prLinkToReviewApiUrl() {
         String test = "https://github.com/woowacourse-teams/2024-corea/pull/1";
         String expected = "https://api.github.com/repos/woowacourse-teams/2024-corea/pulls/1/reviews";
 
@@ -32,31 +27,11 @@ class GithubPullRequestUrlExchangerTest {
     }
 
     @Test
-    @DisplayName("https:// 로 시작하지 않는 주소에 대해 예외를 발생한다.")
-    void validate1() {
-        String test = "http://github.com/woowacourse-teams/2024-corea/pull/1";
+    @DisplayName("올바른 Pull Request URL 을 상응하는 Github API comment 요청 주소로 변환한다.")
+    void prLinkToCommentApiUrl() {
+        String test = "https://github.com/woowacourse-teams/2024-corea/pull/1";
+        String expected = "https://api.github.com/repos/woowacourse-teams/2024-corea/issues/1/comments";
 
-        assertThatThrownBy(() -> githubPullRequestUrlExchanger.prLinkToReviewApiUrl(test))
-                .isInstanceOf(CoreaException.class);
-    }
-
-    @ParameterizedTest
-    @NullAndEmptySource
-    @DisplayName("빈 주소에 대해 예외를 발생한다.")
-    void validate2(String test) {
-        assertThatThrownBy(() -> githubPullRequestUrlExchanger.prLinkToReviewApiUrl(test))
-                .isInstanceOf(CoreaException.class);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {
-            "https://githubs.com/woowacourse-teams/2024-corea/pull/1",
-            "https://github.com/woowacourse-teams/2024-corea/pulls/1",
-            "https://github.com/woowacourse-teams/2024-corea/pulls/1/3",
-            "https://github.com/woowacourse-teams/2024-corea/pull"})
-    @DisplayName("올바르지 않은 Pull Request 주소에 대해 예외를 발생한다.")
-    void validate3(String test) {
-        assertThatThrownBy(() -> githubPullRequestUrlExchanger.prLinkToReviewApiUrl(test))
-                .isInstanceOf(CoreaException.class);
+        assertThat(githubPullRequestUrlExchanger.prLinkToCommentApiUrl(test)).isEqualTo(expected);
     }
 }
--- backend/src/test/java/corea/review/infrastructure/GithubReviewProviderTest.java
+++ backend/src/test/java/corea/review/infrastructure/GithubReviewProviderTest.java
@@ -1,14 +1,19 @@
 package corea.review.infrastructure;
 
+import corea.exception.CoreaException;
 import corea.review.dto.GithubPullRequestReview;
 import corea.review.dto.GithubPullRequestReviewInfo;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Disabled
 @SpringBootTest
@@ -16,6 +21,35 @@ class GithubReviewProviderTest {
 
     @Autowired
     private GithubReviewProvider githubReviewProvider;
+
+    @Test
+    @DisplayName("https:// 로 시작하지 않는 주소에 대해 예외를 발생한다.")
+    void validate1() {
+        String test = "http://github.com/woowacourse-teams/2024-corea/pull/1";
+
+        assertThatThrownBy(() -> githubReviewProvider.provideReviewInfo(test))
+                .isInstanceOf(CoreaException.class);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("빈 주소에 대해 예외를 발생한다.")
+    void validate2(String test) {
+        assertThatThrownBy(() -> githubReviewProvider.provideReviewInfo(test))
+                .isInstanceOf(CoreaException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "https://githubs.com/woowacourse-teams/2024-corea/pull/1",
+            "https://github.com/woowacourse-teams/2024-corea/pulls/1",
+            "https://github.com/woowacourse-teams/2024-corea/pulls/1/3",
+            "https://github.com/woowacourse-teams/2024-corea/pull"})
+    @DisplayName("올바르지 않은 Pull Request 주소에 대해 예외를 발생한다.")
+    void validate3(String test) {
+        assertThatThrownBy(() -> githubReviewProvider.provideReviewInfo(test))
+                .isInstanceOf(CoreaException.class);
+    }
 
     @Test
     @DisplayName("한 사람이 하나의 리뷰를 남겼을 시, 리뷰한 사람의 리뷰 링크를 찾는다.")
