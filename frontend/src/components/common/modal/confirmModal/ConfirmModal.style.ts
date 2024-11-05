ffc9df63199f376538e0896eccda09a10cf58972
jinsil <jinsil378900@gmail.com>
2024-11-05 17:57:25 +0900

    design: 확인 모달 버튼 outline 속성 추가

--- frontend/src/components/common/modal/confirmModal/ConfirmModal.style.ts
+++ frontend/src/components/common/modal/confirmModal/ConfirmModal.style.ts
@@ -16,10 +16,4 @@ export const ConfirmModalContainer = styled.div`
 export const ButtonWrapper = styled.div`
   display: flex;
   justify-content: space-evenly;
-
-  .cancel-button {
-    color: ${({ theme }) => theme.COLOR.primary2};
-    background-color: ${({ theme }) => theme.COLOR.white};
-    border: 2px solid ${({ theme }) => theme.COLOR.primary2};
-  }
 `;
