b2f705e7c094d3f7e07d7964b880f0796d4ac7e9
jinsil <jinsil378900@gmail.com>
2024-11-05 15:15:10 +0900

    design: 버튼 xSmall 디자인 수정

--- frontend/src/components/common/button/Button.style.ts
+++ frontend/src/components/common/button/Button.style.ts
@@ -68,7 +68,7 @@ const sizeStyles = {
   xSmall: css`
     width: fit-content;
     max-width: 120px;
-    padding: 1rem 0.5rem;
+    padding: 1rem 0.3rem;
 
     font: ${({ theme }) => theme.TEXT.semiSmall};
 
