933235e28d9d858b87050efe1a8d7e78791289cc
00kang <kdb070970@gmail.com>
2024-11-07 23:17:13 +0900

    feat: 알람페이지의 알람항목 ui 구현

--- frontend/src/pages/alarm/AlarmPage.style.ts
+++ frontend/src/pages/alarm/AlarmPage.style.ts
@@ -1,7 +1,77 @@
 import styled from "styled-components";
+import media from "@/styles/media";
 
 export const Layout = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+export const AlarmList = styled.ul`
+  ${media.small`
+    width: 100%;
+  `}
+
+  ${media.medium`
+    width: 100%;
+    min-width: 590px;
+    max-width: 800px;
+  `}
+
+  ${media.large`
+    width: 800px;
+  `}
+`;
+
+export const AlarmItem = styled.li`
+  position: relative;
+
+  display: grid;
+  grid-template-columns: 40px 1fr;
+  gap: 2rem;
+  align-items: center;
+
+  width: 100%;
+  min-height: 80px;
+  padding: 1rem 0;
+
+  font: ${({ theme }) => theme.TEXT.small};
+`;
+
+export const ProfileWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 40px;
+  height: 100%;
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  gap: 0.6rem;
+
+  text-align: left;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+
+  font: ${({ theme }) => theme.TEXT.small};
+  line-height: normal;
+  color: ${({ theme }) => theme.COLOR.grey4};
+  word-break: break-all;
+
+  span {
+    font: ${({ theme }) => theme.TEXT.small_bold};
+  }
+`;
+
+export const TimeStamp = styled.div`
+  width: 100%;
+  font: ${({ theme }) => theme.TEXT.xSmall};
+  color: ${({ theme }) => theme.COLOR.grey2};
+  white-space: nowrap;
 `;
--- frontend/src/pages/alarm/AlarmPage.tsx
+++ frontend/src/pages/alarm/AlarmPage.tsx
@@ -1,7 +1,25 @@
+import Profile from "@/components/common/profile/Profile";
 import * as S from "@/pages/alarm/AlarmPage.style";
 
 const AlarmPage = () => {
-  return <S.Layout></S.Layout>;
+  return (
+    <S.Layout>
+      <S.AlarmList>
+        <S.AlarmItem>
+          <S.ProfileWrapper>
+            <Profile imgSrc={"www.myProfile.jpg"} size={40} />
+          </S.ProfileWrapper>
+          <S.ContentWrapper>
+            <S.Content>
+              <span>00kang</span> 님이 <span>프리코스 3주차 로또 미션 - JavaScript</span> 미션에
+              대해 코드리뷰를 완료했습니다.
+            </S.Content>
+            <S.TimeStamp>2024년 44월 44일</S.TimeStamp>
+          </S.ContentWrapper>
+        </S.AlarmItem>
+      </S.AlarmList>
+    </S.Layout>
+  );
 };
 
 export default AlarmPage;
