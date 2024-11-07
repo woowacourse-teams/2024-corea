b481ded08a0959762ba0f4a3d197537c3faa4d1a
00kang <kdb070970@gmail.com>
2024-11-08 00:17:15 +0900

    refactor: 임의의 데이터를 실제 데이터에 맞게 알람페이지 수정

--- frontend/src/pages/alarm/AlarmPage.tsx
+++ frontend/src/pages/alarm/AlarmPage.tsx
@@ -1,22 +1,40 @@
+import { useFetchAlarmList } from "@/hooks/queries/useFetchAlarm";
 import Profile from "@/components/common/profile/Profile";
 import * as S from "@/pages/alarm/AlarmPage.style";
 
 const AlarmPage = () => {
+  const { data } = useFetchAlarmList();
+  const alarmListData = data?.data;
+
+  const getAlarmMessage = (actionType: string, username: string, info: string) => {
+    if (actionType === "REVIEW_COMPLETE") {
+      return (
+        <>
+          <span>{username}</span> 님이 <span>{info}</span> 미션에 대해 코드리뷰를 완료했습니다.
+        </>
+      );
+    }
+    return "";
+  };
+
   return (
     <S.Layout>
       <S.AlarmList>
-        <S.AlarmItem>
-          <S.ProfileWrapper>
-            <Profile imgSrc={"www.myProfile.jpg"} size={40} />
-          </S.ProfileWrapper>
-          <S.ContentWrapper>
-            <S.Content>
-              <span>00kang</span> 님이 <span>프리코스 3주차 로또 미션 - JavaScript</span> 미션에
-              대해 코드리뷰를 완료했습니다.
-            </S.Content>
-            <S.TimeStamp>2024년 44월 44일</S.TimeStamp>
-          </S.ContentWrapper>
-        </S.AlarmItem>
+        {alarmListData?.map((alarm) => (
+          <S.AlarmItem key={alarm.alarmId}>
+            {alarm.actor && (
+              <S.ProfileWrapper>
+                <Profile imgSrc={alarm.actor.thumbnailUrl} size={40} />
+              </S.ProfileWrapper>
+            )}
+            <S.ContentWrapper>
+              <S.Content>
+                {getAlarmMessage(alarm.actionType, alarm.actor.username, alarm.interaction.info)}
+              </S.Content>
+              <S.TimeStamp>{alarm.createAt}</S.TimeStamp>
+            </S.ContentWrapper>
+          </S.AlarmItem>
+        ))}
       </S.AlarmList>
     </S.Layout>
   );
