e2f7d78a60eb097ee6818454450148ce176ffba0
hjk0761 <hjk0761@naver.com>
2024-11-09 18:05:03 +0900

    test: 재촉 알람 테스트 구현

--- backend/src/test/java/corea/alarm/service/AlarmServiceTest.java
+++ backend/src/test/java/corea/alarm/service/AlarmServiceTest.java
@@ -22,6 +22,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ServiceTest
@@ -47,10 +49,10 @@ class AlarmServiceTest {
 
     @BeforeEach
     void setUp() {
-        this.actor = memberRepository.save(MemberFixture.MEMBER_MOVIN());
-        this.receiver = memberRepository.save(MemberFixture.MEMBER_PORORO());
-        this.notReceiver = memberRepository.save(MemberFixture.MEMBER_ASH());
-        this.interaction = roomRepository.save(RoomFixture.ROOM_DOMAIN(memberRepository.save(MemberFixture.MEMBER_ROOM_MANAGER_JOYSON())));
+        actor = memberRepository.save(MemberFixture.MEMBER_MOVIN());
+        receiver = memberRepository.save(MemberFixture.MEMBER_PORORO());
+        notReceiver = memberRepository.save(MemberFixture.MEMBER_ASH());
+        interaction = roomRepository.save(RoomFixture.ROOM_DOMAIN(memberRepository.save(MemberFixture.MEMBER_ROOM_MANAGER_JOYSON())));
         interactionId = interaction.getId();
     }
 
@@ -107,10 +109,32 @@ class AlarmServiceTest {
     @Test
     @Transactional
     @DisplayName("알림 체크를 한다.")
-    void some() {
+    void check_alarm() {
         UserToUserAlarm alarm = userToUserAlarmRepository.save(AlarmFixture.REVIEW_COMPLETE(actor.getId(), receiver.getId(), interactionId));
 
         alarmService.checkAlarm(receiver.getId(), new AlarmCheckRequest(alarm.getId(), "USER"));
         assertThat(alarm.isRead()).isTrue();
+    }
+
+    @Test
+    @DisplayName("리뷰어가 리뷰하지 않은 경우 재촉 알람을 발송한다.")
+    void urge_alarm() {
+        userToUserAlarmRepository.save(AlarmFixture.READ_URGE_REVIEW(actor.getId(), receiver.getId(), interactionId));
+        alarmService.createUrgeAlarm(actor.getId(), receiver.getId(), interactionId);
+
+        List<UserToUserAlarm> urgeAlarms = userToUserAlarmRepository.findAllByActorIdAndReceiverIdAndInteractionId(actor.getId(), receiver.getId(), interactionId);
+
+        assertThat(urgeAlarms).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("안 읽은 재촉 알람이 존재할 때 알람을 생성하지 않는다.")
+    void does_not_create_urge_alarm_when_unread_urge_alarm_exist() {
+        userToUserAlarmRepository.save(AlarmFixture.URGE_REVIEW(actor.getId(), receiver.getId(), interactionId));
+        alarmService.createUrgeAlarm(actor.getId(), receiver.getId(), interactionId);
+
+        List<UserToUserAlarm> urgeAlarms = userToUserAlarmRepository.findAllByActorIdAndReceiverIdAndInteractionId(actor.getId(), receiver.getId(), interactionId);
+
+        assertThat(urgeAlarms).hasSize(1);
     }
 }
