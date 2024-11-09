93344056fd7161b244827396e4d585a1a5b7ce53
hjk0761 <hjk0761@naver.com>
2024-11-09 18:06:09 +0900

    feat: 리뷰 재촉 기능 구현

--- backend/src/main/java/corea/review/controller/ReviewControllerSpecification.java
+++ backend/src/main/java/corea/review/controller/ReviewControllerSpecification.java
@@ -4,6 +4,7 @@ import corea.auth.domain.AuthInfo;
 import corea.exception.ExceptionType;
 import corea.global.annotation.ApiErrorResponses;
 import corea.review.dto.ReviewRequest;
+import corea.review.dto.UrgeRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,18 @@ public interface ReviewControllerSpecification {
                     "JWT 토큰에서 추출된 사용자 정보는 피드백 작성에 필요한 인증된 사용자 정보를 제공합니다. " +
                     "<br><br>**참고:** 이 API를 사용하기 위해서는 유효한 JWT 토큰이 필요하며, " +
                     "토큰이 없거나 유효하지 않은 경우 인증 오류가 발생합니다.")
-    @ApiErrorResponses(value = {ExceptionType.NOT_MATCHED_MEMBER,ExceptionType.ROOM_STATUS_IS_NOT_PROGRESS})
+    @ApiErrorResponses(value = {ExceptionType.NOT_MATCHED_MEMBER, ExceptionType.ROOM_STATUS_IS_NOT_PROGRESS})
     ResponseEntity<Void> complete(AuthInfo authInfo,
                                   ReviewRequest request);
+
+    @Operation(summary = "리뷰 재촉 알람을 보냅니다.",
+            description = "자신에게 배정된 리뷰어에게 리뷰 재촉 알람을 보낼 수 있습니다. <br>" +
+                    "요청 시 `Authorization Header`에 `Bearer JWT token`을 포함시켜야 합니다. " +
+                    "이 토큰을 기반으로 `AuthInfo` 객체가 생성되며 사용자의 정보가 자동으로 주입됩니다. <br>" +
+                    "JWT 토큰에서 추출된 사용자 정보는 피드백 작성에 필요한 인증된 사용자 정보를 제공합니다. " +
+                    "<br><br>**참고:** 이 API를 사용하기 위해서는 유효한 JWT 토큰이 필요하며, " +
+                    "토큰이 없거나 유효하지 않은 경우 인증 오류가 발생합니다.")
+    @ApiErrorResponses(value = {ExceptionType.ALREADY_COMPLETED_REVIEW, ExceptionType.ROOM_STATUS_IS_NOT_PROGRESS})
+    ResponseEntity<Void> urge(AuthInfo authInfo,
+                                  UrgeRequest request);
 }
