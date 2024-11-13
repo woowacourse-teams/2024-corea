838984c4448e17c48ae2265c69646a28866727ea
jinsil <jinsil378900@gmail.com>
2024-11-14 02:20:34 +0900

    design: 방 생성 페이지 디자인 변경

--- frontend/src/components/roomForm/RoomFormLayout.style.ts
+++ frontend/src/components/roomForm/RoomFormLayout.style.ts
@@ -1,48 +1,75 @@
 import styled from "styled-components";
 import media from "@/styles/media";
 
-export const CreateSection = styled.section`
+export const RoomFormContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 4rem;
   align-items: center;
 
   width: 100%;
+  min-width: 335px;
+  max-width: 700px;
+  margin: auto;
   margin-bottom: 20rem;
-  padding: 4rem;
+`;
 
-  border: 1px solid ${({ theme }) => theme.COLOR.grey1};
-  border-radius: 8px;
+export const SectionTitle = styled.h1`
+  display: flex;
+  gap: 1rem;
+  align-items: flex-end;
+
+  width: 100%;
+
+  font: ${({ theme }) => theme.TEXT.xLarge_bold};
+  color: ${({ theme }) => theme.COLOR.grey4};
+`;
+
+export const HelpText = styled.p`
+  font: ${({ theme }) => theme.TEXT.semiSmall};
+  color: ${({ theme }) => theme.COLOR.grey3};
+`;
+
+export const SubSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+
+  width: 100%;
+  padding: 2rem;
+
+  border: 1px solid ${({ theme }) => theme.COLOR.grey0};
+  border-radius: 6px;
   box-shadow: ${({ theme }) => theme.BOX_SHADOW.regular};
 `;
 
 export const RowContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   gap: 1rem;
   align-items: flex-start;
 
   width: 100%;
 
-  ${media.large`
-    flex-direction: row;
-    align-items: flex-start;
+  ${media.small`
+    flex-direction: column;
   `}
 `;
 
 export const ContentLabel = styled.span`
   flex-shrink: 0;
-  width: 250px;
+  width: 200px;
   font: ${({ theme }) => theme.TEXT.medium_bold};
-
-  span {
-    font: ${({ theme }) => theme.TEXT.semiSmall};
-    color: ${({ theme }) => theme.COLOR.error};
-  }
+  line-height: 30px;
 
   ${media.large`
-    line-height: 30px;
+    width: 250px;
   `}
+`;
+
+export const RequiredLabel = styled.span`
+  font: ${({ theme }) => theme.TEXT.medium_bold};
+  color: ${({ theme }) => theme.COLOR.error};
 `;
 
 export const ContentInput = styled.div`
--- frontend/src/components/roomForm/RoomFormLayout.tsx
+++ frontend/src/components/roomForm/RoomFormLayout.tsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import useModal from "@/hooks/common/useModal";
 import useMutateRoom from "@/hooks/mutations/useMutateRoom";
 import Button from "@/components/common/button/Button";
-import ContentSection from "@/components/common/contentSection/ContentSection";
 import Dropdown, { DropdownItem } from "@/components/common/dropdown/Dropdown";
 import { Input } from "@/components/common/input/Input";
 import ConfirmModal from "@/components/common/modal/confirmModal/ConfirmModal";
@@ -31,15 +30,15 @@ interface RoomFormLayoutProps {
 
 const getInitialFormState = (data?: RoomInfo): CreateRoomInfo => ({
   title: data?.title || "",
+  classification: data?.classification || ("" as Classification),
   content: data?.content || "",
   repositoryLink: data?.repositoryLink || "",
   thumbnailLink: data?.thumbnailLink || "",
+  keywords: data?.keywords.filter((keyword) => keyword !== "") || [],
   matchingSize: data?.matchingSize || 1,
-  keywords: data?.keywords || [],
   limitedParticipants: data?.limitedParticipants || 1,
   recruitmentDeadline: data ? new Date(data.recruitmentDeadline) : new Date(),
   reviewDeadline: data ? new Date(data.reviewDeadline) : new Date(),
-  classification: data?.classification || ("" as Classification),
 });
 
 const RoomFormLayout = ({ formType, roomId, data }: RoomFormLayoutProps) => {
@@ -52,7 +51,10 @@ const RoomFormLayout = ({ formType, roomId, data }: RoomFormLayoutProps) => {
   const isFormValid =
     validators.title(formState.title) === "" &&
     validators.classification(formState.classification) === "" &&
+    validators.content(formState.content) === "" &&
     validators.repositoryLink(formState.repositoryLink) === "" &&
+    validators.thumbnailLink(formState.thumbnailLink) === "" &&
+    validators.keywords(formState.keywords) === "" &&
     validators.matchingSize(formState.matchingSize) === "" &&
     validators.limitedParticipants(formState.limitedParticipants, formState.matchingSize) === "" &&
     validators.recruitmentDeadline(formState.recruitmentDeadline) === "" &&
@@ -87,7 +89,7 @@ const RoomFormLayout = ({ formType, roomId, data }: RoomFormLayoutProps) => {
   };
 
   return (
-    <ContentSection title={formType === "create" ? "방 생성하기" : "방 정보 수정하기"}>
+    <>
       <ConfirmModal
         isOpen={isModalOpen}
         onClose={handleCloseModal}
@@ -97,203 +99,221 @@ const RoomFormLayout = ({ formType, roomId, data }: RoomFormLayoutProps) => {
         {formType === "create" ? MESSAGES.GUIDANCE.CREATE_ROOM : MESSAGES.GUIDANCE.EDIT_ROOM}
       </ConfirmModal>
 
-      <S.CreateSection>
-        <S.RowContainer>
-          <S.ContentLabel>
-            제목 <span>*필수입력</span>
-          </S.ContentLabel>
-          <S.ContentInput>
-            <Input
-              name="title"
-              value={formState.title}
-              onChange={(e) => handleInputChange("title", e.target.value)}
-              showCharCount={true}
-              maxLength={50}
-              error={isClickedButton && validators.title(formState.title) !== ""}
-              required
-            />
-            <ErrorText>{isClickedButton && validators.title(formState.title)}</ErrorText>
-          </S.ContentInput>
-        </S.RowContainer>
+      <S.RoomFormContainer>
+        <S.SectionTitle>
+          {formType === "create" ? "방 생성하기" : "방 정보 수정하기"}
+          <S.HelpText>
+            <S.RequiredLabel>*</S.RequiredLabel>표시는 필수 입력 항목입니다.
+          </S.HelpText>
+        </S.SectionTitle>
 
-        <S.RowContainer>
-          <S.ContentLabel>
-            분류 <span>*필수입력</span>
-          </S.ContentLabel>
-          <S.ContentInput>
-            <Dropdown
-              name="classification"
-              dropdownItems={dropdownItems}
-              selectedCategory={formState.classification}
-              onSelectCategory={(value) =>
-                handleInputChange("classification", value as Classification)
-              }
-              error={isClickedButton && validators.classification(formState.classification) !== ""}
-            />
-            <ErrorText>
-              {isClickedButton && validators.classification(formState.classification)}
-            </ErrorText>
-          </S.ContentInput>
-        </S.RowContainer>
+        <S.SubSection>
+          <S.RowContainer>
+            <S.ContentLabel>
+              제목 <S.RequiredLabel>*</S.RequiredLabel>
+            </S.ContentLabel>
+            <S.ContentInput>
+              <Input
+                name="title"
+                value={formState.title}
+                onChange={(e) => handleInputChange("title", e.target.value)}
+                showCharCount={true}
+                maxLength={50}
+                error={isClickedButton && validators.title(formState.title) !== ""}
+                required
+              />
+              <ErrorText>{isClickedButton && validators.title(formState.title)}</ErrorText>
+            </S.ContentInput>
+          </S.RowContainer>
 
-        <S.RowContainer>
-          <S.ContentLabel>내용</S.ContentLabel>
-          <S.ContentInput>
-            <Textarea
-              name="content"
-              value={formState.content}
-              onChange={(e) => handleInputChange("content", e.target.value)}
-              placeholder="방 설명, 코드 리뷰 방향성 등을 적어주세요"
-              rows={5}
-              showCharCount={true}
-              maxLength={4000}
-              error={isClickedButton && validators.content(formState.content) !== ""}
-            />
-            <ErrorText>{isClickedButton && validators.content(formState.content)}</ErrorText>
-          </S.ContentInput>
-        </S.RowContainer>
+          <S.RowContainer>
+            <S.ContentLabel>
+              분류 <S.RequiredLabel>*</S.RequiredLabel>
+            </S.ContentLabel>
+            <S.ContentInput>
+              <Dropdown
+                name="classification"
+                dropdownItems={dropdownItems}
+                selectedCategory={formState.classification}
+                onSelectCategory={(value) =>
+                  handleInputChange("classification", value as Classification)
+                }
+                error={
+                  isClickedButton && validators.classification(formState.classification) !== ""
+                }
+              />
+              <ErrorText>
+                {isClickedButton && validators.classification(formState.classification)}
+              </ErrorText>
+            </S.ContentInput>
+          </S.RowContainer>
 
-        <S.RowContainer>
-          <S.ContentLabel>
-            깃허브 레포지토리 링크 <span>*필수입력</span>
-          </S.ContentLabel>
-          <S.ContentInput>
-            <Input
-              name="repositoryLink"
-              value={formState.repositoryLink}
-              onChange={(e) => handleInputChange("repositoryLink", e.target.value)}
-              error={isClickedButton && validators.repositoryLink(formState.repositoryLink) !== ""}
-              required
-            />
-            <ErrorText>
-              {isClickedButton && validators.repositoryLink(formState.repositoryLink)}
-            </ErrorText>
-          </S.ContentInput>
-        </S.RowContainer>
+          <S.RowContainer>
+            <S.ContentLabel>
+              깃허브 레포지토리 링크 <S.RequiredLabel>*</S.RequiredLabel>
+            </S.ContentLabel>
+            <S.ContentInput>
+              <Input
+                name="repositoryLink"
+                value={formState.repositoryLink}
+                onChange={(e) => handleInputChange("repositoryLink", e.target.value)}
+                error={
+                  isClickedButton && validators.repositoryLink(formState.repositoryLink) !== ""
+                }
+                required
+              />
+              <ErrorText>
+                {isClickedButton && validators.repositoryLink(formState.repositoryLink)}
+              </ErrorText>
+            </S.ContentInput>
+          </S.RowContainer>
 
-        <S.RowContainer>
-          <S.ContentLabel>이미지 링크</S.ContentLabel>
-          <S.ContentInput>
-            <Input
-              name="thumbnailLink"
-              value={formState.thumbnailLink}
-              onChange={(e) => handleInputChange("thumbnailLink", e.target.value)}
-              error={isClickedButton && validators.thumbnailLink(formState.thumbnailLink) !== ""}
-            />
-            <ErrorText>
-              {isClickedButton && validators.thumbnailLink(formState.thumbnailLink)}
-            </ErrorText>
-          </S.ContentInput>
-        </S.RowContainer>
+          <S.RowContainer>
+            <S.ContentLabel>내용</S.ContentLabel>
+            <S.ContentInput>
+              <Textarea
+                name="content"
+                value={formState.content}
+                onChange={(e) => handleInputChange("content", e.target.value)}
+                placeholder="방 설명, 코드 리뷰 방향성 등을 적어주세요"
+                rows={5}
+                showCharCount={true}
+                maxLength={4000}
+                error={isClickedButton && validators.content(formState.content) !== ""}
+              />
+              <ErrorText>{isClickedButton && validators.content(formState.content)}</ErrorText>
+            </S.ContentInput>
+          </S.RowContainer>
 
-        <S.RowContainer>
-          <S.ContentLabel>키워드</S.ContentLabel>
-          <S.ContentInput>
-            <Keyword
-              currentKeywords={formState.keywords}
-              onKeywordsChange={(keywords) => handleInputChange("keywords", keywords)}
-              error={isClickedButton && validators.keywords(formState.keywords) !== ""}
-            />
-            <ErrorText>{isClickedButton && validators.keywords(formState.keywords)}</ErrorText>
-          </S.ContentInput>
-        </S.RowContainer>
+          <S.RowContainer>
+            <S.ContentLabel>키워드</S.ContentLabel>
+            <S.ContentInput>
+              <Keyword
+                currentKeywords={formState.keywords}
+                onKeywordsChange={(keywords) => handleInputChange("keywords", keywords)}
+                error={isClickedButton && validators.keywords(formState.keywords) !== ""}
+              />
+              <ErrorText>{isClickedButton && validators.keywords(formState.keywords)}</ErrorText>
+            </S.ContentInput>
+          </S.RowContainer>
 
-        <S.RowContainer>
-          <S.ContentLabel>
-            상호 리뷰 인원 <span>*필수입력</span>
-          </S.ContentLabel>
-          <S.ContentInput>
-            <Input
-              type="number"
-              min="1"
-              max="5"
-              name="matchingSize"
-              value={formState.matchingSize}
-              onChange={(e) => handleInputChange("matchingSize", parseInt(e.target.value, 10))}
-              error={isClickedButton && validators.matchingSize(formState.matchingSize) !== ""}
-              required
-            />
-            <ErrorText>
-              {isClickedButton && validators.matchingSize(formState.matchingSize)}
-            </ErrorText>
-          </S.ContentInput>
-        </S.RowContainer>
+          <S.RowContainer>
+            <S.ContentLabel>이미지 링크</S.ContentLabel>
+            <S.ContentInput>
+              <Input
+                name="thumbnailLink"
+                value={formState.thumbnailLink}
+                onChange={(e) => handleInputChange("thumbnailLink", e.target.value)}
+                error={isClickedButton && validators.thumbnailLink(formState.thumbnailLink) !== ""}
+              />
+              <ErrorText>
+                {isClickedButton && validators.thumbnailLink(formState.thumbnailLink)}
+              </ErrorText>
+            </S.ContentInput>
+          </S.RowContainer>
+        </S.SubSection>
 
-        <S.RowContainer>
-          <S.ContentLabel>
-            최대 참여 인원 <span>*필수입력</span>
-          </S.ContentLabel>
-          <S.ContentInput>
-            <Input
-              type="number"
-              min={formState.matchingSize}
-              name="limitedParticipants"
-              value={formState.limitedParticipants}
-              onChange={(e) =>
-                handleInputChange("limitedParticipants", parseInt(e.target.value, 10))
-              }
-              error={
-                isClickedButton &&
-                validators.limitedParticipants(
-                  formState.limitedParticipants,
-                  formState.matchingSize,
-                ) !== ""
-              }
-              required
-            />
-            <ErrorText>
-              {isClickedButton &&
-                validators.limitedParticipants(
-                  formState.limitedParticipants,
-                  formState.matchingSize,
-                )}
-            </ErrorText>
-          </S.ContentInput>
-        </S.RowContainer>
+        <S.SubSection>
+          <S.RowContainer>
+            <S.ContentLabel>
+              상호 리뷰 인원 <S.RequiredLabel>*</S.RequiredLabel>
+            </S.ContentLabel>
+            <S.ContentInput>
+              <Input
+                type="number"
+                min="1"
+                max="5"
+                name="matchingSize"
+                value={formState.matchingSize}
+                onChange={(e) => handleInputChange("matchingSize", parseInt(e.target.value, 10))}
+                error={isClickedButton && validators.matchingSize(formState.matchingSize) !== ""}
+                required
+              />
+              <ErrorText>
+                {isClickedButton && validators.matchingSize(formState.matchingSize)}
+              </ErrorText>
+            </S.ContentInput>
+          </S.RowContainer>
 
-        <S.RowContainer>
-          <S.ContentLabel>
-            모집 마감일 <span>*필수입력</span>
-          </S.ContentLabel>
-          <S.ContentInput>
-            <DateTimePicker
-              selectedDateTime={formState.recruitmentDeadline}
-              onDateTimeChange={(newDateTime) =>
-                handleInputChange("recruitmentDeadline", newDateTime)
-              }
-              error={
-                isClickedButton &&
-                validators.recruitmentDeadline(formState.recruitmentDeadline) !== ""
-              }
-            />
-            <ErrorText>
-              {isClickedButton && validators.recruitmentDeadline(formState.recruitmentDeadline)}
-            </ErrorText>
-          </S.ContentInput>
-        </S.RowContainer>
+          <S.RowContainer>
+            <S.ContentLabel>
+              최대 참여 인원 <S.RequiredLabel>*</S.RequiredLabel>
+            </S.ContentLabel>
+            <S.ContentInput>
+              <Input
+                type="number"
+                min={formState.matchingSize}
+                name="limitedParticipants"
+                value={formState.limitedParticipants}
+                onChange={(e) =>
+                  handleInputChange("limitedParticipants", parseInt(e.target.value, 10))
+                }
+                error={
+                  isClickedButton &&
+                  validators.limitedParticipants(
+                    formState.limitedParticipants,
+                    formState.matchingSize,
+                  ) !== ""
+                }
+                required
+              />
+              <ErrorText>
+                {isClickedButton &&
+                  validators.limitedParticipants(
+                    formState.limitedParticipants,
+                    formState.matchingSize,
+                  )}
+              </ErrorText>
+            </S.ContentInput>
+          </S.RowContainer>
 
-        <S.RowContainer>
-          <S.ContentLabel>
-            코드 리뷰 마감일 <span>*필수입력</span>
-          </S.ContentLabel>
-          <S.ContentInput>
-            <DateTimePicker
-              selectedDateTime={formState.reviewDeadline}
-              onDateTimeChange={(newDateTime) => handleInputChange("reviewDeadline", newDateTime)}
-              error={
-                isClickedButton &&
-                validators.reviewDeadline(
-                  formState.reviewDeadline,
-                  formState.recruitmentDeadline,
-                ) !== ""
-              }
-            />
-            <ErrorText>
-              {isClickedButton &&
-                validators.reviewDeadline(formState.reviewDeadline, formState.recruitmentDeadline)}
-            </ErrorText>
-          </S.ContentInput>
-        </S.RowContainer>
+          <S.RowContainer>
+            <S.ContentLabel>
+              모집 마감일 <S.RequiredLabel>*</S.RequiredLabel>
+            </S.ContentLabel>
+            <S.ContentInput>
+              <DateTimePicker
+                selectedDateTime={formState.recruitmentDeadline}
+                onDateTimeChange={(newDateTime) =>
+                  handleInputChange("recruitmentDeadline", newDateTime)
+                }
+                error={
+                  isClickedButton &&
+                  validators.recruitmentDeadline(formState.recruitmentDeadline) !== ""
+                }
+              />
+              <ErrorText>
+                {isClickedButton && validators.recruitmentDeadline(formState.recruitmentDeadline)}
+              </ErrorText>
+            </S.ContentInput>
+          </S.RowContainer>
+
+          <S.RowContainer>
+            <S.ContentLabel>
+              코드 리뷰 마감일 <S.RequiredLabel>*</S.RequiredLabel>
+            </S.ContentLabel>
+            <S.ContentInput>
+              <DateTimePicker
+                selectedDateTime={formState.reviewDeadline}
+                onDateTimeChange={(newDateTime) => handleInputChange("reviewDeadline", newDateTime)}
+                error={
+                  isClickedButton &&
+                  validators.reviewDeadline(
+                    formState.reviewDeadline,
+                    formState.recruitmentDeadline,
+                  ) !== ""
+                }
+              />
+              <ErrorText>
+                {isClickedButton &&
+                  validators.reviewDeadline(
+                    formState.reviewDeadline,
+                    formState.recruitmentDeadline,
+                  )}
+              </ErrorText>
+            </S.ContentInput>
+          </S.RowContainer>
+        </S.SubSection>
 
         <Button
           onClick={() => {
@@ -303,8 +323,8 @@ const RoomFormLayout = ({ formType, roomId, data }: RoomFormLayoutProps) => {
         >
           완료
         </Button>
-      </S.CreateSection>
-    </ContentSection>
+      </S.RoomFormContainer>
+    </>
   );
 };
 
