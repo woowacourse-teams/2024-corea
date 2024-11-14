b813d5b0f99550394a6b390e245c2896bbf7e0aa
jinsil <jinsil378900@gmail.com>
2024-11-14 14:21:46 +0900

    refactor: input forwardRef 제거

--- frontend/src/components/common/input/Input.tsx
+++ frontend/src/components/common/input/Input.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, forwardRef } from "react";
+import { InputHTMLAttributes } from "react";
 import * as S from "@/components/common/input/Input.style";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -6,33 +6,34 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   showCharCount?: boolean;
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ error = false, showCharCount = false, value = "", onChange, ...rest }, ref) => {
-    const onInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (onChange) {
-        onChange(e);
-      }
-    };
+export const Input = ({
+  error = false,
+  showCharCount = false,
+  value = "",
+  onChange,
+  ...rest
+}: InputProps) => {
+  const onInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (onChange) {
+      onChange(e);
+    }
+  };
 
-    return (
-      <S.InputWrapper>
-        <S.StyledInput
-          ref={ref}
-          $error={error}
-          value={value}
-          onChange={onInputHandler}
-          maxLength={rest.maxLength}
-          {...rest}
-        />
-        {showCharCount && (
-          <S.CharCount aria-hidden>
-            {value.toString().length}
-            {rest.maxLength ? ` / ${rest.maxLength}자` : ""}
-          </S.CharCount>
-        )}
-      </S.InputWrapper>
-    );
-  },
-);
-
-Input.displayName = "Input";
+  return (
+    <S.InputWrapper>
+      <S.StyledInput
+        $error={error}
+        value={value}
+        onChange={onInputHandler}
+        maxLength={rest.maxLength}
+        {...rest}
+      />
+      {showCharCount && (
+        <S.CharCount aria-hidden>
+          {value.toString().length}
+          {rest.maxLength ? ` / ${rest.maxLength}자` : ""}
+        </S.CharCount>
+      )}
+    </S.InputWrapper>
+  );
+};
