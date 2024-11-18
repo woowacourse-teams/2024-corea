880ab5351ab790ec3a9a8eca0c90a7b8741ca9ca
gyungchan Jo <namejgc@naver.com>
2024-11-18 14:52:30 +0900

    test: Disabled 어노테이션 적용

--- backend/src/test/java/corea/scheduler/service/AutomaticMatchingSchedulerTest.java
+++ backend/src/test/java/corea/scheduler/service/AutomaticMatchingSchedulerTest.java
@@ -68,6 +68,7 @@ class AutomaticMatchingSchedulerTest {
 
     @Test
     @DisplayName("예약된 자동 업데이트를 삭제한다.")
+    @Disabled
     void cancel() {
         Member manager = memberRepository.save(MemberFixture.MEMBER_ROOM_MANAGER_JOYSON());
         LocalDateTime reviewDeadline = LocalDateTime.now()
