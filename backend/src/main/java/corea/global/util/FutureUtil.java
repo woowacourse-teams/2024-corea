6ca2bc3ddae402c089dca9da6409bbb75389efab
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-11-18 14:56:02 +0900

    feat: 스레드 풀 생성 및 사용하게 지정

--- backend/src/main/java/corea/global/util/FutureUtil.java
+++ backend/src/main/java/corea/global/util/FutureUtil.java
@@ -1,11 +1,16 @@
 package corea.global.util;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import java.util.function.Supplier;
 
 public class FutureUtil {
 
     public static <T> CompletableFuture<T> supplyAsync(Supplier<T> supplier) {
         return CompletableFuture.supplyAsync(supplier);
+    }
+
+    public static <T> CompletableFuture<T> supplyAsync(final Supplier<T> supplier, final Executor executor) {
+        return CompletableFuture.supplyAsync(supplier, executor);
     }
 }
