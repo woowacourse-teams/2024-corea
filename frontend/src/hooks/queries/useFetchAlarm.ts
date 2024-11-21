e193a1aa4b997a7e4fc1fb45e90e6e0ae91e4034
00kang <kdb070970@gmail.com>
2024-11-21 14:54:50 +0900

    refactor: 로그인하지 않았을 때 불필요하게 alarm/count api 호출되는 상황 막기

--- frontend/src/hooks/queries/useFetchAlarm.ts
+++ frontend/src/hooks/queries/useFetchAlarm.ts
@@ -2,10 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { getAlarmCount, getAlarmList } from "@/apis/alarm.api";
 import QUERY_KEYS from "@/apis/queryKeys";
 
-export const useFetchAlarmCount = () => {
+export const useFetchAlarmCount = (enabled: boolean = false) => {
   return useQuery({
     queryKey: [QUERY_KEYS.ALARM_COUNT],
     queryFn: getAlarmCount,
+    enabled,
   });
 };
 
