53ab51194944e53d6472881be1477abc21108034
00kang <kdb070970@gmail.com>
2024-11-22 11:26:51 +0900

    refactor: 알림 문구와 path 객체로 매핑

--- frontend/src/components/alarm/alarmItem/AlarmItem.tsx
+++ frontend/src/components/alarm/alarmItem/AlarmItem.tsx
@@ -1,26 +1,82 @@
 import * as S from "@/components/alarm/alarmItem/AlarmItem.style";
 import Profile from "@/components/common/profile/Profile";
-import { AlarmActionType, AlarmItemData } from "@/@types/alaram";
+import { AlarmItemData } from "@/@types/alaram";
 import { mainLogo } from "@/assets";
 import { formatTimeAgo } from "@/utils/dateFormatter";
+
+const ALARM_MESSAGES_WITH_USERNAME = {
+  REVIEW_COMPLETE: (username: string, info: string) => {
+    return (
+      <>
+        <span>{username}</span> 님이 <span>{info}</span> 미션에 대해 <span>코드리뷰를 완료</span>
+        했습니다.
+      </>
+    );
+  },
+
+  REVIEW_URGE: (username: string, info: string) => {
+    return (
+      <>
+        <span>{username}</span> 님이 <span>{info}</span> 미션에 대해 <span>코드리뷰를 재촉</span>
+        했습니다.
+      </>
+    );
+  },
+
+  FEEDBACK_CREATED: (username: string, info: string) => {
+    return (
+      <>
+        <span>{username}</span> 님이 <span>{info}</span> 미션에 대해 <span>피드백을 작성</span>
+        했습니다.
+      </>
+    );
+  },
+} as const;
+
+const ALARM_MESSAGES_WITHOUT_USERNAME = {
+  MATCH_COMPLETE: (info: string) => {
+    return (
+      <>
+        <span>{info}</span> 미션에 대해 <span>매칭이 완료</span> 됐습니다.
+      </>
+    );
+  },
+
+  MATCH_FAIL: (info: string) => {
+    return (
+      <>
+        <span>{info}</span> 미션에 대해 <span>매칭이 실패</span> 했습니다.
+      </>
+    );
+  },
+} as const;
 
 interface AlarmItemProps {
   alarm: AlarmItemData;
   onAlarmClick: (alarm: AlarmItemData) => void;
-  getAlarmMessage: (actionType: AlarmActionType, info: string, username?: string) => JSX.Element;
 }
 
-const AlarmItem = ({ alarm, onAlarmClick, getAlarmMessage }: AlarmItemProps) => {
+const AlarmItem = ({ alarm, onAlarmClick }: AlarmItemProps) => {
+  const { alarmId, actor, isRead, actionType, interaction, createAt } = alarm;
+
+  const getAlarmMessage = () => {
+    if (actionType === "MATCH_COMPLETE" || actionType === "MATCH_FAIL") {
+      const message = ALARM_MESSAGES_WITHOUT_USERNAME[actionType];
+      return message?.(interaction.info) ?? <></>;
+    }
+
+    const message = ALARM_MESSAGES_WITH_USERNAME[actionType];
+    return message?.(actor?.username ?? "", interaction.info) ?? <></>;
+  };
+
   return (
-    <S.AlarmItem key={alarm.alarmId} $isRead={alarm.isRead} onClick={() => onAlarmClick(alarm)}>
+    <S.AlarmItem key={alarmId} $isRead={isRead} onClick={() => onAlarmClick(alarm)}>
       <S.ProfileWrapper>
-        <Profile imgSrc={alarm.actor ? alarm.actor.thumbnailUrl : mainLogo} size={40} />
+        <Profile imgSrc={actor ? actor.thumbnailUrl : mainLogo} size={40} />
       </S.ProfileWrapper>
       <S.ContentWrapper>
-        <S.Content $isRead={alarm.isRead}>
-          {getAlarmMessage(alarm.actionType, alarm.interaction.info, alarm.actor?.username)}
-        </S.Content>
-        <S.TimeStamp $isRead={alarm.isRead}>{formatTimeAgo(alarm.createAt)}</S.TimeStamp>
+        <S.Content $isRead={isRead}>{getAlarmMessage()}</S.Content>
+        <S.TimeStamp $isRead={isRead}>{formatTimeAgo(createAt)}</S.TimeStamp>
       </S.ContentWrapper>
     </S.AlarmItem>
   );
--- frontend/src/pages/alarm/AlarmPage.tsx
+++ frontend/src/pages/alarm/AlarmPage.tsx
@@ -5,83 +5,21 @@ import AlarmItem from "@/components/alarm/alarmItem/AlarmItem";
 import EmptyAlarm from "@/components/alarm/emptyAlarm/EmptyAlarm";
 import ContentSection from "@/components/common/contentSection/ContentSection";
 import * as S from "@/pages/alarm/AlarmPage.style";
-import { AlarmActionType, AlarmItemData } from "@/@types/alaram";
+import { AlarmItemData } from "@/@types/alaram";
+
+const NAVIGATION_PATHS = {
+  REVIEW_COMPLETE: (interactionId: number) => `/rooms/${interactionId}`,
+  REVIEW_URGE: (interactionId: number) => `/rooms/${interactionId}`,
+  MATCH_COMPLETE: (interactionId: number) => `/rooms/${interactionId}`,
+  MATCH_FAIL: (interactionId: number) => `/rooms/${interactionId}`,
+  FEEDBACK_CREATED: () => "/feedback",
+} as const;
 
 const AlarmPage = () => {
   const navigate = useNavigate();
   const { data } = useFetchAlarmList();
   const { markAsRead } = useMutateAlarm();
   const alarmListData = data?.data;
-
-  const getAlarmMessage = (actionType: AlarmActionType, info: string, username?: string) => {
-    if (actionType === "REVIEW_COMPLETE") {
-      return (
-        <>
-          <span>{username}</span> 님이 <span>{info}</span> 미션에 대해 <span>코드리뷰를 완료</span>
-          했습니다.
-        </>
-      );
-    }
-
-    if (actionType === "REVIEW_URGE") {
-      return (
-        <>
-          <span>{username}</span> 님이 <span>{info}</span> 미션에 대해 <span>코드리뷰를 재촉</span>
-          했습니다.
-        </>
-      );
-    }
-
-    if (actionType === "MATCH_COMPLETE") {
-      return (
-        <>
-          <span>{info}</span> 미션에 대해 <span>매칭이 완료</span> 됐습니다.
-        </>
-      );
-    }
-
-    if (actionType === "MATCH_FAIL") {
-      return (
-        <>
-          <span>{info}</span> 미션에 대해 <span>매칭이 실패</span> 했습니다.
-        </>
-      );
-    }
-
-    if (actionType === "FEEDBACK_CREATED") {
-      return (
-        <>
-          <span>{username}</span> 님이 <span>{info}</span> 미션에 대해 <span>피드백을 작성</span>
-          했습니다.
-        </>
-      );
-    }
-
-    return <></>;
-  };
-
-  const getNavigationPath = (actionType: AlarmActionType, interactionId: number) => {
-    if (actionType === "REVIEW_COMPLETE") {
-      return `/rooms/${interactionId}`;
-    }
-
-    if (actionType === "REVIEW_URGE") {
-      return `/rooms/${interactionId}`;
-    }
-
-    if (actionType === "MATCH_COMPLETE") {
-      return `/rooms/${interactionId}`;
-    }
-
-    if (actionType === "MATCH_FAIL") {
-      return `/rooms/${interactionId}`;
-    }
-
-    if (actionType === "FEEDBACK_CREATED") {
-      return `/feedback`;
-    }
-    return "";
-  };
 
   const handleAlarmClick = async (alarm: AlarmItemData) => {
     const { actionType, alarmId, isRead, interaction } = alarm;
@@ -90,9 +28,9 @@ const AlarmPage = () => {
       await markAsRead.mutateAsync({ alarmId, alarmType: "USER" });
     }
 
-    const path = getNavigationPath(actionType, interaction.interactionId);
+    const path = NAVIGATION_PATHS[actionType];
     if (path) {
-      navigate(path);
+      navigate(path(interaction.interactionId));
     }
   };
 
@@ -105,12 +43,7 @@ const AlarmPage = () => {
       <ContentSection title="알림 내역">
         <S.AlarmList>
           {alarmListData?.map((alarm) => (
-            <AlarmItem
-              key={alarm.alarmId}
-              alarm={alarm}
-              onAlarmClick={handleAlarmClick}
-              getAlarmMessage={getAlarmMessage}
-            />
+            <AlarmItem key={alarm.alarmId} alarm={alarm} onAlarmClick={handleAlarmClick} />
           ))}
         </S.AlarmList>
       </ContentSection>
