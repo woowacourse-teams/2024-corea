02aead8a0b7d27d9391adc1b1dd0548fdfc3cdba
jinsil <jinsil378900@gmail.com>
2025-03-20 18:07:44 +0900

    feat: 로그인, 리프레시, 로그아웃 모킹

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,10 +1,12 @@
+import authHandler from "./handler/authHandler";
 import rankingHandler from "./handler/rankingHandler";
-import alarmHandler from "@/mocks/handler/alaramHandler";
+import alarmHandler from "@/mocks/handler/alarmHandler";
 import feedbackHandler from "@/mocks/handler/feedbackHandler";
 import profileHandler from "@/mocks/handler/profileHandler";
 import roomHandler from "@/mocks/handler/roomHandler";
 
 const handlers = [
+  ...authHandler,
   ...roomHandler,
   ...profileHandler,
   ...feedbackHandler,
