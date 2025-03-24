5ff200d6563507c76eedd3b14c16df610bf17f9a
jinsil <jinsil378900@gmail.com>
2025-03-25 00:28:50 +0900

    feat: QueryProvider로 queryClient 코드 분리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,28 @@
+import QueryProvider from "./QueryProvider";
+import Toast from "./components/common/Toast/Toast";
 import SentryTotalBoundary from "./components/common/errorBoundary/SentryTotalBoundary";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastProvider } from "./providers/ToastProvider";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import Toast from "@/components/common/Toast/Toast";
-import { ToastProvider } from "@/providers/ToastProvider";
 import GlobalStyles from "@/styles/globalStyles";
 import { theme } from "@/styles/theme";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      networkMode: "always",
-      refetchOnWindowFocus: false,
-      retry: false,
-      throwOnError: true,
-    },
-    mutations: {
-      networkMode: "always",
-      onError: () => console.log("에러"),
-    },
-  },
-});
-
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <ToastProvider>
-          <GlobalStyles />
-          <Toast />
+    <QueryProvider>
+      <ToastProvider>
+        <GlobalStyles />
+        <ThemeProvider theme={theme}>
           <SentryTotalBoundary>
+            <ReactQueryDevtools initialIsOpen={false} />
+            <Toast />
             <Outlet />
           </SentryTotalBoundary>
-        </ToastProvider>
-      </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+        </ThemeProvider>
+      </ToastProvider>
+    </QueryProvider>
   );
 };
 
