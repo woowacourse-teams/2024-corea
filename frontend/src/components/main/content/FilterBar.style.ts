45f1c4229965c5d863cbd237336de4a08e2c8847
jinsil <jinsil378900@gmail.com>
2025-03-26 04:21:42 +0900

    feat: FilterBar 컴포넌트 생성

---
+++ frontend/src/components/main/content/FilterBar.style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const SearchBarWrapper = styled.div`
+  width: 180px;
+`;
+
+export const FilterWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
