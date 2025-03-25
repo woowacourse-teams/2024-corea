89abc6321069b48a231a5eb246fdd935d915fe30
jinsil <jinsil378900@gmail.com>
2025-03-25 15:51:34 +0900

    refactor: AlarmButton 분리

---
+++ frontend/src/components/common/header/AlarmButton.tsx
@@ -0,0 +1,26 @@
+import Icon from "../icon/Icon";
+import * as S from "./AlarmButton.style";
+import React from "react";
+import { Link } from "react-router-dom";
+import { useFetchAlarmCount } from "@/hooks/queries/useFetchAlarm";
+import { theme } from "@/styles/theme";
+
+const AlarmButton = () => {
+  const isLoggedIn = !!localStorage.getItem("accessToken");
+  const { data: alarmCountData } = useFetchAlarmCount(isLoggedIn);
+
+  if (!isLoggedIn) return null;
+
+  return (
+    <Link to="/alarm">
+      <S.AlarmIconWrapper>
+        <Icon kind="notificationBell" size="2.8rem" color={theme.COLOR.grey3} />
+        {alarmCountData && alarmCountData.count > 0 && (
+          <S.Count>{alarmCountData.count >= 10 ? "9+" : alarmCountData.count}</S.Count>
+        )}
+      </S.AlarmIconWrapper>
+    </Link>
+  );
+};
+
+export default AlarmButton;
