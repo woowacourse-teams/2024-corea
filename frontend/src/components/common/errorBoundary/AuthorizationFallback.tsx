7b37fbaf631958f107f08318174773edb4e3e5ea
jinsil <jinsil378900@gmail.com>
2025-03-30 16:24:01 +0900

    feat: ErrorModal context 생성

--- frontend/src/components/common/errorBoundary/AuthorizationFallback.tsx
+++ frontend/src/components/common/errorBoundary/AuthorizationFallback.tsx
@@ -1,41 +1,7 @@
-import AlertModal from "../modal/alertModal/AlertModal";
-import React, { useEffect } from "react";
-import useModal from "@/hooks/common/useModal";
-import Banner from "@/components/main/banner/Banner";
-import { githubAuthUrl } from "@/config/githubAuthUrl";
-import MESSAGES from "@/constants/message";
+import React from "react";
 
 const AuthorizationFallback = () => {
-  const { isModalOpen, handleOpenModal, handleCloseModal } = useModal();
-
-  useEffect(() => {
-    const alreadyHandled = sessionStorage.getItem("auth_fallback_handled");
-
-    if (!alreadyHandled) {
-      sessionStorage.setItem("auth_fallback_handled", "true");
-      localStorage.clear();
-      handleOpenModal();
-    }
-  }, []);
-
-  const handleRedirect = (url: string) => {
-    sessionStorage.removeItem("auth_fallback_handled");
-    handleCloseModal();
-    window.location.href = url;
-  };
-
-  return (
-    <>
-      <AlertModal
-        isOpen={isModalOpen}
-        onClose={() => handleRedirect("/")}
-        onConfirm={() => handleRedirect(githubAuthUrl)}
-      >
-        {MESSAGES.ERROR.POST_REFRESH}
-      </AlertModal>
-      <Banner />
-    </>
-  );
+  return <div>Auth 에러</div>;
 };
 
 export default AuthorizationFallback;
