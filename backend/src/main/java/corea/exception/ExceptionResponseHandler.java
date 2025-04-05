01f1b887d8580f6b963a3b5a5220fc759c6d52ce
hjk0761 <hjk0761@naver.com>
2025-04-05 23:40:54 +0900

    feat: 리프레시 토큰 만료로 인한 예외 발생 시 쿠키 삭제 기능 구현

--- backend/src/main/java/corea/exception/ExceptionResponseHandler.java
+++ backend/src/main/java/corea/exception/ExceptionResponseHandler.java
@@ -1,21 +1,38 @@
 package corea.exception;
 
+import corea.auth.infrastructure.CookieProvider;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import static corea.exception.ExceptionType.TOKEN_EXPIRED;
+import static corea.global.config.Constants.REFRESH_COOKIE;
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
+
 @Slf4j
 @ControllerAdvice
 public class ExceptionResponseHandler {
+
     private static final String SERVER_ERROR_MESSAGE = "서버에 문제가 발생했습니다.";
+
+    private final CookieProvider cookieProvider;
+
+    public ExceptionResponseHandler(CookieProvider cookieProvider) {
+        this.cookieProvider = cookieProvider;
+    }
 
     @ExceptionHandler(CoreaException.class)
     public ResponseEntity<CoreaErrorResponse> handleCoreaException(CoreaException e) {
         log.debug("Corea Custom exception [statusCode = {}, errorMessage = {}, cause = {}]", e.getHttpStatus(), e.getMessage(), e.getStackTrace());
-        return ResponseEntity.status(e.getHttpStatus())
-                .body(CoreaErrorResponse.from(e));
+        ResponseEntity.BodyBuilder status = ResponseEntity.status(e.getHttpStatus());
+        if (e.getExceptionType().equals(TOKEN_EXPIRED)) {
+            ResponseCookie expiredRefreshCookie = cookieProvider.createExpiredCookie(REFRESH_COOKIE);
+            status.header(SET_COOKIE, expiredRefreshCookie.toString());
+        }
+        return status.body(CoreaErrorResponse.from(e));
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
